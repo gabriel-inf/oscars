767d6d4668cc44ae17e5f8ee905cadc7385e8581
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-04-04 20:09:22 +0000

    Scripts to create BSS database (one of two separated out from old oscars database; the other is AAAS).
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@173 c75a6b91-02ef-0310-b74a-e20389c9c41d

---
+++ oscars/rm/bss/dbscripts/create_tables.sql
@@ -0,0 +1,54 @@
+CREATE DATABASE IF NOT EXISTS BSS;
+USE BSS;
+
+-- for example, ESnet
+CREATE TABLE IF NOT EXISTS networks (
+    network_id INT(3) NOT NULL AUTO_INCREMENT,
+    network_name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (network_id)
+) type=MyISAM;
+
+CREATE TABLE IF NOT EXISTS routers (
+    router_id INT(5) NOT NULL AUTO_INCREMENT,
+    router_valid BOOLEAN NOT NULL,
+    router_name VARCHAR(50) NOT NULL,
+    router_loopback VARCHAR(50) NOT NULL,
+    network_id INT(3) NOT NULL,
+    PRIMARY KEY (router_id)
+) type=MyISAM;
+
+-- would need trigger updating paths if changed
+
+CREATE TABLE IF NOT EXISTS interfaces (
+    interface_id INT(5) NOT NULL AUTO_INCREMENT,
+    interface_valid BOOLEAN NOT NULL,
+    interface_speed INT(15) NOT NULL,
+    interface_descr VARCHAR(5),
+    interface_alias VARCHAR(512),
+    router_id INT(5) NOT NULL,
+    PRIMARY KEY (interface_id)
+) type=MyISAM;
+
+CREATE TABLE IF NOT EXISTS ipaddrs (
+    ipaddrs_id INT(5) NOT NULL AUTO_INCREMENT,
+    interface INT(5) NOT NULL,
+    PRIMARY KEY (ipaddrs_id)
+) type=MyISAM;
+
+CREATE TABLE IF NOT EXISTS reservations (
+    reservation_id INT(5) NOT NULL AUTO_INCREMENT,
+      -- in GMT
+    reservation_start_time DATETIME,
+    reservation_end_time DATETIME,
+    reservation_qos VARCHAR(50),
+      -- need list of statuses to choose from
+    reservation_status VARCHAR(12),
+    reservation_description TEXT,
+    reservation_created_time DATETIME NOT NULL,
+    reservation_ingress_port INT(5) NOT NULL,
+    reservation_egress_port INT(5) NOT NULL,
+    ingress_interface_id INT(5) NOT NULL,
+    egress_interface_id INT(5) NOT NULL,
+    user_dn VARCHAR(60) NOT NULL,
+    PRIMARY KEY (reservation_id)
+) type = MyISAM;
