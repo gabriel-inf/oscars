75e9877c6f755b72b99a443998dcad3b494d4e77
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-04-15 06:20:55 +0000

    Printing out result key/value pairs.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@306 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/rm/WBUI/cgi/user/t/getprofile.pl
+++ oscars/rm/WBUI/cgi/user/t/getprofile.pl
@@ -2,6 +2,17 @@
 
 require '../soapclient.pl';
 
+my($value);
 my %params = ('loginname' => 'davidr' );
 my($unused, %results) = soap_get_user_profile(%params);
-print $results{'status_msg'}, ' ', $results{'data'}, "\n\n";
+print "Status:  ", $results{'status_msg'}, "\n";
+print "Returning:\n\n";
+foreach $key(sort keys %results)
+{
+    if ($key ne 'status_msg')
+    {
+        $value = $results{$key};
+        if ($value) { print "$key -> $value\n"; }
+        else { print "$key -> \n"; }
+    }
+}
