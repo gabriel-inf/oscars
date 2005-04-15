52e04a2776ac041c715a6735288b26cf9f2299fb
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-04-15 03:46:58 +0000

    Tests for corresponding SOAP methods (they don't test CGI scripts).
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@295 c75a6b91-02ef-0310-b74a-e20389c9c41d

---
+++ oscars/rm/WBUI/cgi/user/t/getprofile.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl -w
+
+require 'soapclient.pl';
+
+my %params = ('loginname' => 'davidr' );
+my($unused, %results) = soap_get_user_profile(%params);
+print $results{'status_msg'}, ' ', $results{'data'}, "\n\n";
---
+++ oscars/rm/WBUI/cgi/user/t/login.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl -w
+
+require 'soapclient.pl';
+
+my $passwd = crypt('shyysh', 'oscars');
+my %params = ('loginname' => 'davidr', 'password' => $passwd);
+my($result, %data) = soap_process_user_login(%params);
+print $data{'status_msg'}, "\n\n";
