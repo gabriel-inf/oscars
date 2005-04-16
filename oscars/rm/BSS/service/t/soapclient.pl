88f903e57895afc132bb3f906a96e32d1d17b686
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-04-16 21:14:00 +0000

    Test directory for BSS SOAP service.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@330 c75a6b91-02ef-0310-b74a-e20389c9c41d

---
+++ oscars/rm/BSS/service/t/soapclient.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl -w
+
+use SOAP::Lite;
+
+
+my $BSS_server = SOAP::Lite
+  -> uri('http://localhost:5000/BSSServer')
+  -> proxy ('http://localhost:5000/soapserver.pl');
+
+sub soap_get_reservations
+{
+    my (%params) = @_;
+    my $response = $BSS_server -> Get_reservations(%params);
+    if ($response->fault) {
+        print $response->faultcode, " ", $response->faultstring, "\n";
+    }
+        #  params are either user profile, or error message
+    return ($response->result(), $response->paramsout());
+}
+
+
+sub soap_create_reservation
+{
+    my (%params) = @_;
+    my $response = $BSS_server -> Create_reservation(%params);
+    if ($response->fault) {
+        print $response->faultcode, " ", $response->faultstring, "\n";
+    }
+        #  params are either user profile, or error message
+    return ($response->result(), $response->paramsout());
+}
+
+
+sub soap_remove_reservation
+{
+    my (%params) = @_;
+    my $response = $BSS_server -> Remove_reservation(%params);
+    if ($response->fault) {
+        print $response->faultcode, " ", $response->faultstring, "\n";
+    }
+        #  params are either user profile, or error message
+    return ($response->result(), $response->paramsout());
+}
+
