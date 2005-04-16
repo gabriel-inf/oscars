371729e1061f7df0c186cdf468a723af67b6a23b
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-04-16 21:04:31 +0000

    Now in AAAS/service/t
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@322 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/rm/WBUI/cgi/user/t/login.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl -w
-
-require '../soapclient.pl';
-
-my $passwd = crypt('shyysh', 'oscars');
-my %params = ('loginname' => 'davidr', 'password' => $passwd);
-my($result, %data) = soap_verify_login(%params);
-print $data{'status_msg'}, "\n\n";
