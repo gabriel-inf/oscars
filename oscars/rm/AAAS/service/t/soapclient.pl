24211ec020f729ab67e21c7b56fda6afc2b3b59b
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-04-17 21:38:47 +0000

    Tests for SOAP client calls using AAAS front end.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@334 c75a6b91-02ef-0310-b74a-e20389c9c41d

---
+++ oscars/rm/AAAS/service/t/soapclient.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl -w
+
+use SOAP::Lite;
+
+use strict;
+
+my $AAAS_server = SOAP::Lite
+  -> uri('http://localhost:4000/AAASServer')
+  -> proxy ('http://localhost:4000/soapserver.pl');
+
+# TODO:  one SOAP call that dispatches according to server, subroutine args
+
+sub soap_verify_login
+{
+    my (%params) = @_;
+    my $response = $AAAS_server -> Verify_login(%params);
+    if ($response->fault) {
+        print $response->faultcode, " ", $response->faultstring, "\n";
+    }
+    return ($response->result(), $response->paramsout());
+}
+
+sub soap_get_profile
+{
+    my (%params) = @_;
+    my $response = $AAAS_server -> Get_profile(%params);
+    if ($response->fault) {
+        print $response->faultcode, " ", $response->faultstring, "\n";
+    }
+        #  params are either user profile, or error message
+    return ($response->result(), $response->paramsout());
+}
+
+
+sub soap_set_profile
+{
+    my (%params) = @_;
+    my $response = $AAAS_server -> Set_profile(%params);
+    if ($response->fault) {
+        print $response->faultcode, " ", $response->faultstring, "\n";
+    }
+        #  params are either user profile, or error message
+    return ($response->result(), $response->paramsout());
+}
