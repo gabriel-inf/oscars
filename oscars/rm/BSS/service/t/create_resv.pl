110148e713e687ff5a5f0cbf376619ea641adf35
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-04-18 20:00:21 +0000

    Fixed syntax errors.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@358 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/rm/BSS/service/t/create_resv.pl
+++ oscars/rm/BSS/service/t/create_resv.pl
@@ -7,7 +7,7 @@ require 'soapclient.pl';
 my( %params );
 
     # setup some example stuff
-params{'id'} =              'NULL';
+$params{'id'} =              'NULL';
 
     # in seconds since epoch
 my $dt = DateTime->now();
@@ -20,8 +20,8 @@ $params{'class'}=           'test';
 $params{'burst_limit'}=     '100m';
 $params{'status'} =         'pending';
 
-$params{'ingress_interface_id'}= '';   db lookup in scheduler
-$params{'egress_interface_id'}=  '';   db lookup in scheduler
+$params{'ingress_interface_id'}= '';   # db lookup in scheduler
+$params{'egress_interface_id'}=  '';   # db lookup in scheduler
 
     # www.mcs.anl.gov, CGI Perl script will do any DNS lookup
 $params{'src_ip'} = '140.221.9.193';     # db lookup in scheduler
