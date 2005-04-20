f64584297041335093034740e3f9de38dccd71b2
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-04-20 20:57:36 +0000

    Bad foreign keys for institutions in users table.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@409 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/rm/AAAS/dbscripts/populate_tables.sql
+++ oscars/rm/AAAS/dbscripts/populate_tables.sql
@@ -13,6 +13,6 @@ delete from auth_types;
 insert into auth_types values('NULL', 'db password');
 
 delete from users;
-insert into users values('NULL', 'Guok', 'Chin', 'chin', ENCRYPT('esosc@rs77'), 'chin@es.net', NULL, NULL, NULL, NULL, 1, NULL, NULL, NULL, 1, 10);
-insert into users values('NULL', 'Robertson', 'David', 'davidr', ENCRYPT('shyysh'), 'dwrobertson@lbl.gov', NULL, NULL, NULL, NULL, 1, NULL, NULL,NULL, 1, 12);
-insert into users values('NULL', 'Lee', 'Jason', 'jason', ENCRYPT('ritazza6'),'jrlee@lbl.gov', NULL, NULL, NULL, NULL, 1, NULL, NULL, NULL, 1, 12);
+insert into users values('NULL', 'Guok', 'Chin', 'chin', ENCRYPT('esosc@rs77'), 'chin@es.net', NULL, NULL, NULL, NULL, 1, NULL, NULL, NULL, 1, 1);
+insert into users values('NULL', 'Robertson', 'David', 'davidr', ENCRYPT('shyysh'), 'dwrobertson@lbl.gov', NULL, NULL, NULL, NULL, 1, NULL, NULL,NULL, 1, 3);
+insert into users values('NULL', 'Lee', 'Jason', 'jason', ENCRYPT('ritazza6'),'jrlee@lbl.gov', NULL, NULL, NULL, NULL, 1, NULL, NULL, NULL, 1, 3);
