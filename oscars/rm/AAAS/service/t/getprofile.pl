7ec65e12d65b69f6b3d38eea788045c72f2630e4
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-04-20 20:55:46 +0000

    Added additional fields to print.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@405 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/rm/AAAS/service/t/getprofile.pl
+++ oscars/rm/AAAS/service/t/getprofile.pl
@@ -5,7 +5,7 @@ require 'soapclient.pl';
 my($value);
 my %params = ('dn' => 'davidr' );
     # names of the fields to be displayed on the screen
-my @fields_to_display = ( 'last_name', 'first_name', 'dn', 'email_primary', 'email_secondary', 'phone_primary', 'phone_secondary', 'description' );
+my @fields_to_display = ( 'last_name', 'first_name', 'dn', 'email_primary', 'email_secondary', 'phone_primary', 'phone_secondary', 'description', 'level', 'register_time', 'activation_key', 'pending_level', 'authorization_id', 'institution_id' );
 
 my($unused, %results) = soap_get_profile(\%params, \@fields_to_display);
 print "Status:  ", $results{'status_msg'}, "\n";
