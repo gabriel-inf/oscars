74ce71e33b7dcc6c4a0db5e268fe7388653d1781
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-04-22 21:27:46 +0000

    Dispatching directly to instance of ReservationHandler class.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@469 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/rm/BSS/service/BSS_server.pl
+++ oscars/rm/BSS/service/BSS_server.pl
@@ -11,10 +11,6 @@ use SOAP::Transport::HTTP;
 use lib '../..';
 use lib '../Scheduler/lib/perl5';
 
-use BSS::Scheduler::SchedulerThread;
-
-use Config::Auto;
-
 ## we want to thread on each accept, as some requests can take a 
 ## while (i.e. running traceroute)
 use SOAP::Transport::HTTP::Daemon::ThreadOnAccept;
@@ -23,50 +19,30 @@ use SOAP::Transport::HTTP::Daemon::ThreadOnAccept;
 # don't want to die on 'Broken pipe' or Ctrl-C
 #$SIG{PIPE} = $SIG{INT} = 'IGNORE';
 
+use BSS::Scheduler::SchedulerThread;
+use BSS::Scheduler::ReservationHandler;
+
+use Config::Auto;
+
 # slurp up the config file
-use vars qw ($config);
+our ($config, $dbUser);
+
 $config = Config::Auto::parse('BSS.config');
 
+$db_handler = BSS::Scheduler::ReservationHandler->new('configs' => $config);
+
 # start up a thread to monitor the DB
-start_scheduler($config);
+start_scheduler($config, $db_handler);
 
 # Create a SOAP server
 #my $daemon = SOAP::Transport::HTTP::Daemon::ThreadOnAccept
 my $daemon = SOAP::Transport::HTTP::Daemon
 	-> new (LocalPort => $config->{'server_port'}, Listen => 5, Reuse => 1)
-	-> dispatch_to('.', 'BSS_Server')
+	-> dispatch_to('.', $db_handler)
 	;
 
 # and away we go
 $daemon->handle;
-
-
-##########################
-
-package BSS_Server;
-
-use lib '../..';
-
-our @ISA = qw(Exporter);
-our @EXPORT = qw(Create_reservation Remove_reservation Get_reservations);
-
-use BSS::Scheduler::ReservationHandler;
-use BSS::Frontend::Reservation;
-
-sub Create_reservation {
-  my ($class, $inref ) = @_;
-  return (create_reservation($inref));
-}
-
-sub Remove_reservation {
-  my ($class, $inref) = @_;
-  return (remove_reservation($inref));
-}
-
-sub Get_reservations {
-  my ($class, $inref) = @_;
-  return (get_reservations($inref));
-}
 
 
 # vim: et ts=4 sw=4
