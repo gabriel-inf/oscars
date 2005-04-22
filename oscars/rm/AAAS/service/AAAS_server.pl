108fbc67fbb280cbcbef313f4b17876cf7499b1d
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-04-22 17:23:57 +0000

    Changes having to do with dispatching directly to class.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@462 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/rm/AAAS/service/AAAS_server.pl
+++ oscars/rm/AAAS/service/AAAS_server.pl
@@ -1,41 +1,24 @@
 #!/usr/bin/perl -w
 
-use SOAP::Transport::HTTP;
-
 use strict;
 
+use Config::Auto;
+use SOAP::Transport::HTTP;
+
+use lib '../..';
+use AAAS::Frontend::User;
+
+our( $config, $dbUser);
+
+$config = Config::Auto::parse('AAAS.config');
+
+$dbUser = AAAS::Frontend::User->new('configs' => $config);
+
 my $daemon = SOAP::Transport::HTTP::Daemon
-  -> new (LocalPort => 2000)
-  -> dispatch_to('AAASServer')
+  -> new (LocalPort => $config->{'server_port'})
+  -> dispatch_to($dbUser)
 ;
 
 $daemon->handle;
-
-package AAASServer;
-
-use lib '../..';
-
-our @ISA = qw(Exporter);
-our @EXPORT = qw(Verify_login Get_profile Set_profile );
-
-use AAAS::Frontend::User;
-use AAAS::Frontend::Admin;
-
-sub Verify_login {
-  my ($class, $params) = @_;
-  return (verify_login($params));
-}
-
-
-sub Get_profile {
-  my ($class, $params, $fields_to_display) = @_;
-  return (get_profile($params, $fields_to_display));
-}
-
-
-sub Set_profile {
-  my ($class, $params) = @_;
-  return (set_profile($params));
-}
 
 
