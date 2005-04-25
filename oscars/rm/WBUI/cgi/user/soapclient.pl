4eb83e1580df7cf7ff26ce9c7d92a702df1ed813
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-04-25 18:34:07 +0000

    Adding fields_to_read argument to soap_get_reservations.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@489 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/rm/WBUI/cgi/user/soapclient.pl
+++ oscars/rm/WBUI/cgi/user/soapclient.pl
@@ -50,8 +50,8 @@ my $BSS_server = SOAP::Lite
 
 sub soap_get_reservations
 {
-    my ($params) = @_;
-    my $response = $BSS_server -> get_reservations($params);
+    my ($params, $fields_to_read) = @_;
+    my $response = $BSS_server -> get_reservations($params, $fields_to_read);
     if ($response->fault) {
         print $response->faultcode, " ", $response->faultstring, "\n";
     }
