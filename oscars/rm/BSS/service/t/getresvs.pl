d8ee6eb3d31a1ab9356ed719aa85b110d4183323
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-04-25 18:36:51 +0000

    Changed fields_to_display to fields_to_read.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@492 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/rm/BSS/service/t/getresvs.pl
+++ oscars/rm/BSS/service/t/getresvs.pl
@@ -4,10 +4,10 @@ require 'soapclient.pl';
 
 my($value);
 my %params = ('id' => 131 );   # FIX
-    # names of the fields to be displayed on the screen
-my @fields_to_display = ( 'start_time', 'end_time', 'created_time', 'bandwidth', 'resv_class', 'burst_limit', 'status', 'src_id', 'dst_id', 'dn', 'description' );
+    # names of the fields to be read and displayed on the screen
+my @fields_to_read = ( 'start_time', 'end_time', 'created_time', 'bandwidth', 'resv_class', 'burst_limit', 'status', 'src_id', 'dst_id', 'dn', 'description' );
 
-my($unused, %data) = soap_get_reservations(\%params, \@fields_to_display);
+my($unused, %data) = soap_get_reservations(\%params, \@fields_to_read);
 if (defined($data{'error_msg'}) && $data{'error_msg'})
 {
     print $data{'error_msg'}, "\n\n";
