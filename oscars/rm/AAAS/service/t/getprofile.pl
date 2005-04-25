54f8cebbf30a96abaebf48ebe8c8de9e5c6e0672
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-04-25 17:14:46 +0000

    More robust printing of error and status messages.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@486 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/rm/AAAS/service/t/getprofile.pl
+++ oscars/rm/AAAS/service/t/getprofile.pl
@@ -8,14 +8,21 @@ my %params = ('dn' => 'davidr' );
 my @fields_to_display = ( 'last_name', 'first_name', 'dn', 'email_primary', 'email_secondary', 'phone_primary', 'phone_secondary', 'description', 'level', 'register_time', 'activation_key', 'pending_level', 'authorization_id', 'institution_id' );
 
 my($unused, %results) = soap_get_profile(\%params, \@fields_to_display);
-print "Status:  ", $results{'status_msg'}, "\n";
-print "Returning:\n\n";
-foreach $key(sort keys %results)
+if (defined($results{'error_msg'}) && $results{'error_msg'})
 {
-    if ($key ne 'status_msg')
+    print $results{'error_msg'}, "\n\n";
+}
+elsif (defined($results{'status_msg'}))
+{
+    print "Status:  ", $results{'status_msg'}, "\n";
+    print "Returning:\n\n";
+    foreach $key(sort keys %results)
     {
-        $value = $results{$key};
-        if ($value) { print "$key -> $value\n"; }
-        else { print "$key -> \n"; }
+        if ($key ne 'status_msg')
+        {
+            $value = $results{$key};
+            if ($value) { print "$key -> $value\n"; }
+            else { print "$key -> \n"; }
+        }
     }
 }
--- oscars/rm/AAAS/service/t/login.pl
+++ oscars/rm/AAAS/service/t/login.pl
@@ -5,4 +5,11 @@ require 'soapclient.pl';
 my $passwd = crypt('shyysh', 'oscars');
 my %params = ('dn' => 'davidr', 'password' => $passwd);
 my($result, %data) = soap_verify_login(\%params);
-print $data{'status_msg'}, "\n\n";
+if (defined($data{'error_msg'}) && $data{'error_msg'})
+{
+    print $data{'error_msg'}, "\n\n";
+}
+elsif (defined($data{'status_msg'}))
+{
+    print $data{'status_msg'}, "\n\n";
+}
