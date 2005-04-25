83eb00d56c6ad75588b0c6136fff838ad07247a7
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-04-25 21:44:45 +0000

    Currently a ref to rows is used.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@496 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/rm/BSS/service/t/getresvs.pl
+++ oscars/rm/BSS/service/t/getresvs.pl
@@ -2,10 +2,12 @@
 
 require 'soapclient.pl';
 
+use Data::Dumper;
+
 my($value);
-my %params = ('id' => 131 );   # FIX
+my %params = ('dn' => 'oscars' );   # FIX
     # names of the fields to be read and displayed on the screen
-my @fields_to_read = ( 'start_time', 'end_time', 'created_time', 'bandwidth', 'resv_class', 'burst_limit', 'status', 'src_id', 'dst_id', 'dn', 'description' );
+my @fields_to_read = ( 'start_time', 'end_time', 'bandwidth', 'status', 'src_id', 'dst_id', 'dn' );
 
 my($unused, %data) = soap_get_reservations(\%params, \@fields_to_read);
 if (defined($data{'error_msg'}) && $data{'error_msg'})
@@ -14,15 +16,20 @@ if (defined($data{'error_msg'}) && $data{'error_msg'})
 }
 elsif (defined($data{'status_msg'}))
 {
-    print "Status:  ", $data{'status_msg'}, "\n";
-    print "Returning:\n\n";
-    foreach $key(sort keys %data)
+    my $rows = $data{'rows'};
+    my ($r, $f);
+    foreach $_ (@fields_to_read)
     {
-        if ($key ne 'status_msg')
-        {
-            $value = $data{$key};
-            if ($value) { print "$key -> $value\n"; }
-            else { print "$key -> \n"; }
-        }
+        print $_, ' ';
     }
+    print "\n\n";
+    foreach $r (@$rows)
+    {
+       foreach $f (@$r)
+       {
+           print $f, ' ';
+       }
+       print "\n";
+    }
+    
 }
