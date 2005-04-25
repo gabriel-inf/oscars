0aa65110484741843a19d417dd21e379049bed97
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-04-25 21:41:13 +0000

    A combination of reservation list and details currently working.  Will split off into two pages, the detailed one being accessible via a href on the tag name.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@493 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/rm/WBUI/cgi/user/reservationlist.pl
+++ oscars/rm/WBUI/cgi/user/reservationlist.pl
@@ -11,6 +11,8 @@ require '../lib/authenticate.pl';
 require 'soapclient.pl';
 
 use Data::Dumper;
+use DateTime;
+use Socket;
 
 
 # login URI
@@ -27,7 +29,7 @@ my %FormData;
 $FormData{'dn'} = 'oscars';
 
     # names of the fields to be read and displayed on the screen
-my @Fields_to_Read = ( 'start_time', 'end_time', 'created_time', 'bandwidth', 'resv_class', 'burst_limit', 'status', 'src_id', 'dst_id', 'dn', 'description' );
+my @Fields_to_Read = ( 'start_time', 'end_time', 'bandwidth', 'status', 'src_id', 'dst_id' );
 
 ( $Error_Status, %Results ) = soap_get_reservations(\%FormData, \@Fields_to_Read);
 
@@ -49,7 +51,9 @@ exit;
 sub print_reservation_detail
 {
   my ($results) = @_;
+  my ($arrayref, $row, $f, $fctr, $dt, $minute, $ipaddr, $host);
 
+  $arrayref = $results->{'rows'};
   print "Content-type: text/html\n\n";
   print "<html>\n";
   print "<head>\n";
@@ -67,22 +71,67 @@ sub print_reservation_detail
 
   print "<p><em>View Active Reservations</em><br>\n";
   print "<p>Click the Reservation ID number link to view detailed information about the reservation.\n";
+  print "(NOTE:  Currently this is mixed with the reservation details.)\n";
   print "</p>\n\n";
 
-  print "<table cellspacing=\"0\" id=\"reservationlist\">\n";
+  print "<table cellspacing=\"0\" width=\"90%\" id=\"reservationlist\">\n";
   print "  <thead>\n";
   print "  <tr>\n";
-  print "    <td>Reservation ID #</td>\n";
-  print "    <td>Login Name</td>\n";
-  print "    <td>Origin</td>\n";
-  print "    <td>Destination</td>\n";
-  print "    <td>Bandwidth</td>\n";
-  print "    <td>Start Time</td>\n";
-  print "    <td>End Time</td>\n";
+  print "    <td width=\"13%\">Start Time</td>\n";
+  print "    <td width=\"13%\">End Time</td>\n";
+  print "    <td width=\"4%\">Bandwidth</td>\n";
+  print "    <td width=\"12%\">Status</td>\n";
+  print "    <td width=\"29%\">Origin</td>\n";
+  print "    <td width=\"29%\">Destination</td>\n";
   print "  </tr>\n";
   print "  </thead>\n";
+
   print "  <tbody>\n";
+      # ordering, number of fields has to be same on client and server
+      # hash would be better, but haven't figured out how to read from db all
+      # at once into hash
+  foreach $row (@$arrayref)
+  {
+      if (@$row[3] ne 'finished')
+      {
+        print "  <tr>\n";
+        $fctr = 0;
+        foreach $f (@$row)
+        {
+            if ($fctr < 2)    # first two fields are starting and ending times
+            {
+              $dt = DateTime->from_epoch( epoch => $f );
+              $minute = $dt->minute();
+              if ($minute < 10)
+              {
+                  $minute = "0" . $minute;
+              }
+              print "      <td>" . $dt->month() . "-" . $dt->day() . " " . $dt->hour() . ":" . $minute . "</td>\n" 
+            }
+            elsif (($fctr == 4) || ($fctr == 5))  # IP's
+            {
+              $ipaddr = inet_aton($f);
+              $host = gethostbyaddr($ipaddr, AF_INET);
+              if ($host)
+              {
+                print "    <td>" . $host . "</td>\n"; 
+              }
+              else
+              {
+                print "    <td>" . $f . "</td>\n"; 
+              }
+            }
+            else
+            {
+              print "    <td>$f</td>\n";
+            }
+            $fctr += 1;
+        }
+        print "  </tr>\n";
+      }
+  }
   print "  </tbody>\n";
+
   print "</table>\n\n";
 
   print "<p>For inquiries, please contact the project administrator.</p>\n\n";
