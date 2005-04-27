fe099a4fef66260ac22733d6f0f7d1119c24eb44
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-04-27 23:37:22 +0000

    Authorization, status messages working better.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@526 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/rm/WBUI/cgi/lib/general.pl
+++ oscars/rm/WBUI/cgi/lib/general.pl
@@ -5,82 +5,49 @@
 # Soo-yeon Hwang (dapi@umich.edu)
 # David Robertson (dwrobertson@lbl.gov)
 
-##### Settings Begin (Global variables) #####
+
+use CGI;
+
+use AAAS::Client::SOAPClient;
+use AAAS::Client::Auth;
+use BSS::Client::SOAPClient;
+
+
 # contact info
 $webmaster = 'dwrobertson@lbl.gov';
 
-##### Settings End #####
+$login_URI = 'https://oscars.es.net/';
 
 
-##### List of sub routines #####
-### parse CGI form input
-# sub Parse_Form_Input_Data
+sub check_login {
+  my( $set_cookie, $cgi ) = @_;
+  my( $login_URI, $auth );
 
-### update status message, and replace main frame if necessary
-# sub Update_Frames
+  $auth = AAAS::Client::Auth->new();
+
+  if ( $set_cookie )
+  {
+      $auth->set_login_status($cgi);
+  }
+  elsif (!($auth->verify_login_status($cgi))) 
+  {
+      print "Location: $login_URI\n\n";
+      return ( "Please try a different login name." );
+  }
+  return ( '' );
+}
 
 
 
-##### sub Parse_Form_Input_Data
-# In: $Form_Method [all/post/get]
-# Out: %Form_Data (name-value pairs)
+##### sub update_status_frame
+##    Prints to the status frame, and sets location of the main frame if
+##    a URI is given.
+#
+##    In:  uri, error or status msg
 #####
-sub Parse_Form_Input_Data
+sub update_frames
 {
-
-  my $Form_Method = $_[0];
-	
-  my( $Form_Info, @Key_Value_Pairs, $Key, $Value, %Form_Data );
-
-  if ( $Form_Method eq 'all' ) {
-      if ( $ENV{'REQUEST_METHOD'} =~ /^POST$/i ) {
-          read( STDIN, $Form_Info, $ENV{'CONTENT_LENGTH'} );
-      }
-      elsif ( $ENV{'REQUEST_METHOD'} =~ /^GET$/i ) {
-          $Form_Info = $ENV{'QUERY_STRING'}; 
-      }
-  }
-  elsif ( $Form_Method eq 'post' ) {
-      read( STDIN, $Form_Info, $ENV{'CONTENT_LENGTH'} );
-  }
-  elsif ( $Form_Method eq 'get' ) {
-      $Form_Info = $ENV{'QUERY_STRING'}; 
-  }
-
-  @Key_Value_Pairs = split( /&/, $Form_Info );
-
-  foreach $_ ( @Key_Value_Pairs ) {
-      ( $Key, $Value ) = split( /=/, $_ );
-      #print STDERR '** ' . $Key . ' ' . $Value . "\n";
-      $Value =~ tr/+/ /;
-      $Value =~ s/%([\dA-Fa-f][\dA-Fa-f])/pack( "C", hex( $1 ) )/eg;
-
-      foreach ( $Value ) {
-          s/\r|\n//g;	# removes line feeds
-          s/^\s+|\s+$//g;	# removes leading/trailing spaces
-          s/&/&amp;/g;
-          s/&amp;\#([0-9]+);/&\#$1;/g;	# takes care of non-latin characters
-          s/</&lt;/g;
-          s/>/&gt;/g;
-          s/"/&quot;/g;
-      }
-
-      $Form_Data{$Key} = $Value;
-  }
-
-  return %Form_Data;
-} 
-
-
-##### sub Update_Frames
-##    Updates status portion of display (form target is status frame)
-##In:  URI of new frame if necessary, error or status msg
-# Result: update status message, sets main frame to another page if success
-#####
-sub Update_Frames
-{
-  my ($uri, $msg) = @_;
-  #print "Content-type: text/html\n\n";
+  my ($target, $uri, $msg) = @_;
   print "<html>\n";
   print "<head>\n";
   print "<link rel=\"stylesheet\" type=\"text/css\" ";
@@ -88,16 +55,22 @@ sub Update_Frames
   print "<script language=\"javascript\" type=\"text/javascript\" src=\"https://oscars.es.net/main_common.js\"></script>\n";
   print "</head>\n";
   print "<body>\n";
-  print "<div>\n";
-  print "<p class=\"topmessage\"><script language=\"javascript\">print_current_date(\"local\");</script>" . " | " . $msg . "</p>\n";
-  print "</div>\n";
-  if ($uri)
-  {
-      print "<script language=\"javascript\">update_main_frame(\"$uri\");</script>";
+  print "<script language=\"javascript\">update_status_message(\"$target\", \"$msg\");</script>";
+  if ($uri) {
+    print "<script language=\"javascript\">update_main_frame(\"$uri\", \"$msg\");</script>";
   }
   print "</body>\n";
   print "</html>\n";
   print "\n\n";
+}
+
+
+sub end_session {
+  my( $cgi ) = @_;
+  my( $auth );
+
+  $auth = AAAS::Client::Auth->new();
+  $auth->logout($cgi);
 }
 
 
