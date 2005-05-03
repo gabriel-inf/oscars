26272e8dff444f347a2ace344c522dabaee06700
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-05-03 00:35:21 +0000

    Added call for getting reservation detail.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@575 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/rm/BSS/Client/SOAPClient.pm
+++ oscars/rm/BSS/Client/SOAPClient.pm
@@ -32,6 +32,19 @@ sub soap_get_reservations
 }
 
 
+sub soap_get_resv_detail
+{
+    my ($params, $fields_to_read) = @_;
+
+    my $response = $BSS_server -> get_reservation_detail($params, $fields_to_read);
+    if ($response->fault) {
+        print $response->faultcode, " ", $response->faultstring, "\n";
+    }
+        #  params are either user profile, or error message
+    return ($response->result(), $response->paramsout());
+}
+
+
 sub soap_create_reservation
 {
     my ($params) = @_;
