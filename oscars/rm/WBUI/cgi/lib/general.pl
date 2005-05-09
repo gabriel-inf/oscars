f6bfe625cb7611e696900029de39bb0ff20aa80b
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-05-09 21:32:20 +0000

    Spaces cleanup.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@596 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/rm/WBUI/cgi/lib/general.pl
+++ oscars/rm/WBUI/cgi/lib/general.pl
@@ -5,7 +5,6 @@
 # Soo-yeon Hwang (dapi@umich.edu)
 # David Robertson (dwrobertson@lbl.gov)
 
-
 use CGI;
 use DateTime;
 use Socket;
@@ -24,22 +23,18 @@ $login_URI = 'https://oscars.es.net/';
 # checks session status only; database check done elsewhere
 
 sub check_login {
-  my( $set_cookie, $cgi ) = @_;
-  my( $auth );
+    my( $set_cookie, $cgi ) = @_;
+    my( $auth );
 
-  $auth = AAAS::Client::Auth->new();
-
-  if ( $set_cookie )
-  {
-      $auth->set_login_status($cgi);
-  }
-  elsif (!($auth->verify_login_status($cgi))) 
-  {
-      return ( 1 );
-  }
-  return ( 0 );
+    $auth = AAAS::Client::Auth->new();
+    if ( $set_cookie ) {
+        $auth->set_login_status($cgi);
+    }
+    elsif (!($auth->verify_login_status($cgi))) {
+        return ( 1 );
+    }
+    return ( 0 );
 }
-
 
 
 ##### sub update_status_frame
@@ -50,83 +45,78 @@ sub check_login {
 #####
 sub update_frames
 {
-  my ($error_made, $target, $uri, $msg) = @_;
-  if ($error_made) { print "Content-type: text/html\n\n"; }
-  print "<html>\n";
-  print "<head>\n";
-  print "<link rel=\"stylesheet\" type=\"text/css\" ";
-  print " href=\"https://oscars.es.net/styleSheets/layout.css\">\n";
-  print "<script language=\"javascript\" type=\"text/javascript\" src=\"https://oscars.es.net/main_common.js\"></script>\n";
-  print "</head>\n";
-  print "<body>\n";
-  print "<script language=\"javascript\">update_status_message(\"$target\", \"$msg\");</script>";
-  if ($uri) {
-    print "<script language=\"javascript\">update_frame(\"main_frame\", \"$uri\");</script>";
-  }
-  print "</body>\n";
-  print "</html>\n";
-  print "\n\n";
+    my ($error_made, $target, $uri, $msg) = @_;
+    if ($error_made) { print "Content-type: text/html\n\n"; }
+    print "<html>\n";
+    print "<head>\n";
+    print "<link rel=\"stylesheet\" type=\"text/css\" ";
+    print " href=\"https://oscars.es.net/styleSheets/layout.css\">\n";
+    print "<script language=\"javascript\" type=\"text/javascript\" src=\"https://oscars.es.net/main_common.js\"></script>\n";
+    print "</head>\n";
+    print "<body>\n";
+    print "<script language=\"javascript\">update_status_message(\"$target\", \"$msg\");</script>";
+    if ($uri) {
+        print "<script language=\"javascript\">update_frame(\"main_frame\", \"$uri\");</script>";
+    }
+    print "</body>\n";
+    print "</html>\n";
+    print "\n\n";
 }
 
 
 sub end_session {
-  my( $cgi ) = @_;
-  my( $auth );
+    my( $cgi ) = @_;
+    my( $auth );
 
-  $auth = AAAS::Client::Auth->new();
-  $auth->logout($cgi);
+    $auth = AAAS::Client::Auth->new();
+    $auth->logout($cgi);
 }
 
 
 sub get_time_str 
 {
-  my( $epoch_seconds ) = @_;
+    my( $epoch_seconds ) = @_;
 
-  my $dt = DateTime->from_epoch( epoch => $epoch_seconds );
-  my $year = $dt->year();
-  if ($year < 10)
-  {
-      $year = "0" . $year;
-  }
-  my $month = $dt->month();
-  if ($month < 10)
-  {
-      $month = "0" . $month;
-  }
-  my $day = $dt->day();
-  if ($day < 10)
-  {
-      $day = "0" . $day;
-  }
-  my $hour = $dt->hour();
-  if ($hour < 10)
-  {
-      $hour = "0" . $hour;
-  }
-  $minute = $dt->minute();
-  if ($minute < 10)
-  {
-      $minute = "0" . $minute;
-  }
-  my $time_tag = $year . $month . $day;
-  my $time_field = $month . "-" . $day . "&nbsp;&nbsp; " . $hour . ":" . $minute;
+    my $dt = DateTime->from_epoch( epoch => $epoch_seconds );
+    my $year = $dt->year();
+    if ($year < 10) {
+        $year = "0" . $year;
+    }
+    my $month = $dt->month();
+    if ($month < 10) {
+        $month = "0" . $month;
+    }
+    my $day = $dt->day();
+    if ($day < 10) {
+        $day = "0" . $day;
+    }
+    my $hour = $dt->hour();
+    if ($hour < 10) {
+        $hour = "0" . $hour;
+    }
+    $minute = $dt->minute();
+    if ($minute < 10) {
+        $minute = "0" . $minute;
+    }
+    my $time_tag = $year . $month . $day;
+    my $time_field = $month . "-" . $day . "&nbsp;&nbsp; " . $hour . ":" . $minute;
 
-  return ( $time_tag, $time_field );
+    return ( $time_tag, $time_field );
 }
 
 
 sub get_oscars_host
 {
-  my( $input ) = @_;
+    my( $input ) = @_;
 
-  my $ipaddr = inet_aton($input);
-  my $host = gethostbyaddr($ipaddr, AF_INET);
-  if ($host) {
-      return($host); 
-  }
-  else {
-      return($input); 
-  }
+    my $ipaddr = inet_aton($input);
+    my $host = gethostbyaddr($ipaddr, AF_INET);
+    if ($host) {
+        return($host); 
+    }
+    else {
+        return($input); 
+    }
 }
 
 
