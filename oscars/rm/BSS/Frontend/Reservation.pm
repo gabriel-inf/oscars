1b4c5b83468d01d9f087be394e0a0ea7bfe347ff
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-05-10 17:27:16 +0000

    No longer using convenience methods; spacing changes.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@603 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/rm/BSS/Frontend/Reservation.pm
+++ oscars/rm/BSS/Frontend/Reservation.pm
@@ -1,7 +1,7 @@
 package BSS::Frontend::Reservation;
 
 # Reservation.pm:
-# Last modified: April 22, 2005
+# Last modified: May 9, 2005
 # Soo-yeon Hwang (dapi@umich.edu)
 # David Robertson (dwrobertson@lbl.gov)
 
@@ -13,16 +13,16 @@ use Data::Dumper;
 
 ######################################################################
 sub new {
-  my ($_class, %_args) = @_;
-  my ($_self) = {%_args};
+    my ($_class, %_args) = @_;
+    my ($_self) = {%_args};
   
-  # Bless $_self into designated class.
-  bless($_self, $_class);
+    # Bless $_self into designated class.
+    bless($_self, $_class);
   
-  # Initialize.
-  $_self->initialize();
+    # Initialize.
+    $_self->initialize();
   
-  return($_self);
+    return($_self);
 }
 
 ######################################################################
@@ -32,6 +32,8 @@ sub initialize {
     $self->{'dbconn'} = BSS::Frontend::Database->new('configs' => $self->{'configs'}) or die "FATAL:  could not connect to database";
 }
 
+
+### Following methods called from SchedulerThread.
 
 ################################
 ### insert_reservation
@@ -46,79 +48,88 @@ sub initialize {
 
 sub insert_reservation
 {
-  my( $self, $inref ) = @_;
-  my( $query, $sth );
-  my( %results );
+    my( $self, $inref ) = @_;
+    my( $query, $sth );
+    my( %results );
 
-  my $over_limit = 0; # whether any time segment is over the bandwidth limit
-  my( %table ) = $self->{'dbconn'}->get_BSS_table('reservations');
+    if (!($self->{'dbconn'}->{'dbh'})) { return( 1, "Database connection not valid\n"); }
+
+    my $over_limit = 0; # whether any time segment is over the bandwidth limit
+    my( %table ) = $self->{'dbconn'}->get_BSS_table('reservations');
 
     ###
     # Get bandwidth and times of reservations overlapping that of the
     # reservation request.
-  $query = "SELECT $table{'reservations'}{'bandwidth'}, $table{'reservations'}{'start_time'}, $table{'reservations'}{'end_time'} FROM reservations WHERE ( $table{'reservations'}{'end_time'} >= ? AND $table{'reservations'}{'start_time'} <= ? )";
+    $query = "SELECT $table{'reservations'}{'bandwidth'}, $table{'reservations'}{'start_time'}, $table{'reservations'}{'end_time'} FROM reservations WHERE ( $table{'reservations'}{'end_time'} >= ? AND $table{'reservations'}{'start_time'} <= ? )";
 
-  $inref->{'created_time'} = '';  # only holds a time if reservation successful
+    $inref->{'created_time'} = '';  # only holds a time if reservation successful
 
-      # handled query with the comparison start & end datetime strings
-  ( $results{'error_msg'}, $sth) = $self->{'dbconn'}->handle_query($query, 'reservations', $inref->{'start_time'}, $inref->{'end_time'});
-  if ( $results{'error_msg'} ) { return( 1, %results ); }
+    # handled query with the comparison start & end datetime strings
+    $sth = $self->{'dbconn'}->{'dbh'}->prepare( $query );
+    if (!$sth) {
+        $results{'error_msg'} = "Can't prepare statement:  $DBI::errstr";
+        return ( 1, %results );
+    }
+    $sth->execute( $inref->{'start_time'}, $inref->{'end_time'} );
+    if ( $sth->errstr ) {
+        $sth->finish();
+        $results{'error_msg'} = "[ERROR] While creating a new reservation:  $DBI::errstr";
+        return( 1, %results );
+    }
 
-  # TODO:  find segments of overlap, determine if bandwidth in any is
-  #        over the limit; return time segments if error
+    # TODO:  find segments of overlap, determine if bandwidth in any is
+    #        over the limit; return time segments if error
 
     # If no segment is over the limit,  record the reservation to the database.
     # otherwise, return error message (TODO) with the times involved.
 
-  #$results{'id'} unless ( $over_limit );
-  if ( $over_limit ) {
-      $self->{'dbconn'}->handle_finish( 'reservations');
-          # TODO:  list of times
-      results{'error_msg'} = '[ERROR] The available bandwidth limit on the network has been reached between '. 'Please modify your reservation request and try again.';
-      return( 1, %results );
-  }
-  else {
-      $self->{'dbconn'}->query_finish();
-      $self->{'dbconn'}->unlock_table( 'reservations' );
-      #print STDERR Dumper($inref);
+    #$results{'id'} unless ( $over_limit );
+    if ( $over_limit ) {
+        $sth->finish();
+        # TODO:  list of times
+        results{'error_msg'} = '[ERROR] The available bandwidth limit on the network has been reached between '. 'Please modify your reservation request and try again.';
+        return( 1, %results );
+    }
+    else {
+        $sth->finish();
 
-      if (($inref->{'ingress_router'} == 0) || ($inref->{'egress_router'} == 0))
-      {
-          $results{'error_msg'} = 'Invalid router id(s): 0.  Unable to do insert.';
-          return( 1, %results );
-      }
-          # get interface id's from edge router ip's
-      $inref->{'ingress_id'} = $inref->{'ingress_router'}; 
-      $inref->{'egress_id'} = $inref->{'egress_router'}; 
+        if (($inref->{'ingress_id'} == 0) || ($inref->{'egress_id'} == 0))
+        {
+            $results{'error_msg'} = 'Invalid router id(s): 0.  Unable to do insert.';
+            return( 1, %results );
+        }
 
-          # get ipaddr id from host's and destination's ip addresses
-      $inref->{'src_id'} = $self->{'dbconn'}->hostaddr_to_idx($inref->{'src_ip'}); 
-      $inref->{'dst_id'} = $self->{'dbconn'}->hostaddr_to_idx($inref->{'dst_ip'}); 
-      $inref->{'created_time'} = time();
+        # get ipaddr id from host's and destination's ip addresses
+        $inref->{'src_id'} = $self->{'dbconn'}->hostaddrs_ip_to_id($inref->{'src_ip'}); 
+        $inref->{'dst_id'} = $self->{'dbconn'}->hostaddrs_ip_to_id($inref->{'dst_ip'}); 
+        $inref->{'created_time'} = time();
 
-      my @insertions;   # copy over input fields that will be set in table
-      my @resv_field_order = $self->{'dbconn'}->get_resv_field_order();
-      foreach $_ ( @resv_field_order ) {
-         $results{$_} = $inref->{$_};
-         push(@insertions, $inref->{$_}); 
-      }
+        my @insertions;   # copy over input fields that will be set in table
+        my @resv_field_order = $self->{'dbconn'}->get_resv_field_order();
+        foreach $_ ( @resv_field_order ) {
+           $results{$_} = $inref->{$_};
+           push(@insertions, $inref->{$_}); 
+        }
 
         # insert all fields for reservation into database
-      $query = "INSERT INTO reservations VALUES ( " . join( ', ', ('?') x @insertions ) . " )";
+        $query = "INSERT INTO reservations VALUES ( " . join( ', ', ('?') x @insertions ) . " )";
 
-      ( $results{'error_msg'}, $sth) = $self->{'dbconn'}->handle_query($query, 'reservations', @insertions);
-      if ( $results{'error_msg'} )
-      {
-          $results{'error_msg'} =~ "s/CantExecuteQuery\n//";
-          $results{'error_msg'} = '[ERROR] An error has occurred while recording the reservation request on the database.<br>[Error] ' . $results{'error_msg'};
-           return( 1, %results );
-      }
-		
-      $results{'id'} = $self->{'dbconn'}->get_id();
-  }
-  $self->{'dbconn'}->handle_finish( 'reservations' );
-  $results{'status_msg'} = 'Your reservation has been processed successfully. Your reservation ID number is ' . $results{'id'} . '.';
-  return( 0, %results );
+        $sth = $self->{'dbconn'}->{'dbh'}->prepare( $query );
+        if (!$sth) {
+            $results{'error_msg'} = "Can't prepare statement:  $DBI::errstr";
+            return (1, %results);
+        }
+        $sth->execute( @insertions );
+        if ( $sth->errstr ) {
+            $sth->finish();
+            $results{'error_msg'} = "[ERROR] While recording the reservation request on the database: $DBI::errstr";
+            return( 1, %results );
+        }
+        $results{'id'} = $self->{'dbconn'}->{'dbh'}->{'mysql_insertid'};
+    }
+    $sth->finish();
+    $results{'status_msg'} = 'Your reservation has been processed successfully. Your reservation ID number is ' . $results{'id'} . '.';
+    return( 0, %results );
 }
 
 
@@ -130,59 +141,77 @@ sub insert_reservation
 # Out: success or failure, and status message
 sub get_reservations
 {
-  my( $self, $inref, $fields_to_read ) = @_;
-  my( $sth, $query );
-  my( %mapping, @field_names, $rref, $arrayref, $r, %results );
+    my( $self, $inref, $fields_to_read ) = @_;
+    my( $sth, $query );
+    my( %mapping, @field_names, $rref, $arrayref, $r, %results );
 
-  my( %table ) = $self->{'dbconn'}->get_BSS_table('reservations');
+    if (!($self->{'dbconn'}->{'dbh'})) { return( 1, "Database connection not valid\n"); }
+
+    my( %table ) = $self->{'dbconn'}->get_BSS_table('reservations');
     # DB query: get the reservation list
     # TODO:  selectall
 
-  foreach $_ ( @$fields_to_read ) {
-      push (@field_names, $table{'reservations'}{$_});
-  }
+    foreach $_ ( @$fields_to_read ) {
+        push (@field_names, $table{'reservations'}{$_});
+    }
 
-  $query = "SELECT ";
-  foreach $_ ( @field_names ) {
-      $query .= $_ . ", ";
-  }
+    $query = "SELECT ";
+    foreach $_ ( @field_names ) {
+        $query .= $_ . ", ";
+    }
     # delete the last ", "
-  $query =~ s/,\s$//;
+    $query =~ s/,\s$//;
     # sort by start time in ascending order
-  $query .= " FROM reservations WHERE $table{'reservations'}{'dn'} = ? ORDER BY $table{'reservations'}{'start_time'}";
+    $query .= " FROM reservations WHERE $table{'reservations'}{'dn'} = ? ORDER BY $table{'reservations'}{'start_time'}";
 
-  ( $results{'error_msg'}, $sth) = $self->{'dbconn'}->handle_query($query, 'reservations', $inref->{'dn'});
-  if ( $results{'error_msg'} ) { return(1, %results ); }
+    $sth = $self->{'dbconn'}->{'dbh'}->prepare( $query );
+    if (!$sth) {
+        $results{'error_msg'} = "Can't prepare statement:  $DBI::errstr";
+        return (1, %results);
+    }
+    $sth->execute( $inref->{'dn'} );
+    if ( $sth->errstr ) {
+        $sth->finish();
+        $results{'error_msg'} = "[ERROR] While getting reservation list: $DBI::errstr";
+        return( 1, %results );
+    }
 
-  $rref = $sth->fetchall_arrayref({user_dn => 1, reservation_start_time => 2, reservation_end_time => 3, reservation_status => 4, src_hostaddrs_id => 5, dst_hostaddrs_id => 6, reservation_id => 7 });
-  $self->{'dbconn'}->query_finish();
-  $self->{'dbconn'}->unlock_table( 'reservations' );
+    $rref = $sth->fetchall_arrayref({user_dn => 1, reservation_start_time => 2, reservation_end_time => 3, reservation_status => 4, src_hostaddrs_id => 5, dst_hostaddrs_id => 6, reservation_id => 7 });
+    $sth->finish();
 
-  $query = "SELECT hostaddrs_id, hostaddrs_ip FROM hostaddrs";
-  ( $results{'error_msg'}, $sth) = $self->{'dbconn'}->handle_query($query, 'hostaddrs');
-  if ( $results{'error_msg'} ) { return(1, %results ); }
+    $query = "SELECT hostaddrs_id, hostaddrs_ip FROM hostaddrs";
+    $sth = $self->{'dbconn'}->{'dbh'}->prepare( $query );
+    if (!$sth) {
+        $results{'error_msg'} = "Can't prepare statement:  $DBI::errstr";
+        return (1, %results);
+    }
+    $sth->execute();
+    if ( $sth->errstr ) {
+        $sth->finish();
+        $results{'error_msg'} = "[ERROR] While getting reservation list: $DBI::errstr";
+        return( 1, %results );
+    }
+    $arrayref = $sth->fetchall_arrayref();
+    foreach $r (@$arrayref) { $mapping{$$r[0]} = $$r[1]; }
 
-  $arrayref = $sth->fetchall_arrayref();
-  foreach $r (@$arrayref)
-  {
-      $mapping{$$r[0]} = $$r[1];
-  }
-  foreach $r (@$rref)
-  {
-      $r->{'src_hostaddrs_id'} = $mapping{$r->{'src_hostaddrs_id'}};
-      $r->{'dst_hostaddrs_id'} = $mapping{$r->{'dst_hostaddrs_id'}};
-  }
-  $results{'rows'} = $rref;
+    foreach $r (@$rref) {
+        $r->{'src_hostaddrs_id'} = $mapping{$r->{'src_hostaddrs_id'}};
+        $r->{'dst_hostaddrs_id'} = $mapping{$r->{'dst_hostaddrs_id'}};
+    }
+    $results{'rows'} = $rref;
 
-  $self->{'dbconn'}->handle_finish( 'hostaddrs' );
-  $results{'status_msg'} = 'Successfully read reservations';
-  return( 0, %results );
+    $sth->finish();
+    $results{'status_msg'} = 'Successfully read reservations';
+    return( 0, %results );
 }
 
 
     # stub
 sub delete_reservation
 {
+    my( $self, $inref ) = @_;
+
+    if (!($self->{'dbconn'}->{'dbh'})) { return( 1, "Database connection not valid\n"); }
 }
 
 
@@ -192,47 +221,158 @@ sub delete_reservation
 # Out: success or failure, and status message
 sub get_reservation_detail
 {
-  my( $self, $inref, $fields_to_display ) = @_;
-  my( $sth, $query );
-  my( %mapping, $r, $arrayref, %results );
+    my( $self, $inref, $fields_to_display ) = @_;
+    my( $sth, $query );
+    my( %mapping, $r, $arrayref, %results );
 
-  my( %table ) = $self->{'dbconn'}->get_BSS_table('reservations');
+    if (!($self->{'dbconn'}->{'dbh'})) { return( 1, "Database connection not valid\n"); }
+
+    my( %table ) = $self->{'dbconn'}->get_BSS_table('reservations');
 
     # DB query: get the user profile detail
-  $query = "SELECT ";
-  foreach $_ ( @$fields_to_display ) {
-      $query .= $table{'reservations'}{$_} . ", ";
-  }
+    $query = "SELECT ";
+    foreach $_ ( @$fields_to_display ) {
+        $query .= $table{'reservations'}{$_} . ", ";
+    }
     # delete the last ", "
-  $query =~ s/,\s$//;
-  $query .= " FROM reservations WHERE $table{'reservations'}{'id'} = ?";
-  ( $results{'error_msg'}, $sth) = $self->{'dbconn'}->handle_query($query, 'reservations', $inref->{'id'});
-  #print STDERR "query: ", $query, " id: ", $inref->{'id'}, "\n";
-
-  if ( $results{'error_msg'} ) { return(1, %results ); }
+    $query =~ s/,\s$//;
+    $query .= " FROM reservations WHERE $table{'reservations'}{'id'} = ?";
+    $sth = $self->{'dbconn'}->{'dbh'}->prepare( $query );
+    if (!$sth) {
+        $results{'error_msg'} = "Can't prepare statement:  $DBI::errstr";
+        return (1, %results);
+    }
+    $sth->execute( $inref->{'id'} );
+    if ( $sth->errstr ) {
+        $sth->finish();
+        $results{'error_msg'} = "[ERROR] While getting reservation details: $DBI::errstr";
+        return( 1, %results );
+    }
 
     # populate %results with the data fetched from the database
-  @results{@$fields_to_display} = ();
-  $sth->bind_columns( map { \$results{$_} } @$fields_to_display );
-  $sth->fetch();
-  $self->{'dbconn'}->query_finish();
-  $self->{'dbconn'}->unlock_table( 'reservations' );
+    @results{@$fields_to_display} = ();
+    $sth->bind_columns( map { \$results{$_} } @$fields_to_display );
+    $sth->fetch();
+    $sth->finish();
 
-  $query = "SELECT hostaddrs_id, hostaddrs_ip FROM hostaddrs";
-  ( $results{'error_msg'}, $sth) = $self->{'dbconn'}->handle_query($query, 'hostaddrs');
-  if ( $results{'error_msg'} ) { return(1, %results ); }
+    $query = "SELECT hostaddrs_id, hostaddrs_ip FROM hostaddrs";
+    $sth = $self->{'dbconn'}->{'dbh'}->prepare( $query );
+    if (!$sth) {
+        $results{'error_msg'} = "[ERROR] Can't prepare statement: $DBI::errstr";
+        return (1, %results);
+    }
+    $sth->execute();
+    if ( $sth->errstr ) {
+        $sth->finish();
+        $results{'error_msg'} = "[ERROR] While getting reservation details: $DBI::errstr";
+        return( 1, %results );
+    }
+    $arrayref = $sth->fetchall_arrayref();
+    foreach $r (@$arrayref) { $mapping{$$r[0]} = $$r[1]; }
 
-  $arrayref = $sth->fetchall_arrayref();
-  foreach $r (@$arrayref)
-  {
-      $mapping{$$r[0]} = $$r[1];
-  }
-  $results{'src_ip'} = $mapping{$results{'src_id'}};
-  $results{'dst_ip'} = $mapping{$results{'dst_id'}};
+    $results{'src_ip'} = $mapping{$results{'src_id'}};
+    $results{'dst_ip'} = $mapping{$results{'dst_id'}};
 
-  $self->{'dbconn'}->handle_finish( 'hostaddrs' );
-  $results{'status_msg'} = 'Successfully got reservation details.';
-  return (0, %results);
+    $sth->finish();
+    $results{'status_msg'} = 'Successfully got reservation details.';
+    return (0, %results);
 }
+
+
+### Following methods called from SchedulerThread.
+
+######################################################################
+sub find_pending_reservations  {
+
+    my ( $self, $stime, $status ) = @_;
+    my ( $sth, $data, $query, $error_msg );
+
+    if (!($self->{'dbconn'}->{'dbh'})) { return( 1, "Database connection not valid\n"); }
+
+    $query = qq{ SELECT * FROM reservations WHERE reservation_status = ? and reservation_start_time < ?};
+    $sth = $self->{'dbconn'}->{'dbh'}->prepare($query);
+    if (!$sth) {
+        $error_msg = "[ERROR] Can't prepare statement: $DBI::errstr";
+        return ( 1, $error_msg);
+    }
+
+    $sth->execute( $status, $stime );
+    if ( $sth->errstr ) {
+        $sth->finish();
+        $error_msg = "[ERROR] While finding pending reservations: $DBI::errstr";
+        return( $error_msg, undef );
+    }
+
+    # get all the data
+    $data = $sth->fetchall_arrayref({});
+    # close it up
+    $sth->finish();
+
+    return( "", $data );
+}
+
+
+######################################################################
+sub find_expired_reservations  {
+
+    my ( $self, $stime, $status ) = @_;
+    my ( $sth, $data, $query, $error_msg);
+
+    if (!($self->{'dbconn'}->{'dbh'})) { return( 1, "Database connection not valid\n"); }
+
+    #print "expired: Looking at time == " . $stime . "\n";
+
+    $query = qq{ SELECT * FROM reservations WHERE reservation_status = ? and reservation_end_time < ?};
+    $sth = $self->{'dbconn'}->{'dbh'}->prepare($query) ;
+    if (!$sth) {
+        $error_msg = "[ERROR] Can't prepare statement: $DBI::errstr";
+        return ( 1, $error_msg);
+    }
+
+    $sth->execute( $status, $stime );
+    if ( $sth->errstr ) {
+        $sth->finish();
+        $error_msg = "[ERROR] While finding expired reservations: $DBI::errstr";
+        return( $error_msg, undef );
+    }
+
+    # get all the data
+    $data = $sth->fetchall_arrayref({});
+
+    # close it up
+    $sth->finish();
+
+    # return the answer
+    return( "", $data );
+}
+
+
+######################################################################
+sub update_reservation {
+
+    my ( $self, $res_id, $status ) = @_;
+    my ( $sth, $query, %results );
+
+    if (!($self->{'dbconn'}->{'dbh'})) { return( 1, "Database connection not valid\n"); }
+
+    $query = qq{ UPDATE reservations SET reservation_status = ? WHERE reservation_id = ?};
+    $sth = $self->{'dbconn'}->{'dbh'}->prepare($query);
+    if (!$sth) {
+        $results{'error_msg'} = "Can't prepare statement:  $DBI::errstr";
+        return ( 1, %results );
+    }
+    $sth->execute( $status, $res_id->{reservation_id});
+    if ( $sth->errstr ) {
+        $sth->finish();
+        $results{'error_msg'} = "[ERROR] While updating your reservation: $DBI::errstr";
+        return( 1, %results );
+    }
+
+    # close it up
+    $sth->finish();
+    $results{'status_msg'} = "Successfully updated reservation.";
+    return( 0, %results );
+}
+
 
 1;
