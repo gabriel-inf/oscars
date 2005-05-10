8189fcc4f80fcf4c8a06e226d57f6759fd1dd854
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-05-10 17:27:51 +0000

    Minor changes.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@604 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/rm/BSS/service/t/create_resv.pl
+++ oscars/rm/BSS/service/t/create_resv.pl
@@ -13,28 +13,26 @@ $params{'id'} =              'NULL';
 
     # in seconds since epoch
 my $dt = DateTime->now();
-$params{'start_time'} =     $dt->epoch - 1200;   # - 20 minutes
+$params{'start_time'} =     $dt->epoch - 60;   # - 1 minute
 $params{'end_time'} =       $dt->epoch + 120;    # + 5 minutes
 
 $params{'created_time'} =   '';   # filled in scheduler
-$params{'bandwidth'}=       '10m';
-$params{'class'}=           'test';
-$params{'burst_limit'}=     '1m';
+$params{'bandwidth'} =      '10m';
+$params{'class'} =          '4';
+$params{'burst_limit'} =    '1m';
 $params{'status'} =         'pending';
 
-$params{'ingress_interface_id'}= '';   # db lookup in scheduler
-$params{'egress_interface_id'}=  '';   # db lookup in scheduler
+$params{'ingress_id'}= '';   # db lookup in scheduler
+$params{'egress_id'}=  '';   # db lookup in scheduler
 
 $params{'src_ip'} = '192.168.2.2';
 $params{'dst_ip'} = '192.168.0.2';
 
 $params{'dn'} =        'oscars';
+$params{'dscp'} =      'ef';
 
 $params{'ingress_port'} =   '';     # db lookup in schedule
 $params{'egress_port'} =    '';     # db lookup in scheduler
-
-$params{'dscp'} =           '';     # optional
-
 $params{'description'} =    'This is a test.';
 
 my($result);
