d0459e2057171db5cf2f169aea82d55a8ae48c2f
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-05-13 00:19:56 +0000

    Going back to previous way.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@662 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/rm/AAAS/service/AAAS_server.pl
+++ oscars/rm/AAAS/service/AAAS_server.pl
@@ -5,60 +5,13 @@ use strict;
 use Config::Auto;
 use SOAP::Transport::HTTP;
 
+use AAAS::Frontend::User;
+
 my $config = Config::Auto::parse('AAAS.config');
+
+my $dbUser = AAAS::Frontend::User->new('configs' => $config);
 
 my $daemon = SOAP::Transport::HTTP::Daemon
   -> new (LocalPort => $config->{'server_port'})
-  -> dispatch_to('AAAS_Dispatcher')
-;
-
-
-# dispatches to user or admin, depending on method call
-
-package AAAS_Dispatcher;
-
-use AAAS::Frontend::User;
-use AAAS::Frontend::Admin;
-
-my $dispatch_config = Config::Auto::parse('AAAS.config');
-my $dbUser = AAAS::Frontend::User->new('configs' => $dispatch_config);
-my $dbAdmin = AAAS::Frontend::Admin->new('configs' => $dispatch_config);
-
-# user
-
-sub verify_login
-{
-   my ($class, $params) = @_;
-   $dbUser->verify_login($params);
-}
-
-sub get_profile
-{
-   my ($class, $params, $fields_to_display) = @_;
-   $dbUser->get_profile($params, $fields_to_display);
-}
-
-sub set_profile
-{
-   my ($class, $params) = @_;
-   $dbUser->set_profile($params);
-}
-
-sub logout
-{
-   my ($class) = @_;
-   $dbUser->logout();
-}
-
-# admin
-
-sub get_userlist
-{
-   my ($class, $params) = @_;
-   $dbAdmin->get_userlist($params);
-}
-
-
-$daemon->handle;
-
-
+  -> dispatch_to($dbUser)
+  -> handle;
