1becc41f2604fa060634f1545fa083c707105c91
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-05-14 00:09:02 +0000

    Having to do with consolidating all config files into rm/oscars.cfg
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@674 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/rm/AAAS/service/AAAS_server.pl
+++ oscars/rm/AAAS/service/AAAS_server.pl
@@ -7,11 +7,11 @@ use SOAP::Transport::HTTP;
 
 use AAAS::Frontend::User;
 
-my $config = Config::Auto::parse('AAAS.config');
+my $config = Config::Auto::parse($ENV{'OSCARS_HOME'} . '/oscars.cfg');
 
 my $dbUser = AAAS::Frontend::User->new('configs' => $config);
 
 my $daemon = SOAP::Transport::HTTP::Daemon
-  -> new (LocalPort => $config->{'server_port'})
+  -> new (LocalPort => $config->{'AAAS_server_port'})
   -> dispatch_to($dbUser)
   -> handle;
