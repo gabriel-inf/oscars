6d0e055b99c61b6deb13f200337f136dfa2094b9
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-05-26 01:28:02 +0000

    Different page gone to depending on whether was logged in as administrator or not.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@740 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/rm/WBUI/cgi/lib/logout.pl
+++ oscars/rm/WBUI/cgi/lib/logout.pl
@@ -11,13 +11,20 @@ require 'general.pl';
 
 
 my $cgi = CGI->new();
+my $login_frame;
 my ($dn, $user_level, $admin_required) = check_session_status(undef, $cgi);
+if (!$admin_required) {
+    $login_frame = "https://oscars.es.net/login_frame.html";
+}
+else {
+    $login_frame = "https://oscars.es.net/admin/login_frame.html";
+}
 
 # nuke session and put the user back at the login screen
 
 if ($dn) { end_session($cgi); }
 
-update_frames(0, "main_frame", "https://oscars.es.net/login_frame.html", "Please log in.");
+update_frames(0, "main_frame", $login_frame, "Please log in.");
 
 exit;
 
