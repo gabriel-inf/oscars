769c5b4bbfa8fe689a07d4214bd1578054e0807e
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-05-26 01:35:33 +0000

    Getting reservation_tag now from db.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@751 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/rm/BSS/service/t/get_resv_detail.pl
+++ oscars/rm/BSS/service/t/get_resv_detail.pl
@@ -4,9 +4,9 @@ use BSS::Client::SOAPClient;
 use Data::Dumper;
 
 my($value);
-my %params = ('reservation_id' => '61' );   # FIX
+my %params = ('reservation_id' => '1' );   # FIX
     # names of the fields to be read and displayed on the screen
-my @fields_to_read = ( 'reservation_start_time', 'reservation_end_time', 'reservation_created_time', 'reservation_bandwidth', 'reservation_burst_limit', 'reservation_status', 'src_hostaddrs_id', 'dst_hostaddrs_id', 'reservation_description' );
+my @fields_to_read = ( 'reservation_start_time', 'reservation_end_time', 'reservation_created_time', 'reservation_bandwidth', 'reservation_burst_limit', 'reservation_status', 'src_hostaddrs_id', 'dst_hostaddrs_id', 'reservation_description', 'reservation_tag' );
 
 my($unused, %results) = BSS::Client::SOAPClient::soap_get_resv_detail(\%params, \@fields_to_read);
 if (defined($results{'error_msg'}) && $results{'error_msg'})
--- oscars/rm/BSS/service/t/getresvs.pl
+++ oscars/rm/BSS/service/t/getresvs.pl
@@ -5,9 +5,9 @@ use BSS::Client::SOAPClient;
 use Data::Dumper;
 
 my($value);
-my %params = ('user_dn' => 'oscars' );   # FIX
+my %params = ('user_dn' => 'dwrobertson@lbl.gov' );   # FIX
     # names of the fields to be read and displayed on the screen
-my @fields_to_read = ( 'user_dn', 'reservation_start_time', 'reservation_end_time', 'reservation_status', 'src_hostaddrs_id', 'dst_hostaddrs_id' );
+my @fields_to_read = ( 'user_dn', 'reservation_start_time', 'reservation_end_time', 'reservation_status', 'src_hostaddrs_id', 'dst_hostaddrs_id', 'reservation_tag' );
 
 my($unused, %data) = soap_get_reservations(\%params, \@fields_to_read);
 if (defined($data{'error_msg'}) && $data{'error_msg'})
@@ -31,7 +31,8 @@ elsif (defined($data{'status_msg'}))
         print $r->{'reservation_end_time'}, ' ';
         print $r->{'reservation_status'}, ' ';;
         print $r->{'src_hostaddrs_id'}, ' ';
-        print $r->{'dst_hostaddrs_id'};
+        print $r->{'dst_hostaddrs_id'}, ' ';
+        print $r->{'reservation_tag'};
         print "\n";
     }
 }
