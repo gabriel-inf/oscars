ed64dce9bf0541febb8943547e2c9bbaaaaafc0a
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-06-03 07:17:21 +0000

    Removing unnecessary quotes.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@791 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/rm/BSS/Traceroute/JnxTraceroute.pm
+++ oscars/rm/BSS/Traceroute/JnxTraceroute.pm
@@ -1,10 +1,7 @@
-#####
-#
+##############################################################################
 # Package: JnxTraceroute.pm
 # Authors: chin guok (chin@es.net), David Robertson (dwrobertson@lbl.gov)
-# Description:
-#   Execute traceroute on Juniper routers.
-#
+# Description:  Execute traceroute on Juniper routers.
 #####
 
 package BSS::Traceroute::JnxTraceroute;
@@ -15,149 +12,112 @@ use Data::Dumper;
 use strict;
 
 
-
-#####
-#
-# Constant definitions.
-#
-#####
-
-
-#####
-#
-# Global variables.
-#
-#####
-
-
-#####
-#
+################
 # Public methods
-#
-#####
+################
 
-#####
-#
-# Method: new
-# Description:
-#   Create a JnxTraceroute
-# Input: <none>
+##############################################################################
+# new:  create a JnxTraceroute.
+# Input:  <none>
 # Output: new object
 #
-#####
 sub new
 {
-  my ($_class, %_args) = @_;
-  my ($_self) = {%_args};
+    my ($_class, %_args) = @_;
+    my ($_self) = {%_args};
 
-  # Bless $_self into designated class.
-  bless($_self, $_class);
+    # Bless $_self into designated class.
+    bless($_self, $_class);
 
-  # Initialize.
-  $_self->initialize();
-
-  return($_self);
+    # Initialize.
+    $_self->initialize();
+    return($_self);
 }
+######
 
-#####
-#
-# Method: traceroute
-# Description:
-#   traceroute from a Juniper router.
-# Input: source, destination
+##############################################################################
+# traceroute:  traceroute from a Juniper router.
+# Input:  source, destination
 # Output: <none>
 #
-#####
 sub traceroute
 {
-  my ($_self, $_src, $_dst) = @_;
-  my ($_hopInfo, $_hopCount, $_cmd);
+    my ($_self, $_src, $_dst) = @_;
+    my ($_hopInfo, $_hopCount, $_cmd);
 
-  if ( !defined($_src) || !defined($_dst) )  {
-    return "ERROR: Traceroute source or destination not defined\n";
-  }
+    if ( !defined($_src) || !defined($_dst) )  {
+        return "ERROR: Traceroute source or destination not defined\n";
+    }
 
-  # Perform the traceroute.
-  $_cmd = "ssh -x -a -i $_self->{'jnxConf'}->{'jnx_key'} -l $_self->{'jnxConf'}->{'jnx_user'} $_src traceroute $_dst";
-  print STDERR "$_cmd\n";
-  if (not(open(_TRACEROUTE_, "$_cmd 2>/dev/null |")))  {
-    return "ERROR: Unable to ssh into router and perform traceroute\n";
-  }
+    # Perform the traceroute.
+    $_cmd = "ssh -x -a -i $_self->{config}->{jnx_key} -l $_self->{config}->{jnx_user} $_src traceroute $_dst";
+    print STDERR "$_cmd\n";
+    if (not(open(_TRACEROUTE_, "$_cmd 2>/dev/null |")))  {
+        return "ERROR: Unable to ssh into router and perform traceroute\n";
+    }
 
-  # Reset hop information.
-  $_hopCount = 0;
-  undef($_self->{'rawHopData'});
-  undef($_self->{'hops'});
+    # Reset hop information.
+    $_hopCount = 0;
+    undef($_self->{rawHopData});
+    undef($_self->{hops});
 
-  # Parse the results.
-  while ($_hopInfo = <_TRACEROUTE_>)  {
-    $_self->{'rawHopData'}[$_hopCount] = $_hopInfo;
+    # Parse the results.
+    while ($_hopInfo = <_TRACEROUTE_>)  {
+        $_self->{rawHopData}[$_hopCount] = $_hopInfo;
 
-    # Get the hop IP address from output.
-    #  e.g.
-    #  1  esnet3-lbl3.es.net (198.129.76.26)  0.628 ms  0.569 ms  0.522 ms
-    $_hopInfo =~ m/\((\d+\.\d+\.\d+\.\d+)\)/;
-    $_self->{'hops'}[$_hopCount] = $1;
-
-    $_hopCount++;
-  }
-  return "";
+        # Get the hop IP address from output, e.g.
+        #  1  esnet3-lbl3.es.net (198.129.76.26)  0.628 ms  0.569 ms  0.522 ms
+        $_hopInfo =~ m/\((\d+\.\d+\.\d+\.\d+)\)/;
+        $_self->{hops}[$_hopCount] = $1;
+        $_hopCount++;
+    }
+    return "";
 }
+######
 
-#####
-#
-# Method: get_raw_hop_data
-# Description:
-#   Returns the raw traceroute data in an array.
+##############################################################################
+# get_raw_hop_data:  returns the raw traceroute data in an array.
 # Input: <none>
-# Output: Array of raw traceroute data
-#         e.g.
-#            1  esnet3-lbl3.es.net (198.129.76.26)  0.628 ms  0.569 ms  0.522 ms
+# Output: Array of raw traceroute data, e.g.
+#         1  esnet3-lbl3.es.net (198.129.76.26)  0.628 ms  0.569 ms  0.522 ms
 #
-#####
 sub get_raw_hop_data
 {
-  my ($_self) = @_;
+    my ($_self) = @_;
 
-  return(@{$_self->{'rawHopData'}});
+    return(@{$_self->{rawHopData}});
 }
+######
 
-#####
+##############################################################################
+# get_hops:  returns the IP addresses of the hops in an array.
+# Input:  <none>
+# Output: array of IP addresses
 #
-# Method: get_hops
-# Description:
-#   Returns the IP addresses of the hops in an array.
-# Input: <none>
-# Output: Array of IP addresses
-#
-#####
 sub get_hops
 {
-  my ($_self) = @_;
+    my ($_self) = @_;
 
-  return(@{$_self->{'hops'}});
+    return(@{$_self->{hops}});
 }
+######
 
-#####
-#
+#################
 # Private methods
-#
-#####
+#################
 
-#####
-#
-# Method: initialize
-# Description:
-#   Initialize jnxTraceroute with default values if not already populated.
+##############################################################################
+# initialize: initialize jnxTraceroute with default values if not already
+#             populated.
 # Input: <none>
 # Output: <none>
 #
-#####
 sub initialize 
 {
-  my ($_self) = @_;
+    my ($_self) = @_;
 
-  $_self->{'jnxConf'} = Config::Auto::parse($ENV{'OSCARS_HOME'} . '/oscars.cfg');
+    $_self->{config} = Config::Auto::parse($ENV{OSCARS_HOME} . '/oscars.cfg');
 }
+######
 
 1;
