f9f9a4ddabc33e51c681303ca61864ec802d566f
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-06-03 08:20:51 +0000

    Changes to get proper traceroutes, general cleanup.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@797 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/rm/BSS/Scheduler/ReservationHandler.pm
+++ oscars/rm/BSS/Scheduler/ReservationHandler.pm
@@ -1,9 +1,9 @@
-######################################################################
+##############################################################################
 # Main package, uses the BSS database front end
 #
 # JRLee
 # DWRobertson
-######################################################################
+##############################################################################
 
 package BSS::Scheduler::ReservationHandler; 
 
@@ -17,11 +17,10 @@ use BSS::Traceroute::JnxTraceroute;
 # BSS data base front end
 use BSS::Frontend::Reservation;
 
-# keep it tight
 use strict;
 
 
-######################################################################
+##############################################################################
 sub new {
     my ($_class, %_args) = @_;
     my ($_self) = {%_args};
@@ -35,118 +34,116 @@ sub new {
     return($_self);
 }
 
-
-######################################################################
 sub initialize {
     my ($self) = @_;
 
-    $self->{'frontend'} = BSS::Frontend::Reservation->new('configs' => $self->{'configs'});
+    $self->{frontend} = BSS::Frontend::Reservation->new('configs' => $self->{configs});
 }
+######
 
-
-################################
-### create reservation
-### 1) compute routers
-### 2) check bandwidth (y2?)
-### 3) return the reservation id
-### IN: ref to hash containing fields corresponding to the reservations table.
-###     Some fields are still empty, and are filled in before inserting a
-###     record
-### OUT: 0 on success, and hash containing all table fields, as well as error
-###     or status message
-################################
+##############################################################################
+# create reservation
+# 1) compute routers
+# 2) check bandwidth (y2?)
+# 3) return the reservation id
+# IN: ref to hash containing fields corresponding to the reservations table.
+#     Some fields are still empty, and are filled in before inserting a
+#     record
+# OUT: 0 on success, and hash containing all table fields, as well as error
+#     or status message
+#
 sub create_reservation {
     # reference to input hash ref containing fields filled in by user
     # This routine fills in the remaining fields.
     my ( $self, $inref ) = @_; 
     my ( $error_status, %results );
 
-    $self->{'output_buf'} = "*********************\n";
-    ($inref->{'ingress_interface_id'}, $inref->{'egress_interface_id'}, $inref->{'reservation_path'}, $results{'error_msg'}) = $self->find_interface_ids($inref->{'src_hostaddrs_ip'}, $inref->{'dst_hostaddrs_ip'});
+    $self->{output_buf} = "*********************\n";
+    ($inref->{ingress_interface_id}, $inref->{egress_interface_id}, $inref->{reservation_path}, $results{error_msg}) = $self->find_interface_ids($inref->{src_hostaddrs_ip}, $inref->{dst_hostaddrs_ip});
 
-    if ($results{'error_msg'}) { return ( 1, %results ); }
+    if ($results{error_msg}) { return ( 1, %results ); }
 
-    ( $error_status, %results ) = $self->{'frontend'}->insert_reservation( $inref );
-    if (!$results{'error_msg'}) {
-        $results{'reservation_tag'} =~ s/@/../;
+    ( $error_status, %results ) = $self->{frontend}->insert_reservation( $inref );
+    if (!$results{error_msg}) {
+        $results{reservation_tag} =~ s/@/../;
     }
     else {
-        $results{'reservation_tag'} = "fatal_reservation_errors";
+        $results{reservation_tag} = "fatal_reservation_errors";
     }
-    open (LOGFILE, ">$ENV{'OSCARS_HOME'}/logs/$results{'reservation_tag'}") || die "Can't open log file.\n";
+    open (LOGFILE, ">$ENV{OSCARS_HOME}/logs/$results{reservation_tag}") || die "Can't open log file.\n";
     print LOGFILE "********************\n";
-    print LOGFILE $self->{'output_buf'};
-    if ($results{'error_msg'}) {
-        print LOGFILE $results{'error_msg'}, "\n";
+    print LOGFILE $self->{output_buf};
+    if ($results{error_msg}) {
+        print LOGFILE $results{error_msg}, "\n";
     }
     close(LOGFILE);
     return ( $error_status, %results );
 }
+######
 
-
-################################
-### Given reservation id, leave the reservation in the db, but mark status as
-### cancelled, and set the ending time to 0 so that find_expired_reservations
-### will tear down the LSP if the reservation is active.
-################################
+##############################################################################
+# delete_reservation:  Given the reservation id, leave the reservation in the
+#     db, but mark status as cancelled, and set the ending time to 0 so that 
+#     find_expired_reservations will tear down the LSP if the reservation is
+#     active.
+#
 sub delete_reservation {
     my ( $self, $inref ) = @_;
 		
-    my ($error_status, %results) = $self->{'frontend'}->delete_reservation( $inref );
+    my ($error_status, %results) = $self->{frontend}->delete_reservation( $inref );
     return ($error_status, %results);
 }
+######
 
-
-################################
-### get_reservations
-### IN: ref to hash containing fields corresponding to the reservations table.
-###     Some fields are still empty, and are filled in before inserting a
-###     record
-### OUT: 0 on success, and hash containing all table fields, as well as error
-###     or status message
-################################
+##############################################################################
+# get_reservations
+# IN: ref to hash containing fields corresponding to the reservations table.
+#     Some fields are still empty, and are filled in before inserting a
+#     record
+# OUT: 0 on success, and hash containing all table fields, as well as error
+#     or status message
+#
 sub get_reservations {
     # reference to input hash ref containing fields filled in by user
     # This routine fills in the remaining fields.
     my ( $self, $inref, $fields_to_display ) = @_; 
 
-    my ($error_status, %results) = $self->{'frontend'}->get_reservations( $inref, $fields_to_display );
+    my ($error_status, %results) = $self->{frontend}->get_reservations( $inref, $fields_to_display );
     return ($error_status, %results);
 }
+######
 
-
-################################
-### get_reservation_detail
-### IN: ref to hash containing fields corresponding to the reservations table.
-###     Some fields are still empty, and are filled in before inserting a
-###     record
-### OUT: 0 on success, and hash containing all table fields, as well as error
-###     or status message
-################################
+##############################################################################
+# get_reservation_detail
+# IN: ref to hash containing fields corresponding to the reservations table.
+#     Some fields are still empty, and are filled in before inserting a
+#     record
+# OUT: 0 on success, and hash containing all table fields, as well as error
+#     or status message
+#
 sub get_reservation_detail {
     # inref is a ref to an input hash containing fields filled in by user.
     # This routine fills in the remaining fields.
     my ( $self, $inref, $fields_to_display ) = @_; 
 
-    my ($error_status, %results) = $self->{'frontend'}->get_reservation_detail( $inref, $fields_to_display );
+    my ($error_status, %results) = $self->{frontend}->get_reservation_detail( $inref, $fields_to_display );
     return ($error_status, %results);
 }
+######
 
-
-##############################################################
-### do ping
-### Freaking Net:Ping uses it own socket, so it has to be
-### root to do icmp. Any smart person would turn off UDP and
-### TCP echo ports ... gezzzz
-##############################################################
-
+##############################################################################
+# do ping:
+# Freaking Net:Ping uses it own socket, so it has to be
+# root to do icmp. Any smart person would turn off UDP and
+# TCP echo ports ... gezzzz
+#
 sub do_ping {
     my ( $self, $host ) = @_;
 
     # use sytem 'ping' command (should be config'd in config file
-    if ($self->{'configs'}{'use_system'}) {
+    if ($self->{configs}{use_system}) {
         my @ping = `/bin/ping -w 10 -c 3 -n  $host`;
-        foreach my $i (@ping) {
+        for my $i (@ping) {
             if ( $i =~ /^64 bytes/ ) {  
                 return 1; 
             }
@@ -164,82 +161,94 @@ sub do_ping {
     }
     return 0;
 }
+######
 
-
-################################
-### do remote trace
-################################
-
+##############################################################################
+# do remote trace:  Run traceroute from src to dst.
+#
+# In:   source, destination IP addresses.
+# Out:  interface ID, path, and error message, if any  
+#
 sub do_remote_trace {
     my ( $self, $src, $dst )  = @_;
     my (@hops);
-    my ($error, $interface_id, $prev_ipaddr, $prev_id, $path);
+    my ($error, $interface_id, $prev_id, @path, $prev_ipaddr);
+    my ($prev_loopback, $loopback_ip);
 
-    $path = "";
-
+    @path = ();
     # try to ping before traceing?
-    if ($self->{'configs'}{'use_ping'}) {
+    if ($self->{configs}{use_ping}) {
         if ( 0 == $self->do_ping($dst)) {
-            return (0, "", "do_remote_trace:  host $dst not pingable");
+            return (0, "", \@path, "host $dst not pingable");
         }
     }
 
     my ($_jnxTraceroute) = new BSS::Traceroute::JnxTraceroute();
-
     $error = $_jnxTraceroute->traceroute($src, $dst);
     if ($error)  {
-        return (0, "", "do_remote_trace: " . $error);
+        return (0, "", \@path, $error);
     }
 
     @hops = $_jnxTraceroute->get_hops();
 
     # if we didn't hop much, maybe the same router?
-    if ($#hops < 0 ) { return (0, "", "do_remote_trace:  same router?"); }
+    if ($#hops < 0 ) { return (0, "", \@path, "same router?"); }
 
     if ($#hops == 0) { 
             # id is 0 if not an edge router (not in interfaces table)
-        ($interface_id, $error) = $self->{'frontend'}->{'dbconn'}->ip_to_xface_id($self->{'configs'}{'jnx_source'});
-        return ($interface_id, $path, $error);
+        ($interface_id, $error) = $self->{frontend}->{dbconn}->ip_to_xface_id($self->{configs}{jnx_source});
+        if ($interface_id != 0) {
+            ($loopback_ip, $error) = $self->{frontend}->{dbconn}->xface_id_to_loopback($interface_id, 'ip');
+            print STDERR '-- ', $loopback_ip, "\n";
+        }
+        #TODO: FIX
+        return ($interface_id, $loopback_ip, \@path, $error);
     }
 
     # start off with an non-existent router
     $interface_id = 0;
 
-    # loop forward till the next router isn't one of ours ...
+    # loop forward till the next router isn't one of ours or doesn't have
+    # an oscars loopback address
     while(defined($hops[0]))  {
-        $self->{'output_buf'} .= "hop:  $hops[0]\n";
-        print STDERR "do_remote_trace:  hop:  $hops[0]\n";
+        $self->{output_buf} .= "hop:  $hops[0]\n";
+        print STDERR "hop:  $hops[0]\n";
         # id is 0 if not an edge router (not in interfaces table)
-        ($interface_id, $error) = $self->{'frontend'}->{'dbconn'}->ip_to_xface_id($hops[0]);
-        if ( $interface_id == 0 ) {
-            # strip trailing ','
-            $path =~ s/,$//;
-            $self->{'output_buf'} .= "edge router is $prev_ipaddr\n";
-            print STDERR "do_remote_trace:  edge router is $prev_ipaddr\n";
-            return ($prev_id, $path, "");
+        ($interface_id, $error) = $self->{frontend}->{dbconn}->ip_to_xface_id($hops[0]);
+        # check to make sure router has a loopback
+        if ($interface_id != 0) {
+            ($loopback_ip, $error) = $self->{frontend}->{dbconn}->xface_id_to_loopback($interface_id, 'ip');
+        }
+        if ($interface_id == 0) {
+            $self->{output_buf} .= "edge router is $prev_loopback\n";
+            print STDERR "edge router is $prev_loopback\n";
+            return ($prev_id, $prev_ipaddr, \@path, "");
         }
 
         # add to the path
-        $path = $interface_id . ',' . $path;
-        $prev_id = $interface_id;
-        $prev_ipaddr = $hops[0];
+        push(@path, $interface_id);
+        if ($loopback_ip) {
+            $prev_id = $interface_id;
+            $prev_ipaddr = $hops[0];
+            $prev_loopback = $loopback_ip;
+        }
         shift(@hops);
     }
 
     # if we didn't find it
-    return (0, $path, "do_remote_trace:  Couldn't trace route to $src");
+    return (0, "", \@path, "Couldn't trace route to $src");
 }
+######
 
-################################
-### do local trace
-################################
-
+##############################################################################
+# do local trace
+#
 sub do_local_trace {
     my ($self, $host)  = @_;
     my ($tr, $hops, $interface_id, $error);
 
     # try to ping before traceing?
-    if ($self->{'configs'}{'use_ping'}) {
+    if ($self->{configs}{use_ping}) {
         if ( 0 == $self->do_ping($host)) {
             return (0, "do_local_trace: Host $host not pingable");
         }
@@ -253,7 +262,7 @@ sub do_local_trace {
     } 
 
     $hops = $tr->hops;
-    $self->{'output_buf'} .= "do_local_trace:  hops = $hops\n";
+    $self->{output_buf} .= "do_local_trace:  hops = $hops\n";
     print STDERR "do_local_trace:  hops = $hops\n";
 
     # if we didn't hop much, mabe the same router?
@@ -262,9 +271,9 @@ sub do_local_trace {
     # loop from the last router back, till we find an edge router
     for my $i (1..$hops-1) {
         my $ipaddr = $tr->hop_query_host($hops - $i, 0);
-        ($interface_id, $error) = $self->{'frontend'}->{'dbconn'}->ip_to_xface_id($ipaddr);
+        ($interface_id, $error) = $self->{frontend}->{dbconn}->ip_to_xface_id($ipaddr);
         if (($interface_id != 0) && (!$error)) {
-            $self->{'output_buf'} .= "do_local_trace:  edge router is $ipaddr\n";
+            $self->{output_buf} .= "do_local_trace:  edge router is $ipaddr\n";
             print STDERR "do_local_trace:  edge router is $ipaddr\n";
             return ($interface_id, "");
         } 
@@ -273,32 +282,35 @@ sub do_local_trace {
     # if we didn't find it
     return (0, "do_local_trace:  could not find edge router");
 }
+######
 
-
-################################
-### run traceroutes to both hosts
-### find edge routers validate both
-### ends
-### IN: src and dst hosts
-### OUT: ids of interfaces of the edge routers, 
-### path list (router indexes), and error msg
-### XXX: validate input
-################################
-
+##############################################################################
+# find_interface_ids:  run traceroutes to both hosts.  Find edge routers and
+# validate both ends.
+# IN:  src and dst host IP addresses
+# OUT: ids of interfaces of the edge routers, path list (router indexes), and
+#      error msg
+# TODO: validate input
+#
 sub find_interface_ids {
     my ($self, $src, $dst, $path) = @_;
 
-    my( $ingress_interface_id, $egress_interface_id, $path, $err_msg, $start_router);
+    my( $ingress_interface_id, $egress_interface_id );
+    my( $loopback_ip, $path, $err_msg, $start_router );
 
-    # find the router closest to the src
-    $self->{'output_buf'} .= "--traceroute:  $self->{'configs'}{'jnx_source'} to source $src\n";
-    ($ingress_interface_id, $path, $err_msg) = $self->do_remote_trace($self->{'configs'}{'jnx_source'}, $src);
+    # Use the default router to run the traceroute to the source.
+    # Find the router with an oscars loopback closest to the source 
+    $self->{output_buf} .= "--traceroute:  $self->{configs}{jnx_source} to source $src\n";
+    ($ingress_interface_id, $loopback_ip, $path, $err_msg) = $self->do_remote_trace(
+                                           $self->{configs}{jnx_source}, $src);
     if ($err_msg) { return ( 0, 0, $err_msg); }
   
-    # now use the default router to run the traceroute to the dst, and find the egress
-    if ( $self->{'configs'}{'run_traceroute'} )  {
-        $self->{'output_buf'} .= "--traceroute:  $self->{'configs'}{'jnx_source'} to destination $dst\n";
-        ($egress_interface_id, $path, $err_msg) = $self->do_remote_trace($self->{'configs'}{'jnx_source'}, $dst);
+    # Now use the address found in the last step to run the traceroute to the
+    # destination, and find the egress.
+    if ( $self->{configs}{run_traceroute} )  {
+        $self->{output_buf} .= "--traceroute:  $loopback_ip to destination $dst\n";
+        ($egress_interface_id, $loopback_ip, $path, $err_msg) = $self->do_remote_trace(
+                                           $loopback_ip, $dst);
         if ($err_msg) { return (0, 0, $err_msg); }
     } else {
         ($ingress_interface_id, $err_msg) = $self->do_local_trace($src);
@@ -309,9 +321,9 @@ sub find_interface_ids {
     {
         return( 0, 0, 0, "Unable to find route." );
     }
-	##return ($ingress_interface_id, $egress_interface_id, ""); 
 	return ($ingress_interface_id, $egress_interface_id, $path, ""); 
 }
+######
 
 ### last line of a module
 1;
--- oscars/rm/BSS/Scheduler/SchedulerThread.pm
+++ oscars/rm/BSS/Scheduler/SchedulerThread.pm
@@ -44,66 +44,60 @@ my ($fakeit) = 0;
 
 my ($_error);
 
-######################################################################
-# This should probably be just new or 
-# some such OO thing. Just startup and deatch
-# a thread to do scheduling, return control
-# to the main prog
-######################################################################
-sub start_scheduler {
-
+##############################################################################
+# start_scheduler:  Start up and detach a thread to do scheduling, and
+#                   return control to the main prog
+#
+sub start_scheduler { 
     # get a copy of the config
     $configs = shift;
 
-    if ($configs->{'debug'}) { print STDERR "Starting Scheduler\n"; }
+    if ($configs->{debug}) { print STDERR "Starting Scheduler\n"; }
     my $handler = threads->create("scheduler");
     $handler->detach();
 
     return 1;
 }
+######
 
-######################################################################
-# Main: Loop forever checking the DB every N
-# minutes for reserversations
-######################################################################
+##############################################################################
+# scheduler: Loop forever checking the DB every N minutes for reserversations
+#
 sub scheduler {
 
-    print STDERR "Scheduler running\n";
     my ($front_end, $error_msg);
 
+    print STDERR "Scheduler running\n";
     $front_end = BSS::Frontend::Reservation->new('configs' => $configs);
 
     while (1) {
 
         # find reservations that need to be actived
-        if ($configs->{'debug'}) { print STDERR "find new\n"; }
+        if ($configs->{debug}) { print STDERR "find new\n"; }
         $error_msg = find_new_reservations($front_end);
         if ($error_msg) {
-            if ($configs->{'debug'}) { print STDERR '** ', $error_msg, "\n"; }
+            if ($configs->{debug}) { print STDERR '** ', $error_msg, "\n"; }
         }
 
 
         # find reservations that need to be deactivated 
-        if ($configs->{'debug'}) { print STDERR "find_exp\n"; }
+        if ($configs->{debug}) { print STDERR "find_exp\n"; }
         $error_msg = find_expired_reservations($front_end);
         if ($error_msg) {
-            if ($configs->{'debug'}) { print STDERR '** ', $error_msg, "\n"; }
+            if ($configs->{debug}) { print STDERR '** ', $error_msg, "\n"; }
         }
 
         # check every do_poll_time seconds
-        sleep($configs->{'db_poll_time'});
+        sleep($configs->{db_poll_time});
     }
 }
+######
 
-######################################################################
-# Find reservations to run
-# need to find all the reservatations in db
-# that need to be setup and run in the next N
-# minutes
-######################################################################
+##############################################################################
+# find_new_reservations:  find reservations to run.  Find all the
+#    reservatations in db that need to be setup and run in the next N minutes.
+#
 sub find_new_reservations {
-
-
     my ($front_end) = @_;
 
     my ($timeslot, $resv, $status);
@@ -111,34 +105,33 @@ sub find_new_reservations {
     my $cur_time = localtime();
 
     # configurable
-    $timeslot = time() + $configs->{'reservation_time_interval'};
-    if ($configs->{'debug'}) {
+    $timeslot = time() + $configs->{reservation_time_interval};
+    if ($configs->{debug}) {
         print STDERR "pending: $cur_time \n";
     }
 
     # find reservations that need to be scheduled
-    ($error_msg, $resv) = $front_end->find_pending_reservations($timeslot, $configs->{'PENDING'});
+    ($error_msg, $resv) = $front_end->find_pending_reservations($timeslot, $configs->{PENDING});
     if ($error_msg) {
         return ($error_msg);
     }
 
-    foreach my $r (@$resv) {
+    for my $r (@$resv) {
         ## calls to pss to setup reservations
         my %lsp_info = map_fields($front_end, $r);
         $status = setup_pss(\%lsp_info, $r);
-        if ($configs->{'debug'}) { print STDERR "update reservation to active\n"; }
-        update_reservation( $r, $status, $configs->{'ACTIVE'}, $front_end);
+        if ($configs->{debug}) { print STDERR "update reservation to active\n"; }
+        update_reservation( $r, $status, $configs->{ACTIVE}, $front_end);
     }
     return "";
 }
+######
 
-######################################################################
+##############################################################################
+# find_expired_reservations:  find reservations that have expired, and tear
+#                             them down
 #
-# Find reservations that have expired, and tear them down
-#
-######################################################################
 sub find_expired_reservations {
-
     my ($front_end) = @_;
 
     my $cur_time = localtime();
@@ -146,29 +139,29 @@ sub find_expired_reservations {
 
     # configurable
     $timeslot = time() + $configs->{reservation_time_interval};
-    if ($configs->{'debug'}) { print STDERR "expired: $cur_time \n"; }
+    if ($configs->{debug}) { print STDERR "expired: $cur_time \n"; }
 
     # find active reservation past the timeslot
-    ($error_msg, $resv) = $front_end->find_expired_reservations($timeslot, $configs->{'ACTIVE'});
+    ($error_msg, $resv) = $front_end->find_expired_reservations($timeslot, $configs->{ACTIVE});
     if ($error_msg) { return $error_msg; }
        
-    foreach my $r (@$resv) {
+    for my $r (@$resv) {
         my %lsp_info = map_fields($front_end, $r);
         $status = teardown_pss(\%lsp_info, $r);
 
-        if ($configs->{'debug'}) { print STDERR "update reservation to active\n"; }
-        update_reservation( $r, $status, $configs->{'FINISHED'}, $front_end);
+        if ($configs->{debug}) { print STDERR "update reservation to active\n"; }
+        update_reservation( $r, $status, $configs->{FINISHED}, $front_end);
     }
     return "";
 }
-######################################################################
-#
-# Format the args and call pss to do the configuraion change
-#
-######################################################################
-sub setup_pss {
+######
 
+##############################################################################
+# setup_pss:  format the args and call pss to do the configuration change
+#
+sub setup_pss {
     my ($lspInfo, $r) = @_;   
+
     my ($_error, $status);
 
     print STDERR "execing pss to schedule reservations\n";
@@ -187,15 +180,14 @@ sub setup_pss {
     print STDERR "LSP setup complete\n" ;
     return( "" );
 }
+######
 
-######################################################################
+##############################################################################
+# teardown_pss:  format the args and call pss to teardown the configuraion 
 #
-# Format the args and call pss to teardown the configuraion 
-#
-######################################################################
 sub teardown_pss {
-
     my ($lspInfo, $r) = @_;
+
     my ($_error);
 
     if ($fakeit == 0 ) {
@@ -211,16 +203,13 @@ sub teardown_pss {
     print STDERR "LSP teardown complete\n" ;
     return( "" );
 }
+######
 
-
-######################################################################
+##############################################################################
+# update_reservation: change the status of the reservervation from pending to
+#                     active
 #
-# Change the status of the reservervation from pending to active
-#
-######################################################################
-
 sub update_reservation {
-
     my ($resv, $error_msg, $status, $front_end) = @_;
 
     if ( !$error_msg ) {
@@ -228,43 +217,40 @@ sub update_reservation {
         $front_end->update_reservation($resv, $status)
     } else {
         print STDERR "Changing status to failed\n";
-        $front_end->update_reservation($resv, $configs->{'FAILED'})
+        $front_end->update_reservation($resv, $configs->{FAILED})
     }
 }
+######
 
-######################################################################
-
-sub map_fields
-{
+##############################################################################
+sub map_fields {
     my ( $front_end, $data ) = @_;
+
     my ( %results, $error );
-    my ( $ingress_loopback_name, $egress_loopback_name, $src_hostaddrs_ip, $dst_hostaddrs_ip );
+    my ( $ingress_loopback_ip, $egress_loopback_ip, $src_hostaddrs_ip, $dst_hostaddrs_ip );
 
      # get loopbacks for routers, given interface ids
-    ($ingress_loopback_name, $error) = $front_end->{'dbconn'}->xface_id_to_loopback($data->{'ingress_interface_id'});
-    ($egress_loopback_name, $error) = $front_end->{'dbconn'}->xface_id_to_loopback($data->{'egress_interface_id'});
+    ($ingress_loopback_ip, $error) = $front_end->{dbconn}->xface_id_to_loopback($data->{ingress_interface_id}, 'ip');
+    ($egress_loopback_ip, $error) = $front_end->{dbconn}->xface_id_to_loopback($data->{egress_interface_id}, 'ip');
+     print "lsp_from: $ingress_loopback_ip, lsp_to:  $egress_loopback_ip\n";
      # get host IP addresses, given id 
-    ($src_hostaddrs_ip, $error) = $front_end->{'dbconn'}->hostaddrs_id_to_ip($data->{'src_hostaddrs_id'});
-    ($dst_hostaddrs_ip, $error) = $front_end->{'dbconn'}->hostaddrs_id_to_ip($data->{'dst_hostaddrs_id'});
+    ($src_hostaddrs_ip, $error) = $front_end->{dbconn}->hostaddrs_id_to_ip($data->{src_hostaddrs_id});
+    ($dst_hostaddrs_ip, $error) = $front_end->{dbconn}->hostaddrs_id_to_ip($data->{dst_hostaddrs_id});
     %results = (
-      'name' => "oscars_$data->{'reservation_id'}",
-      'lsp_from' => $ingress_loopback_name,
+      'name' => "oscars_$data->{reservation_id}",
+      'lsp_from' => $ingress_loopback_ip,
       #'lsp_from' => 'dev-rt20-e.es.net',
-      'lsp_to' => $egress_loopback_name,
+      'lsp_to' => $egress_loopback_ip,
       #'lsp_to' => "10.0.0.1",
-      'bandwidth' => $data->{'reservation_bandwidth'},
-      'lsp_class-of-service' => $data->{'reservation_class'},
-      'policer_burst-size-limit' =>  $data->{'reservation_burst_limit'},
+      'bandwidth' => $data->{reservation_bandwidth},
+      'lsp_class-of-service' => $data->{reservation_class},
+      'policer_burst-size-limit' =>  $data->{reservation_burst_limit},
       'source-address' => $src_hostaddrs_ip,
       'destination-address' => $dst_hostaddrs_ip,
     );
     return ( %results );
 }
 
-
-#########################
-##  End of package
-#########################
 1;
 
 # vim: et ts=4 sw=4
