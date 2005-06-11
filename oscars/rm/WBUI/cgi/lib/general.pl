5c0262b9abb381d4631a93a0595997ca2eb675dd
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-06-11 00:01:03 +0000

    Added "authorized" method.  Comments changes.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@870 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/rm/WBUI/cgi/lib/general.pl
+++ oscars/rm/WBUI/cgi/lib/general.pl
@@ -1,7 +1,7 @@
 # general.pl
 #
 # library for general cgi script usage
-# Last modified: June 6, 2005
+# Last modified: June 10, 2005
 # Soo-yeon Hwang (dapi@umich.edu)
 # David Robertson (dwrobertson@lbl.gov)
 
@@ -13,12 +13,14 @@ use AAAS::Client::SOAPClient;
 use AAAS::Client::Auth;
 
 
-# Checks session status only; the checks for correct password,
-# administrative level done in front end to database during
-# initial login.
-
+##############################################################################
+# check_session_status;  checks CGI session status only; the checks for
+# correct password and administrative level are done in the front end to the
+# database during initial login.
+#
 sub check_session_status {
     my( $login_results, $cgi ) = @_;
+
     my( $auth, $dn, $user_level );
 
     $auth = AAAS::Client::Auth->new();
@@ -31,17 +33,33 @@ sub check_session_status {
         return($dn, $user_level);
     }
 }
+######
 
-
-##### sub update_frames
-##    Prints to the status frame, and sets location of the main frame if
-##    a URI is given.
+##############################################################################
+# authorized:  Given the user level string, see if the user has the required
+#              privilege 
 #
-##    In:  uri, error or status msg
-#####
-sub update_frames
-{
+sub authorized {
+    my( $user_level, $required_priv ) = @_;
+ 
+    for my $priv (split(' ', $user_level)) {
+        if ($priv eq $required_priv) {
+            return( 1 );
+        }
+    }
+    return( 0 );
+}
+######
+
+##############################################################################
+# update_frames:  Prints to the status frame, and sets location of the main
+# frame if a URI is given.
+#
+# In:  uri, error or status msg
+#
+sub update_frames {
     my ($print_type_header, $target, $uri, $msg) = @_;
+
     if ($print_type_header) { print 'Content-type: text/html', "\n\n"; }
     print '<html>', "\n";
     print '<head>', "\n";
@@ -58,8 +76,11 @@ sub update_frames
     print '</html>', "\n";
     print "\n\n";
 }
+######
 
-
+##############################################################################
+# end_session:  Ends CGI session.
+#
 sub end_session {
     my( $cgi ) = @_;
     my( $auth );
@@ -67,10 +88,12 @@ sub end_session {
     $auth = AAAS::Client::Auth->new();
     $auth->logout($cgi);
 }
+######
 
-
-sub get_time_str 
-{
+##############################################################################
+# get_time_str:  converts from epoch in seconds to current local date and time
+#
+sub get_time_str {
     my( $epoch_seconds ) = @_;
 
     my $dt = DateTime->from_epoch( epoch => $epoch_seconds );
@@ -97,10 +120,12 @@ sub get_time_str
     my $time_field = $month . "-" . $day . "&nbsp;&nbsp; " . $hour . ":" . $minute;
     return( $time_field );
 }
+######
 
-
-sub get_oscars_host
-{
+##############################################################################
+# get_oscars_host:  finds host name from IP address if possible
+#
+sub get_oscars_host {
     my( $input ) = @_;
 
     my $ipaddr = inet_aton($input);
@@ -112,6 +137,7 @@ sub get_oscars_host
         return($input); 
     }
 }
+######
 
 
 # Don't touch the line below
