89eb99c929d5e079f009d62e97806ff5267c0857
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-06-14 23:06:20 +0000

    Got refresh button working for reservation list, reservation details pages.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@887 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/rm/WBUI/cgi/reservations/details.pl
+++ oscars/rm/WBUI/cgi/reservations/details.pl
@@ -2,7 +2,7 @@
 
 # details.pl:  Linked to by resvlist_form.pl.  Lists the details of
 #              a reservation.
-# Last modified: June 13, 2005
+# Last modified: June 14, 2005
 # Soo-yeon Hwang (dapi@umich.edu)
 # David Robertson (dwrobertson@lbl.gov)
 
@@ -26,7 +26,17 @@ if ($dn) {
     }
     $form_params{user_dn} = $dn;
     $form_params{user_level} = $user_level;
-    ($error_status, %results) = BSS::Client::SOAPClient::soap_get_reservations(\%form_params);
+        # Check if reservation is being cancelled
+    if ($form_params{cancel}) {
+        ($error_status, %results) = soap_delete_reservation(\%form_params);
+        if (!$error_status) {
+            update_frames($error_status, "main_frame", "", $results{error_msg});
+            exit;
+        }
+    }
+    # print updated reservation info (may be more than just new status)
+    ($error_status, %results) =
+                 BSS::Client::SOAPClient::soap_get_reservations(\%form_params);
     if (!$error_status) {
         update_frames($error_status, "main_frame", "", $results{status_msg});
         print_reservation_detail($user_level, \%form_params, \%results);
@@ -54,48 +64,97 @@ sub print_reservation_detail
     print '<head>', "\n";
     print '<link rel="stylesheet" type="text/css" ';
     print ' href="https://oscars.es.net/styleSheets/layout.css">', "\n";
-    print '    <script language="javascript" type="text/javascript" src="https://oscars.es.net/main_common.js"></script>', "\n";
-    print '    <script language="javascript" type="text/javascript" src="https://oscars.es.net/timeprint.js"></script>', "\n";
+    print '    <script language="javascript" type="text/javascript"';
+    print '        src="https://oscars.es.net/main_common.js"></script>', "\n";
+    print '    <script language="javascript" type="text/javascript"';
+    print '        src="https://oscars.es.net/timeprint.js"></script>', "\n";
     print '</head>', "\n\n";
 
-    print "<body onload=\"stripe('reservationlist', '#fff', '#edf3fe');\">\n\n";
+    print "<body onload=\"stripe('reservationlist', '#fff', '#edf3fe');\">\n";
 
-    print '<script language="javascript">print_navigation_bar("', $user_level, '", "reservationlist");</script>', "\n";
+    print '<script language="javascript">';
+    print '    print_navigation_bar("', $user_level, '", "reservationlist");';
+    print '</script>', "\n";
 
     print '<div id="zebratable_ui">', "\n\n";
 
-    print '<p><em>View Reservation</em><br/></p>', "\n\n";
+    print '<p><form method="post" action="https://oscars.es.net/cgi-bin/';
+    print 'reservations/details.pl">', "\n";
+
+    print '<input type="hidden" name="reservation_id" value="';
+    print $form_params{reservation_id} . '">', "\n";
+
+    print '<input type="submit" value="Refresh">', "\n";
+    print '</form></p>', "\n";
 
     print '<table cellspacing="0" width="90%" id="reservationlist">', "\n";
 
     print "  <tr><td>Tag:  </td><td>$row->{reservation_tag}</td></tr>\n"; 
 
-    print '  <tr><td>Start Time:  </td><td>';
-    print '<script language="javascript">print_current_date("", ' . $row->{reservation_start_time} . ", 'local');</script></td></tr>\n";
+    print '  <tr><td>Start Time:  </td><td>', "\n";
+    print '    <script language="javascript">', "\n";
+    print '    print_current_date("", ' . $row->{reservation_start_time};
+    print '                       , "local");', "\n";
+    print '    </script>', "\n";
+    print '  </td></tr>', "\n";
 
     print '  <tr><td>End Time:  </td><td>';
-    print '<script language="javascript">print_current_date("", ' . $row->{reservation_end_time} . ", 'local');</script></td></tr>\n";
+    print '    <script language="javascript">', "\n";
+    print '    print_current_date("", ' . $row->{reservation_end_time};
+    print '                       , "local");', "\n";
+    print '    </script>', "\n";
+    print '  </td></tr>', "\n";
 
     print '  <tr><td>Created Time:  </td><td>';
-    print '<script language="javascript">print_current_date("", ' . $row->{reservation_created_time} . ", 'local');</script></td></tr>\n";
+    print '    <script language="javascript">', "\n";
+    print '    print_current_date("", ' . $row->{reservation_created_time};
+    print '                       , "local");', "\n";
+    print '    </script>', "\n";
+    print '  </td></tr>', "\n";
 
-    print "  <tr><td>Bandwidth:  </td><td>$row->{reservation_bandwidth}</td></tr>\n";
-    print "  <tr><td>Burst Limit:  </td><td>$row->{reservation_burst_limit}</td></tr>\n";
-    print "  <tr><td>Status:  </td><td>$row->{reservation_status}</td></tr>\n";
-    print "  <tr><td>Origin:  </td><td>", get_oscars_host($row->{src_hostaddrs_id}), "</td></tr>\n";
-    print "  <tr><td>Destination:  </td><td>", get_oscars_host($row->{dst_hostaddrs_id}), "</td></tr>\n";
-    print "  <tr><td>Description:  </td><td>", $row->{reservation_description}, "</td></tr>\n";
+    print '  <tr>', "\n";
+    print '  <td>Bandwidth:  </td>', "\n";
+    print '  <td>' . $row->{reservation_bandwidth} . '</td>', "\n";
+    print '  </tr>', "\n";
+
+    print '  <tr>', "\n";
+    print '  <td>Burst Limit:  </td>', "\n";
+    print '  <td>' . $row->{reservation_burst_limit} . '</td>', "\n";
+    print '  </tr>', "\n";
+
+    print '  <tr>', "\n";
+    print '  <td>Status:  </td>', "\n";
+    print '  <td>' . $row->{reservation_status} . '</td>', "\n";
+    print '  </tr>', "\n";
+
+    print '  <tr>', "\n";
+    print '  <td>Origin:  </td>', "\n";
+    print '  <td>' . get_oscars_host($row->{src_hostaddrs_id}) . '</td>', "\n";
+    print '  </tr>', "\n";
+
+    print '  <tr>', "\n";
+    print '  <td>Destination:  </td>', "\n";
+    print '  <td>' . get_oscars_host($row->{dst_hostaddrs_id}) . '</td>', "\n";
+    print '  </tr>', "\n";
+
+    print '  <tr>', "\n";
+    print '  <td>Description:  </td>', "\n";
+    print '  <td>' . $row->{reservation_description} . '</td>', "\n";
+    print '  </tr>', "\n";
 
     print '  <tr><td>Action: </td><td>';
     if (($row->{reservation_status} eq 'pending') ||
         ($row->{reservation_status} eq 'active')) {
-       print  '<a href="https://oscars.es.net/cgi-bin/reservations/list_form.pl?reservation_id=' . $row->{reservation_id} . '">CANCEL</a></td></tr>' . "\n";
+       print  '<a href="https://oscars.es.net/cgi-bin/reservations/details.pl';
+       print  '?reservation_id=' . $row->{reservation_id} . '&cancel=1">';
+       print  'CANCEL</a></td></tr>' . "\n";
     }
     else { print '<td></td></tr>', "\n"; }
 
     print "</table>\n";
     print '<br/><br/>';
-    print '<a href="https://oscars.es.net/cgi-bin/reservations/list_form.pl">Back to reservations list</a>';
+    print '<a href="https://oscars.es.net/cgi-bin/reservations/list_form.pl">';
+    print 'Back to reservations list</a>', "\n";
 
     print "<p>For inquiries, please contact the project administrator.</p>\n\n";
     print "</div>\n\n";
--- oscars/rm/WBUI/cgi/reservations/list_form.pl
+++ oscars/rm/WBUI/cgi/reservations/list_form.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 # list_form.pl:  page listing reservations
-# Last modified: June 10, 2005
+# Last modified: June 14, 2005
 # Soo-yeon Hwang (dapi@umich.edu)
 # David Robertson (dwrobertson@lbl.gov)
 
@@ -29,23 +29,7 @@ if (!$error_status) {
         $form_params{user_dn} = '*';
     }
     else { $form_params{user_dn} = $dn; }
-
-        # The reservation id, if present, indicates a deletion
-    if ($form_params{reservation_id}) {
-        ($error_status, %results) = soap_delete_reservation(\%form_params);
-        if (!$error_status) {
-            # save the status message
-            my $update_status = $results{status_msg};
-            # undefine reservation_id so get more than just that row
-            $form_params{reservation_id} = undef;
-            # get the updated data
-            ($error_status, %results) = soap_get_reservations(\%form_params);
-            $results{status_msg} = $update_status;
-        }
-    }
-    else {
-        ($error_status, %results) = soap_get_reservations(\%form_params);
-    }
+    ($error_status, %results) = soap_get_reservations(\%form_params);
     if (!$error_status) {
         update_frames($error_status, "main_frame", "", $results{status_msg});
         print_reservations(\%form_params, \%results);
@@ -76,23 +60,35 @@ sub print_reservations {
     print '<head>', "\n";
     print '<link rel="stylesheet" type="text/css" ';
     print ' href="https://oscars.es.net/styleSheets/layout.css">', "\n";
-    print '    <script language="javascript" type="text/javascript" src="https://oscars.es.net/main_common.js"></script>', "\n";
-    print '    <script language="javascript" type="text/javascript" src="https://oscars.es.net/timeprint.js"></script>', "\n";
-    print '    <script language="javascript" type="text/javascript" src="https://oscars.es.net/sorttable.js"></script>', "\n";
+    print '    <script language="javascript" type="text/javascript"';
+    print '        src="https://oscars.es.net/main_common.js"></script>', "\n";
+    print '    <script language="javascript" type="text/javascript"';
+    print '         src="https://oscars.es.net/timeprint.js"></script>', "\n";
+    print '    <script language="javascript" type="text/javascript"';
+    print '         src="https://oscars.es.net/sorttable.js"></script>', "\n";
     print '</head>', "\n\n";
 
-    print "<body onload=\"stripe('reservationlist', '#fff', '#edf3fe');\">\n\n";
-    print '<script language="javascript">print_navigation_bar("', $user_level, '", "reservationlist");</script>', "\n";
+    print "<body onload=\"stripe('reservationlist', '#fff', '#edf3fe');\">\n";
+    print '<script language="javascript">';
+    print 'print_navigation_bar("', $user_level, '", "reservationlist");';
+    print '</script>', "\n";
 
     print '<div id="zebratable_ui">', "\n\n";
 
     print '<p><em>View Reservations</em></p>', "\n";
-    print '<p>Click on a column header to sort by that column. ', "\n";
+    print '<p>Click on a column header to sort by that column. ';
     print 'Times given are in the time zone of the browser. ', "\n";
-    print 'Click on the Reservation Tag link to view detailed information about the reservation. ', "\n";
+    print 'Click on the Reservation Tag link to view detailed information ';
+    print 'about the reservation. ', "\n";
     print '</p>', "\n\n";
 
-    print '<table cellspacing="0" width="90%" class="sortable" id="reservationlist">', "\n";
+    print '<p><form method="post" action="https://oscars.es.net/cgi-bin/';
+    print 'reservations/list_form.pl">', "\n";
+    print '<input type="submit" value="Refresh">', "\n";
+    print '</form></p>', "\n";
+
+    print '<table cellspacing="0" width="90%" class="sortable" ';
+    print '       id="reservationlist">', "\n";
     print '  <thead>', "\n";
     print '  <tr>', "\n";
     print '    <td >Tag</td>', "\n";
@@ -113,7 +109,7 @@ sub print_reservations {
     print '  </tbody>', "\n";
     print '</table>', "\n\n";
 
-    print '<p>For inquiries, please contact the project administrator.</p>', "\n\n";
+    print '<p>For inquiries, please contact the project administrator.</p>';
     print '</div>', "\n\n";
 
     print '<script language="javascript">print_footer();</script>', "\n";
@@ -128,10 +124,26 @@ sub print_row
     my( $row, $user_level ) = @_;
     my( $seconds, $ip );
 
-    print '    <td><a href="https://oscars.es.net/cgi-bin/reservations/details.pl?reservation_id=' . $row->{reservation_id} . '">' . $row->{reservation_tag} . '</a></td>' . "\n"; 
+    print '    <td>', "\n";
+    print '    <a href="https://oscars.es.net/cgi-bin/reservations/';
+    print 'details.pl?reservation_id=' . $row->{reservation_id} . '">';
+    print      $row->{reservation_tag}; 
+    print '    </a></td>', "\n";
   
-    print '    <td><script language="javascript">print_current_date("", ' . $row->{reservation_start_time} . ", 'local');</script></td>\n";
-    print '    <td><script language="javascript">print_current_date("", ' . $row->{reservation_end_time} . ", 'local');</script></td>\n";
+    print '    <td>', "\n";
+    print '    <script language="javascript">';
+    print '    print_current_date("", ' . $row->{reservation_start_time};
+    print '                       , "local");';
+    print '    </script>', "\n";
+    print '    </td>', "\n";
+
+    print '    <td>';
+    print '    <script language="javascript">';
+    print '    print_current_date("", ' . $row->{reservation_end_time};
+    print '                      , "local");';
+    print '    </script>', "\n";
+    print '    </td>', "\n";
+
     print "    <td>" . $row->{reservation_status} . "</td>\n";
 
     $ip = get_oscars_host($row->{src_hostaddrs_id});
