af7cd4a02638866970a750389fa16b9aaacdd069
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-06-15 18:25:20 +0000

    Made into sub-class again, of Common::Database.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@895 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/rm/AAAS/Frontend/Database.pm
+++ oscars/rm/AAAS/Frontend/Database.pm
@@ -1,7 +1,8 @@
 package AAAS::Frontend::Database;
 
-# Database.pm:  package for AAAS database settings
-# Last modified: May 18, 2005
+# Database.pm:  package for AAAS database handling
+#               inherits from Common::Database
+# Last modified: June 14, 2005
 # Soo-yeon Hwang (dapi@umich.edu)
 # David Robertson (dwrobertson@lbl.gov)
 
@@ -9,6 +10,10 @@ use strict;
 
 use DBI;
 use Data::Dumper;
+
+use Common::Database;
+
+our @ISA = qw(Common::Database);
 
 ###############################################################################
 sub new {
@@ -24,42 +29,6 @@ sub new {
   
     return($_self);
 }
-
-sub initialize {
-    my ( $_self ) = @_;
-    # hash holds database handle for each connected user
-    $_self->{handles} = {};
-}
-######
-
-###############################################################################
-#
-sub check_connection
-{
-    my ( $self, $user_dn, $do_login, $reconnect ) = @_;
-    my ( %attr ) = (
-        RaiseError => 0,
-        PrintError => 0,
-    );
-    # TODO:  FIX, may need views, or different db logins for one thing
-    if ($reconnect || !(defined($self->{handles}->{$user_dn}))) {
-        if ($do_login) {
-            $self->{handles}->{$user_dn} = DBI->connect(
-                 $self->{configs}->{use_AAAS_database}, 
-                 $self->{configs}->{AAAS_login_name}, 
-                 'ritazza6',
-                 \%attr)
-        }
-        else {
-            return( "You must log in first before accessing the database");
-        }
-    }
-    if (!$self->{handles}->{$user_dn}) {
-        return( "Unable to make database connection: $DBI::errstr");
-    }
-    return "";
-}
-######
 
 ###############################################################################
 #
@@ -78,48 +47,6 @@ sub get_user_levels {
 }
  
 ######
-
-###############################################################################
-#
-sub do_query
-{
-    my( $self, $user_dn, $query, @args ) = @_;
-    my( $sth, $error_msg );
-
-    $sth = $self->{handles}->{$user_dn}->prepare( $query );
-    if ($DBI::err) {
-        $error_msg = "[DBERROR] Preparing $query:  $DBI::errstr";
-        return (undef, $error_msg);
-    }
-    $sth->execute( @args );
-    if ( $DBI::err ) {
-        $error_msg = "[DBERROR] Executing $query:  $DBI::errstr";
-        $sth->finish();
-        return(undef, $error_msg);
-    }
-    return( $sth, '');
-}
-######
-
-###############################################################################
-sub logout {
-    my( $self, $user_dn ) = @_;
-
-    my $results = {};
-    if (!$self->{handles}->{$user_dn}) {
-        $results->{status_msg} = 'Already logged out.';
-        return ( 0, $results );
-    }
-    if (!$self->{handles}->{$user_dn}->disconnect()) {
-        $results->{error_msg} = "Could not disconnect from database";
-        return ( 1, $results );
-    }
-    $self->{handles}->{$user_dn} = undef;
-    $results->{status_msg} = 'Logged out';
-    return ( 0, $results );
-}
-######
-
 
 # Don't touch the line below
 1;
