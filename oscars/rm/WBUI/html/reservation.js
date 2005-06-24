e0a0b34555333b6c90f5db22b0be39fdfd6abc1e
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-06-24 23:50:10 +0000

    Stylistic changes, and enforcing more error checking.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@970 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/rm/WBUI/html/reservation.js
+++ oscars/rm/WBUI/html/reservation.js
@@ -19,6 +19,10 @@ function check_form( form )
     }
     */
 
+    // Check for empty required fields, and whether non-blank fields have
+    // proper values for each field, in the order of: check whether its value
+    // is a valid number/integer, check whether it's in a proper range, and
+    // then check other conditions 
     if ( isblank(form.src_address.value) )
     {
         alert( "Please enter starting host name, or its IP address ." );
@@ -31,37 +35,73 @@ function check_form( form )
         form.dst_address.focus();
         return false;
     }
-    if ( isblank(form.reservation_bandwidth.value) )
-    {
+    if ( isblank(form.reservation_bandwidth.value) ) {
         alert( "Please enter the amount of bandwidth that you want to reserve." );
         form.reservation_bandwidth.focus();
         return false;
     }
+    else if ( validate_numeric(form.reservation_bandwidth.value) == false ) {
+        alert( "The bandwidth must be a positive integer." );
+        form.reservation_bandwidth.focus();
+        return false;
+    }
+    else if ( (form.reservation_bandwidth.value < 10 ) ||
+             (form.reservation_bandwidth.value >= 10000)) {
+        alert( "The amount of bandwidth must be in the range 10-10000 Mbps." );
+        form.reservation_bandwidth.focus();
+        return false;
+    }
 
-    if ( isblank(form.reservation_description.value) )
-    {
+    if ( isblank(form.reservation_description.value) ) {
         alert( "Please describe the purpose of this reservation request." );
         form.reservation_description.focus();
         return false;
     }
 
-    // finished checking empty fields
-    // now starting to check whether some fields have proper values
-    // for each field, in the order of: check whether its value is a valid number/integer, check whether it's in a proper range, and then check other conditions 
     if ( form.src_address.value == form.dst_address.value ) {
-        alert( "Please provide different IP adresses for origin and destination." );
+        alert( "Please provide different IP addresses for the source and destination." );
         form.src_address.focus();
         return false;
     }
-    if ( validate_numeric(form.reservation_bandwidth.value) == false ) {
-        alert( "The bandwidth is not a number. Please check again." );
-        form.reservation_bandwidth.focus();
-        return false;
+    // check non-required fields if a value has been entered
+    if ( !isblank(form.reservation_src_port.value) ) {
+        if ( validate_numeric(form.reservation_src_port.value) == false ) {
+            alert( "The source port must be a positive integer." );
+            form.reservation_src_port.focus();
+            return false;
+        }
+        else if ( (form.reservation_src_port.value < 1024) ||
+                (form.reservation_src_port.value > 65535) ) {
+            alert( "The source port, if given, must be in the range 1024-65535." );
+            form.reservation_src_port.focus();
+            return false;
+        }
     }
-    if ( form.reservation_bandwidth.value <= 0 ) {
-        alert( "The amount of bandwidth should be greater than 0. Please check again." );
-        form.reservation_bandwidth.focus();
-        return false;
+    if ( !isblank(form.reservation_dst_port.value) ) {
+        if ( validate_numeric(form.reservation_dst_port.value) == false ) {
+            alert( "The destination port must be a positive integer." );
+            form.reservation_dst_port.focus();
+            return false;
+        }
+        else if ( (form.reservation_dst_port.value < 1024) ||
+                (form.reservation_dst_port.value > 65535) ) {
+            alert( "The destination port, if given, must be in the range 1024-65535." );
+            form.reservation_dst_port.focus();
+            return false;
+        }
+    }
+    if ( !isblank(form.reservation_dscp.value) ) {
+        if ( validate_numeric(form.reservation_dscp.value) == false ) {
+            alert( "The DSCP must be a positive integer." );
+            form.reservation_dscp.focus();
+            return false;
+        }
+        else if ( (form.reservation_dscp.value < 0) ||
+                (form.reservation_dscp.value > 63) ) {
+            alert( "The DSCP, if given, must be in the range 0-63." );
+            form.reservation_dscp.focus();
+            return false;
+        }
     }
     // at this point success only depends on a correct date
     return check_date(form);
