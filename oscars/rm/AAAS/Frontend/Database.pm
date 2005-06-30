0aa64857445acf999167f74be12daac8c0de721a
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-06-30 23:52:53 +0000

    Moved AAAS-specific material from Common.  Added check_login, which is called from the BSS.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@1037 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/rm/AAAS/Frontend/Database.pm
+++ oscars/rm/AAAS/Frontend/Database.pm
@@ -2,7 +2,7 @@ package AAAS::Frontend::Database;
 
 # Database.pm:  package for AAAS database handling
 #               inherits from Common::Database
-# Last modified: June 14, 2005
+# Last modified: June 30, 2005
 # Soo-yeon Hwang (dapi@umich.edu)
 # David Robertson (dwrobertson@lbl.gov)
 
@@ -29,6 +29,84 @@ sub new {
   
     return($_self);
 }
+
+###############################################################################
+sub login_user {
+    my( $self, $user_dn ) = @_;
+
+    #print STDERR "Login called with $user_dn\n";
+    $self->Common::Database::login_user($user_dn);
+    if ($user_dn ne 'unpriv') {
+        $self->update_user_status($user_dn, 'Logged in');
+    }
+}
+######
+
+###############################################################################
+sub logout {
+    my( $self, $user_dn ) = @_;
+
+    my $results = {};
+    if (!$self->{handles}->{$user_dn}) {
+        $results->{status_msg} = 'Already logged out.';
+        return ( 0, $results );
+    }
+    if ($user_dn ne 'unpriv') {
+        $self->update_user_status($user_dn, 'Logged out');
+    }
+    if (!$self->{handles}->{$user_dn}->disconnect()) {
+        $results->{error_msg} = "Could not disconnect from database";
+        return ( 1, $results );
+    }
+    if ($user_dn ne 'unpriv') {
+        $self->{handles}->{$user_dn} = undef;
+    }
+    $results->{status_msg} = 'Logged out';
+    return ( 0, $results );
+}
+######
+
+###############################################################################
+sub update_user_status {
+    my( $self, $user_dn, $status ) = @_;
+
+    my( $query, $sth, $err_msg );
+
+    $query = "UPDATE users SET user_status = ? WHERE user_dn = '$user_dn'";
+    ($sth, $err_msg) = $self->do_query($user_dn, $query, $status);
+    if ($err_msg) { return $err_msg; }
+    return ( "" );
+}
+######
+
+###############################################################################
+# enforce_connection:  Checks to see if user has logged in.
+#
+sub enforce_connection {
+    my ( $self, $user_dn ) = @_;
+
+    my ( $query, $sth, $err_msg );
+
+    $query = "SELECT user_status FROM users WHERE user_dn = ?";
+    ($sth, $err_msg) = $self->do_query('', $query, $user_dn);
+    if ($err_msg) { return $err_msg; }
+
+    if (!$sth->rows) {
+        $sth->finish();
+        $err_msg = 'Please check your login name and try again.';
+        return $err_msg;
+    }
+    my $ref = $sth->fetchrow_hashref();
+    if (!$ref->{user_status} || ($ref->{user_status} eq 'Logged out')) {
+        return( "You must log in first before accessing the database");
+    }
+    elsif (!$self->{handles}->{$user_dn}) {
+        $err_msg = $self->login_user($user_dn);
+        if ($err_msg) { return( 1, $err_msg) }
+    }
+    return "";
+}
+######
 
 ###############################################################################
 #
