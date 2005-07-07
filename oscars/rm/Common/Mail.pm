2088a41f1eb48753ea71a8188d71b174b992b542
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-07-07 23:17:57 +0000

    Now sending to oscars-admin.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@1073 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/rm/Common/Mail.pm
+++ oscars/rm/Common/Mail.pm
@@ -2,7 +2,7 @@ package Common::Mail;
 
 # Mail.pm:  handles sending notification mail messages
 # 
-# Last modified: June 27, 2005
+# Last modified: July 7, 2005
 # David Robertson (dwrobertson@lbl.gov)
 # Soo-yeon Hwang (dapi@umich.edu)
 
@@ -73,9 +73,8 @@ sub get_webmaster {
 sub get_admins {
     my( $self ) = @_;
 
-    # for now
     #return( 'dwrobertson@lbl.gov' );
-    return( 'chin@es.net dwrobertson@lbl.gov' );
+    return( 'oscars-admin@es.net' );
 }
 ######
 
