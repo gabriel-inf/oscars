2e8643c069c59d1fda7e1d540435012dd1cd36a2
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-07-07 02:55:20 +0000

    Simplified dispatching (choosing methods done on server now).
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@1064 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/rm/BSS/Client/SOAPClient.pm
+++ oscars/rm/BSS/Client/SOAPClient.pm
@@ -7,68 +7,23 @@ use Exporter;
 
 our @ISA = qw(Exporter);
 
-our @EXPORT = qw( soap_logout_user soap_get_reservations soap_create_reservation soap_delete_reservation );
+our @EXPORT = qw( bss_dispatcher );
  
 
 my $BSS_server = SOAP::Lite
-  -> uri('http://localhost:3000/BSS/Scheduler/ReservationHandler')
+-> uri('http://localhost:3000/Dispatcher')
   -> proxy ('http://localhost:3000/BSS_server.pl');
 
 
 ##############################################################################
-# soap_logout:  Called after AAAS::soap_logout by CGI script.
+# bss_dispatcher:  calls BSS SOAP server
 #
-sub soap_logout_user {
+sub bss_dispatcher {
     my ($params) = @_;
 
-    my $response = $BSS_server->logout($params);
-    if ($response->fault) {
-        print STDERR $response->faultcode, " ", $response->faultstring, "\n";
-    }
-    return ($response->result(), $response->paramsout());
+    return $BSS_server->dispatch($params);
 }
 ######
 
-##############################################################
-# Client calls to BSS front end, invoked from user forms.
-##############################################################
-
-##############################################################################
-#
-sub soap_get_reservations {
-    my ( $params ) = @_;
-
-    my $response = $BSS_server->get_reservations($params);
-    if ($response->fault) {
-        print STDERR $response->faultcode, " ", $response->faultstring, "\n";
-    }
-        #  params are either user profile, or error message
-    return ($response->result(), $response->paramsout());
-}
 ######
-
-##############################################################################
-#
-sub soap_create_reservation {
-    my ($params) = @_;
-    my $response = $BSS_server->create_reservation($params);
-    if ($response->fault) {
-        print STDERR $response->faultcode, " ", $response->faultstring, "\n";
-    }
-        #  params are either user profile, or error message
-    return ($response->result(), $response->paramsout());
-}
-######
-
-##############################################################################
-#
-sub soap_delete_reservation {
-    my ($params) = @_;
-    my $response = $BSS_server->delete_reservation($params);
-    if ($response->fault) {
-        print STDERR $response->faultcode, " ", $response->faultstring, "\n";
-    }
-        #  params are either user profile, or error message
-    return ($response->result(), $response->paramsout());
-}
-######
+1;
