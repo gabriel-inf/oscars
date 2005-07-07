a6b957500754e8b547a31209cac1610620ca4b15
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-07-07 02:55:03 +0000

    Simplified dispatching (done on server now).
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@1063 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/rm/AAAS/Client/SOAPClient.pm
+++ oscars/rm/AAAS/Client/SOAPClient.pm
@@ -6,7 +6,7 @@ use Exporter;
 
 our @ISA = qw(Exporter);
 
-our @EXPORT = qw( soap_verify_login soap_check_login soap_logout soap_get_profile soap_set_profile soap_get_userlist);
+our @EXPORT = qw( aaas_dispatcher );
 
 
 ######################################
@@ -14,102 +14,17 @@ our @EXPORT = qw( soap_verify_login soap_check_login soap_logout soap_get_profil
 ######################################
 
 my $AAAS_server = SOAP::Lite
-  -> uri('http://localhost:2000/AAAS/Frontend/User')
+  -> uri('http://localhost:2000/Dispatcher')
   -> proxy ('http://localhost:2000/AAAS_server.pl');
 
 
 
-################################
-# Methods called from user forms
-################################
-
 ##############################################################################
 #
-sub soap_verify_login
+sub aaas_dispatcher
 {
     my ($params) = @_;
-    my $response = $AAAS_server->verify_login($params);
-    if ($response->fault) {
-        print STDERR $response->faultcode, " ", $response->faultstring, "\n";
-    }
-    return ($response->result(), $response->paramsout());
-}
-######
-
-##############################################################################
-#
-sub soap_get_profile {
-    my ( $params ) = @_;
-
-    my $response = $AAAS_server->get_profile($params);
-    if ($response->fault) {
-        print $response->faultcode, " ", $response->faultstring, "\n";
-    }
-        #  params are either user profile, or error message
-    return ($response->result(), $response->paramsout());
-}
-######
-
-##############################################################################
-#
-sub soap_set_profile {
-    my ( $params ) = @_;
-
-    my $response = $AAAS_server->set_profile($params);
-    if ($response->fault) {
-        print $response->faultcode, " ", $response->faultstring, "\n";
-    }
-        #  params are either user profile, or error message
-    return ($response->result(), $response->paramsout());
-}
-######
-
-##############################################################################
-#
-sub soap_logout {
-    my ( $params ) = @_;
-  
-    my $response = $AAAS_server->logout($params);
-    if ($response->fault) {
-        print $response->faultcode, " ", $response->faultstring, "\n";
-    }
-        #  params are either user profile, or error message
-    return ($response->result(), $response->paramsout());
-}
-######
-    
-
-#################################
-# Methods called from admin forms
-#################################
-
-##############################################################################
-#
-sub soap_get_userlist
-{
-    my ( $params ) = @_;
-    my $response = $AAAS_server->get_userlist($params);
-    if ($response->fault) {
-        print $response->faultcode, " ", $response->faultstring, "\n";
-    }
-    return ($response->result(), $response->paramsout());
-}
-######
-
-################################
-# Methods called from BSS 
-################################
-
-##############################################################################
-#
-sub soap_check_login
-{
-    my ($params) = @_;
-    my $response = $AAAS_server->check_login_status($params);
-    if ($response->fault) {
-        print STDERR $response->faultcode, " ", $response->faultstring, "\n";
-    }
-    return ($response->result(), $response->paramsout());
+    return $AAAS_server->dispatch($params);
 }
 ######
 
