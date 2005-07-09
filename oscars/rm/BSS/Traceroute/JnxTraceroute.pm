df06ab2ef17e6a623b04631490180142c19d72bb
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-07-09 03:59:21 +0000

    Using exceptions with Error package now rather than propagated returned errors.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@1088 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/rm/BSS/Traceroute/JnxTraceroute.pm
+++ oscars/rm/BSS/Traceroute/JnxTraceroute.pm
@@ -8,6 +8,9 @@ package BSS::Traceroute::JnxTraceroute;
 
 use Config::Auto;
 use Data::Dumper;
+use Error qw(:try);
+
+use Common::Exception;
 
 use strict;
 
@@ -23,15 +26,15 @@ use strict;
 #
 sub new
 {
-    my ($_class, %_args) = @_;
-    my ($_self) = {%_args};
+    my ($class, %args) = @_;
+    my ($self) = {%args};
 
-    # Bless $_self into designated class.
-    bless($_self, $_class);
+    # Bless $self into designated class.
+    bless($self, $class);
 
     # Initialize.
-    $_self->initialize();
-    return($_self);
+    $self->initialize();
+    return($self);
 }
 ######
 
@@ -42,52 +45,37 @@ sub new
 #
 sub traceroute
 {
-    my ($_self, $_src, $_dst) = @_;
-    my ($_hopInfo, $_hopCount, $_cmd);
+    my ($self, $src, $dst) = @_;
+    my ($hopInfo, $hopCount, $cmd);
 
     # Clear error message.
-    $_self->{errMsg} = 0;
-    if ( !defined($_src) || !defined($_dst) )  {
-        $_self->{errMsg} = "ERROR: Traceroute source or destination not defined\n";
-        return(0);
+    if ( !defined($src) || !defined($dst) )  {
+        throw Common::Exception("Traceroute source or destination not defined.");
     }
 
     # Perform the traceroute.
-    $_cmd = "ssh -x -a -i $_self->{config}->{jnx_key} -l $_self->{config}->{jnx_user} $_src traceroute $_dst wait 1";
-    print STDERR "$_cmd\n";
-    if (not(open(_TRACEROUTE_, "$_cmd 2>/dev/null |")))  {
-        $_self->{errMsg} = "ERROR: Unable to ssh into router and perform traceroute\n";
-        return(0);
+    $cmd = "ssh -x -a -i $self->{config}->{jnx_key} -l $self->{config}->{jnx_user} $src traceroute $dst wait 1";
+    print STDERR "$cmd\n";
+    if (not(open(_TRACEROUTE_, "$cmd 2>/dev/null |")))  {
+        throw Common::Exception("Unable to ssh into router and perform traceroute.");
     }
 
     # Reset hop information.
-    $_hopCount = 0;
-    undef($_self->{rawHopData});
-    undef($_self->{hops});
+    $hopCount = 0;
+    undef($self->{rawHopData});
+    undef($self->{hops});
 
     # Parse the results.
-    while ($_hopInfo = <_TRACEROUTE_>)  {
-        $_self->{rawHopData}[$_hopCount] = $_hopInfo;
+    while ($hopInfo = <_TRACEROUTE_>)  {
+        $self->{rawHopData}[$hopCount] = $hopInfo;
 
         # Get the hop IP address from output, e.g.
         #  1  esnet3-lbl3.es.net (198.129.76.26)  0.628 ms  0.569 ms  0.522 ms
-        next if ($_hopInfo !~ m/\((\d+\.\d+\.\d+\.\d+)\)/);
-        $_self->{hops}[$_hopCount] = $1;
-        $_hopCount++;
+        next if ($hopInfo !~ m/\((\d+\.\d+\.\d+\.\d+)\)/);
+        $self->{hops}[$hopCount] = $1;
+        $hopCount++;
     }
     return(1);
-}
-######
-
-##############################################################################
-# get_error:  Return the error message (0 if none).
-# In:  <none>
-# Out: Error message
-#
-sub get_error {
-    my ($_self) = @_;
-
-    return($_self->{errMsg});
 }
 ######
 
@@ -99,9 +87,9 @@ sub get_error {
 #
 sub get_raw_hop_data
 {
-    my ($_self) = @_;
+    my ($self) = @_;
 
-    return(@{$_self->{rawHopData}});
+    return(@{$self->{rawHopData}});
 }
 ######
 
@@ -112,9 +100,9 @@ sub get_raw_hop_data
 #
 sub get_hops
 {
-    my ($_self) = @_;
+    my ($self) = @_;
 
-    return(@{$_self->{hops}});
+    return(@{$self->{hops}});
 }
 ######
 
@@ -130,11 +118,9 @@ sub get_hops
 #
 sub initialize 
 {
-    my ($_self) = @_;
+    my ($self) = @_;
 
-    $_self->{config} = Config::Auto::parse($ENV{OSCARS_HOME} . '/oscars.cfg');
-    # clear error message
-    $_self->{errMsg} = 0;
+    $self->{config} = Config::Auto::parse($ENV{OSCARS_HOME} . '/oscars.cfg');
 }
 ######
 
