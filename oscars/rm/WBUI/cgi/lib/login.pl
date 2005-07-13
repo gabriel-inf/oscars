20141a5b5cd8794deaa779684238ccac4951ea1e
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-07-13 18:39:06 +0000

    $results->{error_msg} no longer used
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@1108 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/rm/WBUI/cgi/lib/login.pl
+++ oscars/rm/WBUI/cgi/lib/login.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w
 
 # login.pl:  Main Service Login script
-# Last modified: July 6, 2005
+# Last modified: July 13, 2005
 # Soo-yeon Hwang (dapi@umich.edu)
 # David Robertson (dwrobertson@lbl.gov)
 
@@ -16,18 +16,9 @@ my $cgi = CGI->new();
 # Check that the user exists, the correct password has been given, the user
 # account has been activated, and the user has the proper privilege level
 # to perform database operations.
-my ($err_msg, $som) = verify_user($cgi);
-if ($err_msg) {
-    update_status_frame(1, $err_msg); 
-    exit;
-}
-
+$som = verify_user($cgi);
 $results = $som->result;
 
-if ($results->{error_msg}) {
-    update_status_frame(1, $results->{error_msg}); 
-    exit;
-}
 my ($user_dn, $user_level, $oscars_home) = check_session_status($results, $cgi);
 print_info($user_dn, $user_level);
 update_status_frame(0, "User $user_dn successfully logged in.");
@@ -48,15 +39,21 @@ sub verify_user {
 
     # validate user input (just check for empty fields)
     if ( !$cgi->param('user_dn') ) {
-        return( 'Please enter your login name', undef );
+        update_status_frame(1, 'Please enter your login name'); 
+        exit;
     }
     if ( !$cgi->param('user_password') ) {
-        return( 'Please enter your password.', undef );
+        update_status_frame(1, 'Please enter your password'); 
+        exit;
     }
     $soap_params{user_dn} = $cgi->param('user_dn');
     $soap_params{user_password} = $cgi->param('user_password');
     $soap_params{method} = 'soap_verify_login'; 
     my $som = aaas_dispatcher(\%soap_params);
-    return ( '', $som );
+    if ($som->faultstring) {
+        update_status_frame(1, $som->faultstring); 
+        exit;
+    }
+    return ( $som );
 }
 ######
