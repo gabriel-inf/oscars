c825184480c9a365fad2aaf3b3062b989f64cf26
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-07-27 23:17:58 +0000

    Stylistic changes.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@1142 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/rm/WBUI/html/scripts/userprofile.js
+++ oscars/rm/WBUI/html/scripts/userprofile.js
@@ -1,7 +1,8 @@
 /*
 Javascript functions for main service: my profile interface
-Last modified: October 16, 2004
+Last modified: July 27, 2004
 Soo-yeon Hwang (dapi@umich.edu)
+David Robertson (dwrobertson@lbl.gov)
 */
 
 /* List of functions:
@@ -9,59 +10,51 @@ check_form( form )
 */
 
 // check user input and validate it
-function check_form( form )
-{
+function check_form( form ) {
 
-	if ( isblank(form.user_password.value) )
-	{
-		alert( "Please enter the current password." );
-		form.user_password.focus();
-		return false;
-	}
+    if ( isblank(form.user_password.value) ) {
+        alert( "Please enter the current password." );
+        form.user_password.focus();
+        return false;
+    }
 
-	if ( !(isblank(form.password_new_once.value)) )
-        {
-	    if ( form.password_new_once.value != form.password_new_twice.value ) {
-		    alert( "Please enter the same new password twice for verification." );
-		    form.password_new_once.focus();
-		    return false;
-            }
-            form.update_password.value = 1;
-	}
+    if ( !(isblank(form.password_new_once.value)) ) {
+        if ( form.password_new_once.value != form.password_new_twice.value ) {
+            alert( "Please enter the same new password twice for verification." );
+            form.password_new_once.focus();
+            return false;
+        }
+        form.update_password.value = 1;
+    }
 
-	if ( isblank(form.user_first_name.value) )
-	{
-		alert( "Please enter the first name." );
-		form.user_first_name.focus();
-		return false;
-	}
+    if ( isblank(form.user_first_name.value) ) {
+        alert( "Please enter the first name." );
+        form.user_first_name.focus();
+        return false;
+    }
 
-	if ( isblank(form.user_last_name.value) )
-	{
-		alert( "Please enter the last name." );
-		form.user_last_name.focus();
-		return false;
-	}
+    if ( isblank(form.user_last_name.value) ) {
+        alert( "Please enter the last name." );
+        form.user_last_name.focus();
+        return false;
+    }
 
-	if ( isblank(form.institution.value) )
-	{
-		alert( "Please enter the organization." );
-		form.institution.focus();
-		return false;
-	}
+    if ( isblank(form.institution.value) ) {
+        alert( "Please enter the organization." );
+        form.institution.focus();
+        return false;
+    }
 
-	if ( isblank(form.user_email_primary.value) )
-	{
-		alert( "Please enter the primary e-mail address." );
-		form.user_email_primary.focus();
-		return false;
-	}
+    if ( isblank(form.user_email_primary.value) ) {
+        alert( "Please enter the primary e-mail address." );
+        form.user_email_primary.focus();
+        return false;
+    }
 
-	if ( isblank(form.user_phone_primary.value) )
-	{
-		alert( "Please enter the primary phone number." );
-		form.user_phone_primary.focus();
-		return false;
-	}
-	return true;
+    if ( isblank(form.user_phone_primary.value) ) {
+        alert( "Please enter the primary phone number." );
+        form.user_phone_primary.focus();
+        return false;
+    }
+    return true;
 }
