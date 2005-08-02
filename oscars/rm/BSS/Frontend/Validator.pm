bd0e09538ecc7f0f81bef0b7c3b24b0f4e020982
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-08-02 21:49:26 +0000

    Making server-side validation functionality a separate class.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@1154 c75a6b91-02ef-0310-b74a-e20389c9c41d

---
+++ oscars/rm/BSS/Frontend/Validator.pm
@@ -0,0 +1,107 @@
+# Validator.pm:  Input validation
+# Last modified: August 2, 2005
+# David Robertson (dwrobertson@lbl.gov)
+
+package BSS::Frontend::Validator;
+
+use strict;
+
+use Data::Dumper;
+
+my $form_variables = {
+    'insert_reservation' => {
+        'reservation_start_time' => (
+            {'regexp' => '.+',
+             'error' => "Please enter the reservation starting time."
+            }
+        ),
+        'duration_hour' => (
+            {'regexp' => '.+',
+             'error' => "Please enter the duration in hours."
+            }
+        ),
+        'src_address' => (
+            {'regexp' => '.+',
+             'error' => "Please enter starting host name or IP address."
+            }
+        ),
+        'dst_address' => (
+            {'regexp' => '.+',
+             'error' => "Please enter destination host name or IP address."
+            }
+        ),
+        'reservation_bandwidth' => (
+            {'regexp' => '.+',
+             'error' => "Please enter the bandwidth you wish to reserve."
+            }
+        ),
+        'reservation_description' => (
+            {'regexp' => '.+',
+             'error' => "Please enter a description of the purpose for this reservation."
+            }
+        ),
+    },
+    # no tests yet
+    'delete_reservation' => {},
+    'get_reservations' => {}
+};
+
+###############################################################################
+sub new {
+    my ($_class, %_args) = @_;
+    my ($_self) = {%_args};
+  
+    # Bless $_self into designated class.
+    bless($_self, $_class);
+  
+    # Initialize.
+    $_self->initialize();
+  
+    return($_self);
+}
+
+sub initialize {
+    my ($self) = @_;
+}
+######
+
+###############################################################################
+## validate:  input validation
+#
+sub validate {
+    my( $self, $inref ) = @_;
+
+    my( $m, $k, $r );
+
+    $m = $form_variables->{$inref->{method}};
+    if (!$m) {
+        return "Cannot validate $inref->{method}";
+    }
+    # validate form variables for that method
+    for $k (keys %{$m}) {
+        # set to blank string if not defined
+        if (!defined($inref->{$k})) {
+            $inref->{$k} = '';
+        }
+        # for all tests 
+        for $r ($m->{$k}) {
+            if ($inref->{$k} !~ $r->{regexp}) {
+                return $r->{error};
+            }
+        }
+    }
+    return "";
+}
+######
+
+sub numeric_compare {
+    my( $self, $val, $lesser_val, $greater_val ) = @_;
+
+    if ($lesser_val > $val) { return 0; }
+    if ($greater_val < $val) { return 0; }
+    return 1;
+}
+######
+
+######
+1;
