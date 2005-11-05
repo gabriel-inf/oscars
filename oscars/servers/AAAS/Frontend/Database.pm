fa79c1c5541275cc05c1a2fee55860d8a986f91d
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-11-05 23:10:53 +0000

    Removing per-user database handle, login, and logout.  User status will be determined from last active time.  Removed user_dn arg to do_query.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@1355 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/servers/AAAS/Frontend/Database.pm
+++ oscars/servers/AAAS/Frontend/Database.pm
@@ -2,7 +2,7 @@ package AAAS::Frontend::Database;
 
 # Database.pm:  package for AAAS database handling
 #               inherits from Common::Database
-# Last modified: October 18, 2005
+# Last modified: November 5, 2005
 # David Robertson (dwrobertson@lbl.gov)
 
 use strict;
@@ -32,76 +32,6 @@ sub new {
 }
 
 ###############################################################################
-sub login_user {
-    my( $self, $user_dn ) = @_;
-
-    #print STDERR "Login called with $user_dn\n";
-    $self->Common::Database::login_user($user_dn);
-    if ($user_dn ne 'unpriv') {
-        $self->update_user_status($user_dn, 'Logged in');
-    }
-}
-######
-
-###############################################################################
-sub logout {
-    my( $self, $user_dn ) = @_;
-
-    my $results = {};
-    if (!$self->{oscars_logins}->{$user_dn}) {
-        throw Common::Exception("Already logged out.");
-    }
-    if ($user_dn ne 'unpriv') {
-        $self->update_user_status($user_dn, 'Logged out');
-    }
-    if (!$self->{oscars_logins}->{$user_dn}->disconnect()) {
-        throw Common::Exception("Could not disconnect from database");
-    }
-    if ($user_dn ne 'unpriv') {
-        $self->{oscars_logins}->{$user_dn} = undef;
-    }
-    return ( $results );
-}
-######
-
-###############################################################################
-sub update_user_status {
-    my( $self, $user_dn, $status ) = @_;
-
-    my( $query, $sth );
-
-    $query = "UPDATE users SET user_status = ? WHERE user_dn = '$user_dn'";
-    $sth = $self->do_query($user_dn, $query, $status);
-    return;
-}
-######
-
-###############################################################################
-# enforce_connection:  Checks to see if user has logged in.
-#
-sub enforce_connection {
-    my ( $self, $user_dn ) = @_;
-
-    my ( $query, $sth );
-
-    $query = "SELECT user_status FROM users WHERE user_dn = ?";
-    $sth = $self->do_query('', $query, $user_dn);
-
-    if (!$sth->rows) {
-        $sth->finish();
-        throw Common::Exception("Please check your login name and try again.");
-    }
-    my $ref = $sth->fetchrow_hashref();
-    if (!$ref->{user_status} || ($ref->{user_status} eq 'Logged out')) {
-        throw Common::Exception("You must log in first before accessing the database.");
-    }
-    # for now, handle set up per connection
-    $self->login_user($user_dn);
-    return;
-}
-######
-
-###############################################################################
 #
 sub get_user_levels {
     my( $self, $user_dn ) = @_;
@@ -109,7 +39,7 @@ sub get_user_levels {
     my( %levels, $r, $sth, $query );
 
     $query = "SELECT user_level_bit, user_level_description FROM user_levels";
-    $sth = $self->do_query($user_dn, $query);
+    $sth = $self->do_query($query);
     my $rows = $sth->fetchall_arrayref();
     for $r (@$rows) { $levels{$$r[1]} = $$r[0]; }
     $levels{'inactive'} = 0;
@@ -126,7 +56,7 @@ sub get_institution_id {
 
     $query = "SELECT institution_id FROM institutions
               WHERE institution_name = ?";
-    $sth = $self->do_query($user_dn, $query, $inref->{institution});
+    $sth = $self->do_query($inref->{institution});
     if (!$sth->rows) {
         $sth->finish();
         throw Common::Exception("The organization " .
