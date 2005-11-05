666bec685c66f7b831bdc6dbee067a4ec396f73b
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-11-05 23:09:58 +0000

    In process of changing to different mode of authorization.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@1354 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/servers/AAAS/Frontend/Auth.pm
+++ oscars/servers/AAAS/Frontend/Auth.pm
@@ -1,7 +1,7 @@
 package AAAS::Frontend::Auth;
 
 # Auth.pm:  Database interactions dealing with authorization.
-# Last modified: July 8, 2005
+# Last modified: November 5, 2005
 # Soo-yeon Hwang (dapi@umich.edu)
 # David Robertson (dwrobertson@lbl.gov)
 
@@ -63,47 +63,11 @@ sub get_str_level {
 
 ###############################################################################
 #
-sub get_numeric_level {
-    my( $self, $level_str ) = @_;
+sub authorized {
+    my( $self, $user_dn, $resource ) = @_;
 
-    my( @privs, $p, $numeric_level );
-
-    $numeric_level = $self->{user_levels}{readonly};
-    @privs = split(' ', $level_str);
-    for $p (@privs) {
-        if ($p eq 'user') {
-            $numeric_level |= $self->{user_levels}{user};
-        }
-        elsif ($p eq 'engr') {
-            $numeric_level |= $self->{user_levels}{engr};
-        }
-        elsif ($p eq 'admin') {
-            $numeric_level |= $self->{user_levels}{admin};
-        }
-    }
-    return( $numeric_level );
-}
-######
-
-###############################################################################
-#
-sub verify {
-    my( $self, $user_priv, $required_privs, $convert ) = @_;
-
-    if ($convert) {
-        $user_priv = $self->get_numeric_level($user_priv);
-    }
-    $required_privs = $self->get_numeric_level($required_privs);
-    # See if user has been activated
-    if ( $user_priv == $self->{user_levels}->{inactive} ) {
-        throw Common::Exception("This account is not authorized or activated yet.");
-    }
-    # next, see if user has at least the required privileges
-    elsif (!($user_priv & $required_privs)) {
-        throw Common::Exception("This function requires the following privileges: " .
-                     $self->get_str_level($required_privs));
-    }
-    return;
+    # TODO
+    return 1;
 }
 ######
 
