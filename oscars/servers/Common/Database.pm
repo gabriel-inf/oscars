81c66554174ae8b7c1ecb654e67740af7067466c
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-11-05 22:01:58 +0000

    Removing user_dn (authorization required for each request) and database handle for each user.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@1353 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/servers/Common/Database.pm
+++ oscars/servers/Common/Database.pm
@@ -1,7 +1,7 @@
 package Common::Database;
 
 # Database.pm:  superclass for AAAS and BSS database handling
-# Last modified: October 18, 2005
+# Last modified: November 5, 2005
 # David Robertson (dwrobertson@lbl.gov)
 # Soo-yeon Hwang (dapi@umich.edu)
 
@@ -29,16 +29,6 @@ sub new {
 
 sub initialize {
     my ( $self ) = @_;
-    # hash holds database handle for each connected user
-    $self->{oscars_logins} = {};
-}
-######
-
-###############################################################################
-# login_user:  Sets up database handle for user
-#
-sub login_user {
-    my ( $self, $user_dn ) = @_;
 
     my ( %attr ) = (
         RaiseError => 0,
@@ -49,42 +39,20 @@ sub login_user {
     # correctly.  So instead,
     # if a handle is left over from a previous session, attempts to disconnect.
     # If it was timed out, the error is ignored.
-    if ($self->{oscars_logins}->{$user_dn}) {
-        $self->{oscars_logins}->{$user_dn}->disconnect();
+    if ($self->{dbh}) {
+        $self->{dbh}->disconnect();
     }
 
     # Start with a fresh handle.
-    $self->{oscars_logins}->{$user_dn} = DBI->connect(
+    $self->{dbh} = DBI->connect(
                  $self->{database}, 
                  $self->{dblogin}, 
                  $self->{password},
                  \%attr);
-    if (!$self->{oscars_logins}->{$user_dn}) {
+    if (!$self->{dbh}) {
         throw Common::Exception( "Unable to make database connection: $DBI::errstr");
     }
     return;
-}
-######
-
-###############################################################################
-# get_port:  gets next port number
-#
-sub get_port {
-    my( $self ) = @_;
-
-    my( $sth, $query );
-
-        # get default for now
-    $query = 'SELECT server_port ' .
-             'FROM servers WHERE server_id = 1';
-    $sth = $self->do_query('', $query);
-    if (!$sth->rows) {
-        $sth->finish();
-        throw Common::Exception("Could not find port number.");
-    }
-    my $ref = $sth->fetchrow_hashref();
-    $sth->finish();
-    return( $ref->{server_port} );
 }
 ######
 
@@ -113,15 +81,9 @@ sub get_debug_level {
 ###############################################################################
 #
 sub do_query {
-    my( $self, $user_dn, $query, @args ) = @_;
+    my( $self, $query, @args ) = @_;
 
-    if (!$user_dn) {
-        $user_dn = 'unpriv';
-        # Handle for unprivileged database access.
-        # Makes sure there is a fresh handle for that pseudo-user.
-        $self->login_user('unpriv');
-    }
-    my $sth = $self->{oscars_logins}->{$user_dn}->prepare( $query );
+    my $sth = $self->{dbh}->prepare( $query );
     if ($DBI::err) {
         throw Common::Exception("[DBERROR] Preparing $query:  $DBI::errstr");
     }
