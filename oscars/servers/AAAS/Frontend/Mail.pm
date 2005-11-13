15ab3f5035e7963120a5fef1819c4bc955448cd8
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-11-13 08:35:38 +0000

    Was in Common.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@1425 c75a6b91-02ef-0310-b74a-e20389c9c41d

---
+++ oscars/servers/AAAS/Frontend/Mail.pm
@@ -0,0 +1,78 @@
+package Frontend::Mail;
+
+# Mail.pm:  handles sending notification mail messages
+# 
+# Last modified: July 11, 2005
+# David Robertson (dwrobertson@lbl.gov)
+# Soo-yeon Hwang (dapi@umich.edu)
+
+use strict;
+
+use Data::Dumper;
+
+###############################################################################
+#
+sub new {
+    my( $class, %args ) = @_;
+    my( $self ) = { %args };
+  
+    bless( $self, $class );
+    $self->initialize();
+    return( $self );
+}
+
+sub initialize {
+    my ( $self ) = @_;
+    # email text encoding
+    $self->{notification_email_encoding} = 'ISO-8859-1';
+    $self->{sendmail_cmd} = '/usr/sbin/sendmail -oi';
+    $self->{webmaster} = 'dwrobertson@lbl.gov';
+}
+######
+
+###############################################################################
+# send_mail:  Mails message.  Used by both AAAS and BSS for notifications
+#
+sub send_mail {
+    my( $self, $sender, $recipient, $subject, $msg ) = @_;
+
+    if (!open(MAIL, "|$self->{sendmail_cmd} $recipient")) {
+        return( $! );           
+    }
+    print MAIL "From: $sender\n";
+    print MAIL "To:   $recipient\n";
+    print MAIL "Subject:  OSCARS: $subject\n";
+    print MAIL 'Content-Type: text/plain; charset="' .
+                   $self->{notification_email_encoding} . '"' . "\n\n";
+			
+    print MAIL $msg;
+    print MAIL "\n";
+    print MAIL "---------------------------------------------------\n";
+    print MAIL "=== This is an auto-generated e-mail ===\n";
+
+    if (!close( MAIL )) { return( $! ); }
+    return( "" );
+}
+######
+
+###############################################################################
+#
+sub get_webmaster {
+    my( $self ) = @_;
+
+    return( $self->{webmaster} );
+}
+######
+
+###############################################################################
+#
+sub get_admins {
+    my( $self ) = @_;
+
+    #return( 'oscars-admin@es.net' );
+    return( 'dwrobertson@lbl.gov chin@es.net' );
+}
+######
+
+# Don't touch the line below
+1;
