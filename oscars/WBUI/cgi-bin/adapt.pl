8428b971a67f37bc9bbbf283498894f7f0e3efce
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-11-16 00:03:11 +0000

    Attempting to minimize any nastiness with globals and mod_perl
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@1455 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/WBUI/cgi-bin/adapt.pl
+++ oscars/WBUI/cgi-bin/adapt.pl
@@ -1,21 +1,14 @@
 #!/usr/bin/perl -w
 
 # adapt.pl: Script called by all CGI forms.  Client::SOAPAdapter
-#           does all the work.
+#           (called by Client::Runner) does all the work.
 # Last modified:  November 15, 2005
 # David Robertson (dwrobertson@lbl.gov)
 
-use Client::SOAPAdapter;
+use strict;
+use Client::Runner;
 
-# TODO:  how to instantiate this only once and reuse
-#        (taking out "my" doesn't do the trick)
-my $soap_server = SOAP::Lite
-                 -> uri('http://198.128.14.164/Dispatcher')
-                 -> proxy ('https://198.128.14.164/SOAP');
-my $adapter = Client::SOAPAdapter->new();
-my $results = $adapter->make_soap_call($soap_server);
-$adapter->output($results);
-exit;
+Client::Runner::run();
 
 ######
 1;
