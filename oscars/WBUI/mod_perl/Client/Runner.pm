51a5e7feadbd4b746928169cef2de6c7642ae2ed
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-11-19 04:06:53 +0000

    Extracting method name from query string; any non-"method" pair in query string is added to a hash of arguments.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@1505 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/WBUI/mod_perl/Client/Runner.pm
+++ oscars/WBUI/mod_perl/Client/Runner.pm
@@ -1,7 +1,7 @@
 package Client::Runner;
 
 # Calls SOAPAdapter to make SOAP call and format results for output.
-# Last modified:  November 15, 2005
+# Last modified:  November 18, 2005
 # David Robertson (dwrobertson@lbl.gov)
 
 use strict;
@@ -13,14 +13,24 @@ use Client::SOAPAdapter;
 ###############################################################################
 #
 sub run {
+    my ( $method_name, %args );
+
     my $soap_server = SOAP::Lite
                           -> uri('http://198.128.14.164/Dispatcher')
                           -> proxy ('https://198.128.14.164/SOAP');
-    my $adapter = Client::SOAPAdapterFactory->new($ENV{'REQUEST_URI'},
-                                                  $ENV{'QUERY_STRING'});
-    my $soap_params = $adapter->pre_call();
+    my $query = $ENV{'QUERY_STRING'};
+    my @pairs = split(';', substr($query, 1));
+    for my $pair (@pairs) {
+        my ($key, $val) = split('=', $pair) ; 
+        if ($key eq 'method') { $method_name=$val; }
+        else { $args{$key} = $val; }
+    }
+    #my $uri = $ENV{'REQUEST_URI'};
+    my $factory = Client::SOAPAdapterFactory->new();
+    my $adapter = $factory->create_instance($method_name, \%args);
+    my $soap_params = $adapter->before_call();
     my $results = $adapter->make_call($soap_server, $soap_params);
-    $adapter->post_call($results);
+    $adapter->after_call($results);
     $adapter->output($results);
 }
 ######
