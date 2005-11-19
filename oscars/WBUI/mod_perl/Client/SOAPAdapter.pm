f832f578933329062d9d1ff465d1b3abcc6baadc
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-11-19 04:07:58 +0000

    Name changes for pre_call and post_call, preparing to add factory pattern functionality.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@1506 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/WBUI/mod_perl/Client/SOAPAdapter.pm
+++ oscars/WBUI/mod_perl/Client/SOAPAdapter.pm
@@ -1,4 +1,11 @@
+# Last modified:  November 18, 2005
+# David Robertson (dwrobertson@lbl.gov)
+
+use strict;
+
 package Client::SOAPAdapterFactory;
+
+use Client::AAAS::Login;
 
 ###############################################################################
 #
@@ -12,21 +19,22 @@ sub new {
 }
 
 sub initialize {
-    my ($self) = @_;
+    my $self = @_;
+}
+######
 
-    # TODO:  create correct instance of SOAPAdapter
+# probably a better way to do this ...
+sub create_instance {
+    my( $self, $method_name ) = @_;
+    # TODO:  create correct subclass of SOAPAdapter given method name
+    #        in query string
+    return Client::AAAS::Login->new();
 }
 ######
 
 
 package Client::SOAPAdapter;
 
-# Last modified:  November 18, 2005
-# David Robertson (dwrobertson@lbl.gov)
-
-use strict;
-
-use Error qw(:try);
 use Data::Dumper;
 
 use CGI;
@@ -48,17 +56,15 @@ sub new {
 sub initialize {
     my ($self) = @_;
 
-    my $args = $ENV{'QUERY_STRING'};
-    my $uri = $ENV{'REQUEST_URI'};
     $self->{cgi} = CGI->new();
     $self->{session} = Client::UserSession->new();
 }
 ######
 
 ##############################################################################
-# pre_call:  Perform operations necessary before making SOAP call
+# before_call:  Perform operations necessary before making SOAP call
 #
-sub pre_call {
+sub before_call {
     my( $self ) = @_;
 
     my( $params );
@@ -98,9 +104,9 @@ sub output {
 ######
 
 ##############################################################################
-# post_call:  Perform any operations necessary after making SOAP call
+# after_call:  Perform any operations necessary after making SOAP call
 #
-sub post_call {
+sub after_call {
     my( $self, $results ) = @_;
 
 }
