7321d97ecf7df35e613ed104fe6ef313bab3bc31
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-11-19 00:25:23 +0000

    Initial check-in.  Directory for sub-classes of Client::SOAPAdapter
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@1499 c75a6b91-02ef-0310-b74a-e20389c9c41d

---
+++ oscars/WBUI/mod_perl/Client/AAAS/GetUserList.pm
@@ -0,0 +1,78 @@
+package Client::AAAS::GetUserList;
+
+# Handles get user list form submission
+#
+# Last modified:  November 18, 2005
+# David Robertson (dwrobertson@lbl.gov)
+
+use strict;
+
+use CGI;
+use Data::Dumper;
+
+###############################################################################
+#
+sub new {
+    my( $class, %args ) = @_;
+    my( $self ) = { %args };
+  
+    bless( $self, $class );
+    return( $self );
+}
+######
+
+###############################################################################
+# output:  If the caller has admin privileges print a list of 
+#          all users returned by the SOAP call
+#
+# In:  results of SOAP call
+# Out: None
+#
+sub output {
+    my ( $self, $params ) = @_;
+
+    print "<xml>\n";
+    print qq{
+      <msg>Successfully read user list.</msg>
+    <div id="zebratable_ui">
+      <p>Click on the user's last name to view detailed user information.</p>
+      <table cellspacing="0" width="90%" class="sortable" id="userlist">
+        <thead><tr>
+          <td>Last Name</td>          <td>First Name</td>
+          <td>Distinguished Name</td> <td>Level</td>
+          <td>Organization</td>       <td>Status</td>
+        </tr></thead>
+      <tbody>
+    };
+    for my $row (@$params) { $self->print_row( $row ); }
+    print qq{
+      </tbody></table>
+      </div>";
+    };
+    print "</xml>\n";
+}
+######
+
+###############################################################################
+# print_row:  print the information for one user
+#
+sub print_row {
+    my( $self, $row ) = @_;
+
+    print qq{
+    <tr>
+      <td><a href="#" style="/styleSheets/layout.css"
+        onclick="new_page(
+        '/perl/adapt.pl?method=get_profile;id=$row->{user_dn}');
+        return false;">$row->{user_last_name}</a></td>
+      <td>$row->{user_first_name}</td> <td>$row->{user_dn}</td>
+      <td>$row->{user_level}</td>      <td>$row->{institution_id}</td>
+      <td>$row->{user_status}</td>
+    </tr>
+    };
+}
+
+######
+ 
+######
+1;
---
+++ oscars/WBUI/mod_perl/Client/AAAS/Login.pm
@@ -0,0 +1,82 @@
+package Client::AAAS::Login;
+
+# Handles user login.
+#
+# Last modified:  November 18, 2005
+# David Robertson (dwrobertson@lbl.gov)
+
+use strict;
+
+use Data::Dumper;
+
+###############################################################################
+#
+sub new {
+    my( $class, %args ) = @_;
+    my( $self ) = { %args };
+  
+    bless( $self, $class );
+    return( $self );
+}
+
+##############################################################################
+# Overrides to avoid trying to verify a non-existent session.
+#
+sub pre_call {
+    my( $self, $params ) = @_;
+}
+
+##############################################################################
+sub output {
+    my( $self, $params ) = @_;
+
+    print "<xml>\n";
+    print qq{
+      <msg>User $params->{user_dn} signed in.</msg>
+      <div id="info_form">
+      <p>
+      With the advent of service sensitive applications (such as remote-
+      controlled experiments, time constrained massive data transfers,
+      video-conferencing, etc.), it has become apparent that there is a need
+      to augment the services present in today's ESnet infrastructure.
+      </p>
+
+      <p>
+      Two DOE Office of Science workshops in the past two years have clearly 
+      identified both science discipline driven network requirements and a 
+      roadmap for meeting these requirements.  This project begins to 
+      address one element of the roadmap: dynamically provisioned, QoS paths.
+      </p>
+
+      <p>
+      The focus of the ESnet On-Demand Secure Circuits and Advance Reservation 
+      System (OSCARS) is to develop and deploy a prototype service that enables 
+      on-demand provisioning of guaranteed bandwidth secure circuits within 
+      ESnet.
+      </p>
+
+      <p>To begin using OSCARS, click on one of the notebook tabs.</p>
+      </div>
+    };
+    print "</xml>\n";
+}
+######
+
+##############################################################################
+# overrides super-class call
+#
+sub post_call {
+    my( $self, $params );
+
+    my( $sid );
+
+    ($params->{user_dn}, $sid ) =
+                                $self->{session}->start_session($self->{cgi});
+    print $self->{cgi}->header(
+         -type=>'text/xml',
+         -cookie=>$self->{cgi}->cookie(CGISESSID => $sid));
+}
+######
+
+######
+1;
