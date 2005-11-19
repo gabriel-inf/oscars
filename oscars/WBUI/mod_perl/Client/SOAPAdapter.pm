ce22c9b8128ca8eb0d374231c908e6fd8d00e37a
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-11-19 07:09:24 +0000

    Factory package closer to working.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@1509 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/WBUI/mod_perl/Client/SOAPAdapter.pm
+++ oscars/WBUI/mod_perl/Client/SOAPAdapter.pm
@@ -1,11 +1,10 @@
 # Last modified:  November 18, 2005
 # David Robertson (dwrobertson@lbl.gov)
 
-use strict;
 
 package Client::SOAPAdapterFactory;
-
-use Client::AAAS::Login;
+    use strict;
+    use Data::Dumper;
 
 ###############################################################################
 #
@@ -14,33 +13,31 @@ sub new {
     my( $self ) = { %args };
   
     bless( $self, $class );
-    $self->initialize();
     return( $self );
 }
 
-sub initialize {
-    my $self = @_;
-}
-######
+sub instantiate {
+    my( $self, $requested_type, $args ) = @_;
 
-# probably a better way to do this ...
-sub create_instance {
-    my( $self, $method_name ) = @_;
-    # TODO:  create correct subclass of SOAPAdapter given method name
-    #        in query string
-    return Client::AAAS::Login->new();
-}
-######
+    my @loc_array      = split('::', $requested_type); 
+    shift(@loc_array);  # remove parent directory
+    my $location       = join('/', @loc_array) . ".pm";
+    my $subclass       = "$requested_type";
 
+    require $location;
+    return $subclass->new(%$args);
+}
+
+#############################################################################
 
 package Client::SOAPAdapter;
+    use strict;
 
-use Data::Dumper;
+    use Data::Dumper;
+    use CGI;
+    use SOAP::Lite;
 
-use CGI;
-use SOAP::Lite;
-
-use Client::UserSession;
+    use Client::UserSession;
 
 ###############################################################################
 #
