a8cd071294c08dd2590c359ee04d25698b6ed05a
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-11-21 14:14:33 +0000

    Initial check-in.  Just displays whether modification to profile was successfully done in status bar.  Doesn't change many portion of form.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@1579 c75a6b91-02ef-0310-b74a-e20389c9c41d

---
+++ oscars/WBUI/mod_perl/Client/AAAS/SetProfile.pm
@@ -0,0 +1,37 @@
+###############################################################################
+package Client::AAAS::SetProfile;
+
+# Handles modifying a user's profile.
+#
+# Last modified:  November 21, 2005
+# David Robertson (dwrobertson@lbl.gov)
+
+use strict;
+
+use Data::Dumper;
+
+use Client::SOAPAdapter;
+our @ISA = qw{Client::SOAPAdapter};
+
+#______________________________________________________________________________
+
+
+##############################################################################
+# output:  print user profile form, and results retrieved via
+# a SOAP call, if any
+#
+sub output {
+    my( $self, $results ) = @_;
+
+    print $self->{cgi}->header(
+        -type=>'text/xml');
+    print "<xml>\n";
+    print qq{
+    <msg>Successfully updated user $self->{user_dn}'s proifle</msg>
+    };
+    print "</xml>\n";
+} #____________________________________________________________________________
+
+
+######
+1;
