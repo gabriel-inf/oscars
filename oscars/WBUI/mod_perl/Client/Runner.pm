b472d4986e5682188c832314495eb981e67dad70
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-11-21 04:58:21 +0000

    Fixed the way CGI parameters are set up.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@1514 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/WBUI/mod_perl/Client/Runner.pm
+++ oscars/WBUI/mod_perl/Client/Runner.pm
@@ -1,12 +1,16 @@
 package Client::Runner;
 
 # Calls SOAPAdapter to make SOAP call and format results for output.
-# Last modified:  November 18, 2005
+# Last modified:  November 20, 2005
 # David Robertson (dwrobertson@lbl.gov)
 
 use strict;
 
+use Apache2::RequestIO;
+use CGI qw{:cgi};
 use SOAP::Lite;
+use Data::Dumper;
+
 use Client::SOAPAdapter;
 
 
@@ -14,24 +18,22 @@ use Client::SOAPAdapter;
 #
 sub run {
 
-    my ( $method_name, %args );
+    my ( %soap_params );
 
     my $soap_server = SOAP::Lite
                           -> uri('http://198.128.14.164/Dispatcher')
                           -> proxy ('https://198.128.14.164/SOAP');
-    my $query = $ENV{'QUERY_STRING'};
-    my @pairs = split(';', substr($query, 1));
-    for my $pair (@pairs) {
-        my ($key, $val) = split('=', $pair) ; 
-        if ($key eq 'method') { $method_name=$val; }
-        else { $args{$key} = $val; }
-    }
-    #my $uri = $ENV{'REQUEST_URI'};
     my $factory = Client::SOAPAdapterFactory->new();
-    my $adapter = $factory->instantiate($method_name, \%args);
-    my $soap_params = $adapter->before_call();
-    my $results = $adapter->make_call($soap_server, $soap_params);
-    $adapter->after_call($results);
+    my $cgi = CGI->new();
+    my $adapter = $factory->instantiate($cgi);
+    my $user_dn = $adapter->authenticate();
+        # TODO:  handle cleanly when not authenticated or authorized
+    if (!$user_dn) { return; }
+    my $authorized = $adapter->authorize($user_dn);
+    if (!$authorized) { return; }
+    $adapter->modify_params(\%soap_params);  # adapts from CGI params
+    my $results = $adapter->make_call($soap_server, \%soap_params);
+    $adapter->post_process($results);
     $adapter->output($results);
 }
 ######
