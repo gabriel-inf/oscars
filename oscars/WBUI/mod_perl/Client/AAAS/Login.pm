a324041bf2050fe1dbcec1800e94840d79664b2a
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-11-21 05:01:41 +0000

    Doing initialization in sub-classes now.  Renamed most of the methods. Changed the output method to call navigation bar and information page setup.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@1518 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/WBUI/mod_perl/Client/AAAS/Login.pm
+++ oscars/WBUI/mod_perl/Client/AAAS/Login.pm
@@ -1,75 +1,93 @@
+###############################################################################
 package Client::AAAS::Login;
 
 # Handles user login.
 #
-# Last modified:  November 18, 2005
+# Last modified:  November 20, 2005
 # David Robertson (dwrobertson@lbl.gov)
 
 use strict;
 
 use Data::Dumper;
+use CGI qw{:cgi};
+
+use Client::UserSession;
+use Client::NavigationBar;
+use Client::GetInfo;
 
 use Client::SOAPAdapter;
 our @ISA = qw{Client::SOAPAdapter};
 
-##############################################################################
+#******************************************************************************
+sub new {
+    my( $class, %args ) = @_;
+    my( $self ) = { %args };
+  
+    bless( $self, $class );
+    $self->initialize();
+    return( $self );
+}
+
+sub initialize {
+    my ($self) = @_;
+
+    $self->{session} = Client::UserSession->new();
+} #____________________________________________________________________________ 
+
+
+#******************************************************************************
 # Overrides super-class call to avoid trying to verify a non-existent session.
+# In this case, the SOAP call is used to authenticate.
 #
-sub before_call {
+sub authenticate {
+    my( $self ) = @_;
+
+} #____________________________________________________________________________ 
+
+
+#******************************************************************************
+sub modify_params {
     my( $self, $params ) = @_;
-}
 
-##############################################################################
-sub output {
-    my( $self, $params ) = @_;
+    $params->{server_name} = 'AAAS';
+    $self->SUPER::modify_params($params);
+} #____________________________________________________________________________ 
 
-    print "<xml>\n";
-    print qq{
-      <msg>User $params->{user_dn} signed in.</msg>
-      <div id="info_form">
-      <p>
-      With the advent of service sensitive applications (such as remote-
-      controlled experiments, time constrained massive data transfers,
-      video-conferencing, etc.), it has become apparent that there is a need
-      to augment the services present in today's ESnet infrastructure.
-      </p>
 
-      <p>
-      Two DOE Office of Science workshops in the past two years have clearly 
-      identified both science discipline driven network requirements and a 
-      roadmap for meeting these requirements.  This project begins to 
-      address one element of the roadmap: dynamically provisioned, QoS paths.
-      </p>
-
-      <p>
-      The focus of the ESnet On-Demand Secure Circuits and Advance Reservation 
-      System (OSCARS) is to develop and deploy a prototype service that enables 
-      on-demand provisioning of guaranteed bandwidth secure circuits within 
-      ESnet.
-      </p>
-
-      <p>To begin using OSCARS, click on one of the notebook tabs.</p>
-      </div>
-    };
-    print "</xml>\n";
-}
-######
-
-##############################################################################
+#******************************************************************************
 # overrides super-class call
 #
-sub after_call {
-    my( $self, $params );
+sub post_process {
+    my( $self, $results ) = @_;
 
     my( $sid );
 
-    ($params->{user_dn}, $sid ) =
+    ($results->{user_dn}, $sid ) =
                                 $self->{session}->start_session($self->{cgi});
     print $self->{cgi}->header(
          -type=>'text/xml',
          -cookie=>$self->{cgi}->cookie(CGISESSID => $sid));
-}
-######
+} #____________________________________________________________________________ 
+
+
+#******************************************************************************
+sub output {
+    my( $self, $results ) = @_;
+
+    my $info = Client::GetInfo->new();
+    my $navigation_bar = Client::NavigationBar->new();
+    for $_ (keys(%$results)) {
+        if ($results->{$_}) {
+            print STDERR "result: $_, value: $results->{$_}\n";
+        }
+    }
+    print "<xml>\n";
+    print "<msg>User $results->{user_dn} signed in.</msg>\n";
+    # TODO:  FIX hard-wired admin
+    $navigation_bar->initialize('admin');
+    $info->output();
+    print "</xml>\n";
+} #____________________________________________________________________________ 
 
 ######
 1;
