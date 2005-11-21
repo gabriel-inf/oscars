f815aea4138cbb75406b35c285df5f68342fdb1a
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-11-21 10:35:32 +0000

    All tabs except logout require onclick (mistakenly removed it).
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@1557 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/WBUI/mod_perl/Client/NavigationBar.pm
+++ oscars/WBUI/mod_perl/Client/NavigationBar.pm
@@ -10,16 +10,17 @@ use strict;
 
 use Data::Dumper;
 
-#******************************************************************************
+
 sub new {
     my( $class, %args ) = @_;
     my( $self ) = { %args };
   
     bless( $self, $class );
     return( $self );
-}
+} #____________________________________________________________________________
 
-#******************************************************************************
+
+###############################################################################
 # Initializes tabbed bar used for navigation within the OSCARS site. 
 #
 sub initialize {
@@ -29,22 +30,23 @@ sub initialize {
 
     <navigation_bar>
     <ul id="tabnav">
-      <li id="info_form">
+      <li id="get_info">
         <a style="/styleSheets/layout.css" title="Information page"
-           onclick="new_page('info_form');return false;"
+           class="active"
+           onclick="new_section('get_info');return false;"
            href="#">Information</a>
       </li>
     };
     if ($user_permission eq 'admin') {    # TODO:  FIX auth
       print qq{
-      <li id="acctlist_form">
+      <li id="view_users">
         <a style="/styleSheets/layout.css" title="List user accounts"
-           onclick="new_page('acctlist_form');return false;"
+           onclick="new_section('view_users');return false;"
            href="#">List Accounts</a>
       </li>
-      <li id="new_user_form">
+      <li id="add_user">
         <a style="/styleSheets/layout.css" title="Add a new user account"
-           onclick="new_page('new_user_form');return false;"
+           onclick="new_section('add_user');return false;"
            href="#">Add User</a>
       </li>
       };
@@ -53,18 +55,18 @@ sub initialize {
       <li id="get_profile">
         <a style="/styleSheets/layout.css" 
            title="View and/or edit your information"
-           onclick="new_page('get_profile');return false;"
+           onclick="new_section('get_profile');return false;"
            href="#">User Profile</a>
       </li>
-      <li id="list_form">
+      <li id="view_reservations">
         <a style="/styleSheets/layout.css" 
            title="View/Edit selected list of reservations"
-           onclick="new_page('list_form');return false;"
+           onclick="new_section('view_reservations');return false;"
            href="#">View/Edit Reservations</a>
       </li>
-      <li id="creation_form">
+      <li id="create_reservation">
         <a style="/styleSheets/layout.css" title="Create a new reservation"
-           onclick="new_page('creation_form');return false;"
+           onclick="new_section('create_reservation');return false;"
            href="#">Make Reservation</a>
       </li>
       <li id="logout">
