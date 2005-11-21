7e3bfd718a64f98f1a2dea661a9d2ea95d630efe
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-11-21 12:06:31 +0000

    Initial check-ins of these methods.  get_profile almost works, add_user just has form display working.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@1567 c75a6b91-02ef-0310-b74a-e20389c9c41d

---
+++ oscars/WBUI/mod_perl/Client/AAAS/AddUser.pm
@@ -0,0 +1,182 @@
+###############################################################################
+package Client::AAAS::AddUser;
+
+# Handles add_user form submission
+#
+# Last modified:  November 21, 2005
+# David Robertson (dwrobertson@lbl.gov)
+
+use strict;
+
+use Data::Dumper;
+
+use Client::SOAPAdapter;
+our @ISA = qw{Client::SOAPAdapter};
+
+#______________________________________________________________________________
+
+
+###############################################################################
+# Currently a noop.
+#
+sub make_call {
+    my( $self, $soap_server, $soap_params ) = @_;
+
+    return {};
+} #____________________________________________________________________________ 
+
+
+###############################################################################
+# output: print add user form, and results from SOAP call, if any
+#
+sub output {
+    my( $self, $params ) = @_;
+
+    print $self->{cgi}->header(
+         -type=>'text/xml');
+    print "<xml>\n";
+    print qq{
+    <msg>Add User Form</msg>
+    <div>
+    <h3>Add a new user</h3>
+    <p>The <strong>Admin Password</strong> is your password for 
+    <strong>$self->{user_dn}</strong>.</p>
+    <p>Required fields are outlined in green.</p>
+    <form method="post" action="" onsubmit="
+        return submit_form(this, '/perl/adapt.pl?method=add_user', '');">
+    <table>
+    <tr>
+      <td>Distinguished Name</td>
+      <td>
+        <input class="required" type="text" name="user_dn" size="40"></input>
+      </td>
+    </tr>
+    };
+    $self->output_password_fields();
+    print qq{
+    <tr>
+      <td>User Level</td>
+      <td>
+        <input class="required" type="text" name="user_level" size="40">
+        </input>
+      </td>
+    </tr>
+    };
+    $self->output_profile_fields();
+    print qq{
+    <p><input type="submit" value="Create Profile"></input></p>
+    </form>
+    <p>For inquiries, please contact the project administrator.</p>
+    </div>
+    };
+    print "</xml>\n";
+} #____________________________________________________________________________
+
+
+##############################################################################
+# output_profile_fields:  print fields of user profile
+#     Note that the space is important between the starting and ending 
+#     textarea tags.
+#
+sub output_profile_fields {
+    my( $self ) = @_;
+
+    print qq{
+      <tr>
+        <td>First Name</td>
+        <td>
+          <input class="required" type="text" name="user_first_name" size="40"
+                 value=""></input>
+        </td>
+      </tr>
+      <tr>
+        <td>Last Name</td>
+        <td>
+          <input class="required" type="text" name="user_last_name" size="40">
+          </input>
+         </td>
+      </tr>
+      <tr>
+        <td>Organization</td><td>
+          <input class="required" type="text" name="institution" size="40">
+          </input>
+        </td>
+      </tr>
+      <tr>
+        <td>Personal Description</td>
+        <td>
+          <textarea name="user_description" rows="3" cols="50"> </textarea>
+        </td>
+      </tr>
+      <tr>
+        <td>E-mail (Primary)</td>
+        <td>
+          <input class="required" type="text" name="user_email_primary"
+                 size="40"></input>
+        </td>
+      </tr>
+      <tr>
+        <td>E-mail (Secondary)</td>
+        <td>
+          <input type="text" name="user_email_secondary" size="40"></input>
+        </td>
+      </tr>
+      <tr>
+        <td>Phone Number (Primary)</td>
+        <td>
+          <input class="required" type="text" name="user_phone_primary"
+                 size="40">
+          </input>
+        </td>
+      </tr>
+      <tr>
+        <td>Phone Number (Secondary)</td>
+        <td>
+          <input type="text" name="user_phone_secondary" size="40"></input>
+        </td>
+      </tr>
+      </table>
+
+      <p>Please check your contact information carefully before submitting 
+      the form.</p>
+    };
+} #____________________________________________________________________________
+
+
+##############################################################################
+# output_password_fields:  print rows having to do with passwords
+#
+sub output_password_fields {
+    my( $self ) = @_;
+
+    print qq{
+      <tr>
+        <td>Admin Password</td>
+        <td>
+          <input class="required" type="password" name="user_password" 
+                 size="40">
+          </input>
+         </td>
+      </tr>
+      <tr>
+        <td>New User Password</td>
+        <td>
+          <input class="required" type="password" name="password_new_once" 
+                 size="40">
+          </input>
+        </td>
+      </tr>
+      <tr>
+        <td>(Enter twice)</td>
+        <td>
+          <input class="required" type="password" name="password_new_twice" 
+                 size="40">
+          </input>
+        </td>
+      </tr>
+    };
+} #____________________________________________________________________________
+ 
+
+######
+1;
---
+++ oscars/WBUI/mod_perl/Client/AAAS/GetProfile.pm
@@ -0,0 +1,177 @@
+###############################################################################
+package Client::AAAS::GetProfile;
+
+# Handles get_profile form submission
+#
+# Last modified:  November 21, 2005
+# David Robertson (dwrobertson@lbl.gov)
+
+use strict;
+
+use Data::Dumper;
+
+use Client::SOAPAdapter;
+our @ISA = qw{Client::SOAPAdapter};
+
+#______________________________________________________________________________
+
+
+##############################################################################
+# output:  print user profile form, and results retrieved via
+# a SOAP call, if any
+#
+sub output {
+    my( $self, $results ) = @_;
+
+    print $self->{cgi}->header(
+        -type=>'text/xml');
+    print "<xml>\n";
+    print qq{
+    <msg>User profile</msg>
+    <div>
+    <h3>Editing profile for user: $self->{user_dn}</h3>
+    <p>Required fields are outlined in green.</p>
+    <form method="post" action=""
+        onsubmit="return submit_form(this,
+                                     '/perl/adapt.pl?method=set_profile',
+                                     '');">
+    <table>
+      <tr>
+        <td>Distinguished Name</td>
+        <td>$self->{user_dn}</td>
+      </tr>
+    };
+    $self->output_password_fields($results);
+    if ($results->{admin}) {
+        print qq{
+        <tr>
+          <td>User Level</td>
+          <td>
+            <input class="required" type="text" name="user_level" size="40"
+                   value="notdone"></input>
+          </td>
+        </tr>
+        };
+    }
+    $self->output_profile_fields($results);
+    print qq{
+    <p><input type="submit" value="Change Profile"></input></p>
+    </form>
+    <p>For inquiries, please contact the project administrator.</p>
+    </div>
+    };
+    print "</xml>\n";
+} #____________________________________________________________________________
+
+
+##############################################################################
+# output_profile_fields:  print fields of user profile
+#
+sub output_profile_fields {
+    my( $self, $row ) = @_;
+
+    print STDERR Dumper($row);
+    print qq{
+      <tr>
+        <td>First Name</td>
+        <td>
+          <input class="required" type="text" name="user_first_name" size="40"
+                 value="$row->{user_first_name}">
+          </input>
+        </td>
+      </tr>
+      <tr>
+        <td>Last Name</td>
+        <td>
+          <input class="required" type="text" name="user_last_name" size="40"
+                 value="$row->{user_last_name}">
+          </input>
+        </td>
+      </tr>
+      <tr>
+        <td>Organization</td>
+        <td>
+          <input class="required" type="text" name="institution" size="40"
+                 value="notdone">
+          </input>
+        </td>
+      </tr>
+      <tr>
+        <td valign="top">Personal Description</td>
+        <td>
+          <textarea name="user_description" rows="3" cols="50">
+              $row->{user_description}
+          </textarea>
+        </td>
+      </tr>
+      <tr>
+        <td>E-mail (Primary)</td>
+        <td>
+          <input class="required" type="text" name="user_email_primary"
+                 size="40" value="$row->{user_email_primary}">
+          </input>
+        </td>
+      </tr>
+      <tr>
+        <td>E-mail (Secondary)</td>
+        <td>
+          <input type="text" name="user_email_secondary" size="40"
+                 value="$row->{user_email_secondary}">
+          </input>
+        </td>
+      </tr>
+      <tr>
+        <td>Phone Number (Primary)</td>
+        <td>
+          <input class="required" type="text" name="user_phone_primary"
+                 size="40" value="$row->{user_phone_primary}">
+           </input>
+        </td>
+      </tr>
+      <tr>
+        <td>Phone Number (Secondary)</td>
+        <td>
+          <input type="text" name="user_phone_secondary" size="40"
+                 value="$row->{user_phone_secondary}">
+          </input>
+        </td>
+      </tr>
+      </table>
+      <p>Please check your contact information carefully before submitting 
+      the form.</p>
+    };
+} #____________________________________________________________________________
+
+
+##############################################################################
+# output_password_fields:  print rows having to do with passwords
+#
+sub output_password_fields {
+    my( $self, $params ) = @_;
+
+    print qq{
+      <tr>
+        <td>Current Password</td>
+        <td>
+          <input class="required" type="password" name="user_password" 
+                 size="40"></input>
+        </td>
+      </tr>
+      <tr>
+        <td>New Password (Enter twice )</td>
+        <td>
+          <input type="password" name="password_new_once" size="40"></input>
+        </td>
+      </tr>
+      <tr>
+        <td>(Leave blank to stay the same)</td>
+        <td>
+          <input type="password" name="password_new_twice" size="40"></input>
+        </td>
+      </tr>
+    };
+} #____________________________________________________________________________
+ 
+
+######
+1;
