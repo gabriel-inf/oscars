5ff4ab19ea6d331ef5a86e7db778bf079cc86794
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-11-23 02:48:20 +0000

    Added back old authorization material.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@1590 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/WBUI/mod_perl/Client/UserSession.pm
+++ oscars/WBUI/mod_perl/Client/UserSession.pm
@@ -2,7 +2,7 @@
 package Client::UserSession;
 
 # Handles user session.
-# Last modified:  November 18, 2005
+# Last modified:  November 22, 2005
 # David Robertson (dwrobertson@lbl.gov)
 
 use strict;
@@ -17,9 +17,20 @@ sub new {
     my( $self ) = { %args };
   
     bless( $self, $class );
+    $self->initialize();
     return( $self );
-} #____________________________________________________________________________
+}
 
+sub initialize {
+    my( $self ) = @_;
+
+    # TODO:  Fix when have ROAM
+    $self->{user_levels} = {
+        'user' => 2,
+        'engr' => 4,
+        'admin' => 8,
+    };
+} #___________________________________________________________________________                                         
 
 ###############################################################################
 # start_session: Sets cookie containing session id to be used in granting
@@ -31,13 +42,14 @@ sub new {
 #
 sub start_session
 {
-    my( $self, $cgi ) = @_;
+    my( $self, $cgi, $results ) = @_;
 
     my $session = CGI::Session->new("driver:File", undef, {Directory => "/tmp"});
     my $sid = $session->id();
     my $cookie = $cgi->cookie(CGISESSID => $sid);
-    $session->param("user_dn", $cgi->param('user_dn'));
-    return( $cgi->param('user_dn'), $sid );
+    $session->param('user_dn', $results->{user_dn});
+    $session->param('user_level', $results->{user_level});
+    return( $sid );
 } #____________________________________________________________________________
 
 
@@ -57,13 +69,40 @@ sub verify_session
     # Unauthorized user may know to set CGISESSID cookie. However,
     # an entirely new session (without the dn param) will be 
     # created if there is no valid session with that id.
-    my $stored_token = $session->param("user_dn");
-    if (!$stored_token)  {
+    my $user_dn = $session->param("user_dn");
+    if (!$user_dn)  {
         return( undef );
     }
     else {
-       $cgi->param(-name=>'user_dn',-value=>$stored_token);
-       return( $stored_token );
+       $cgi->param(-name=>'user_dn',-value=>$user_dn);
+       return( $user_dn );
+    }
+} #____________________________________________________________________________
+
+
+###############################################################################
+# authorize_session:  Checks to see that a cookie containing a valid session
+# id is set, and get authorization level.  TODO: will be completely redone.
+#
+# In:  ref to CGI instance
+# Out: 1 (authorized)/0 (not authorized)
+#
+sub authorize_session
+{
+    my( $self, $cgi ) = @_;
+
+    my $session = CGI::Session->new(undef, $cgi, {Directory => "/tmp"});
+
+    # Unauthorized user may know to set CGISESSID cookie. However,
+    # an entirely new session (without the dn param) will be 
+    # created if there is no valid session with that id.
+    my $user_level = $session->param("user_level");
+    if (!$user_level)  {
+        return( undef );
+    }
+    else {
+       $cgi->param(-name=>'user_level',-value=>$user_level);
+       return( $user_level );
     }
 } #____________________________________________________________________________
 
@@ -75,6 +114,38 @@ sub end_session
   
     my $session = CGI::Session->new(undef, $cgi, {Directory => "/tmp"});
     $session->clear(["user_dn"]);
+    $session->clear(["user_level"]);
+} #____________________________________________________________________________
+
+
+##############################################################################
+# authorized:  Given the user level code, see if the user has the required
+#              privilege
+# 
+sub authorized {
+    my( $self, $user_level, $required_priv ) = @_;
+ 
+    return($self->{user_levels}->{$required_priv} & $user_level );
+} #____________________________________________________________________________
+
+
+###############################################################################
+#
+sub get_str_level {
+    my( $self, $level_flag ) = @_;
+
+    my $level = "";
+    $level_flag += 0;
+    if ($self->{user_levels}->{admin} & $level_flag) {
+        $level .= 'admin ';
+    }
+    if ($self->{user_levels}->{engr} & $level_flag) {
+        $level .= 'engr ';
+    }
+    if ($self->{user_levels}->{user} & $level_flag) {
+        $level .= 'user';
+    }
+    return( $level );
 } #____________________________________________________________________________
 
 
