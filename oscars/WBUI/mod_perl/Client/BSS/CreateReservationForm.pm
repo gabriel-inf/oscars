7dcb3e23f53001534c4af30a70dbfb986a6f51b2
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-11-23 02:45:18 +0000

    Changed authorization test necessary to be able to print out sensitive fields such as ingress router, and changed quotes for material not requiring interpolation.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@1586 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/WBUI/mod_perl/Client/BSS/CreateReservationForm.pm
+++ oscars/WBUI/mod_perl/Client/BSS/CreateReservationForm.pm
@@ -3,7 +3,7 @@ package Client::BSS::CreateReservationForm;
 
 # Handles request to display the make reservation form.
 #
-# Last modified:  November 21, 2005
+# Last modified:  November 22, 2005
 # David Robertson (dwrobertson@lbl.gov)
 
 use strict;
@@ -13,7 +13,7 @@ use Data::Dumper;
 use Client::SOAPAdapter;
 our @ISA = qw{Client::SOAPAdapter};
 
-#______________________________________________________________________________ 
+#______________________________________________________________________________
 
 
 ###############################################################################
@@ -23,7 +23,7 @@ sub make_call {
     my( $self, $soap_server, $soap_params ) = @_;
 
     return {};
-} #____________________________________________________________________________ 
+} #____________________________________________________________________________
 
 
 ###############################################################################
@@ -42,15 +42,15 @@ sub output {
     print "<xml>\n";
     print qq{
     <msg>Reservation creation form</msg>
-    <div id="reservation_ui">
-    <form method="post" action=""
+    <div id='reservation_ui'>
+    <form method='post' action=''
       onsubmit="return submit_form(this, 'create_reservation', '');">
-      <input type="hidden" name="reservation_start_time"></input>
-      <input type="hidden" name="reservation_end_time"></input>
-      <input type="hidden" name="user_dn" value="$self->{user_dn}">
+      <input type='hidden' name='reservation_start_time'></input>
+      <input type='hidden' name='reservation_end_time'></input>
+      <input type='hidden' name='user_dn' value="$self->{user_dn}">
       </input>
-      <input type="submit" value="Reserve bandwidth"></input>
-      <input type="reset" value="Reset form fields"></input>
+      <input type='submit' value='Reserve bandwidth'></input>
+      <input type='reset' value='Reset form fields'></input>
 
       <p>Required inputs are bordered in green.  Ranges or types of valid 
       entries are given in parentheses below the input fields.</p>
@@ -60,13 +60,13 @@ sub output {
           <th>Source</th> <th>Destination</th> <th>Bandwidth (Mbps)</th>
         </tr>
         <tr>
-          <td class="required">
-            <input type="text" name="source_host" size="29"></input></td>
-          <td class="required">
-            <input type="text" name="destination_host" size="29"></input></td>
-          <td class="required">
-            <input type="text" name="reservation_bandwidth" maxlength="7" 
-                   size="14"></input></td>
+          <td class='required'>
+            <input type='text' name='source_host' size='29'></input></td>
+          <td class='required'>
+            <input type='text' name='destination_host' size='29'></input></td>
+          <td class='required'>
+            <input type='text' name='reservation_bandwidth' maxlength='7' 
+                   size='14'></input></td>
         </tr>
         <tr>
           <td>(Host name or IP address)</td> <td>(Host name or IP address)</td>
@@ -77,7 +77,7 @@ sub output {
       your reservation.  Fields left blank will default to the examples below 
       the input fields.  The default time zone is the local time.
     };
-    if ($results->{engr}) {
+    if ($self->{session}->authorized($results->{user_level}, 'engr')) {
         print qq{
           Checking the <strong>Persistent</strong> box makes a reservation's 
           duration indefinite, overriding the duration field.
@@ -89,44 +89,44 @@ sub output {
         <tr><th>Year</th>   <th>Month</th>      <th>Date</th> <th>Hour</th>
             <th>Minute</th> <th>UTC offset</th> <th>Duration (Hours)</th>
     };
-    if ($results->{engr}) {
-        print "<th>Persistent</th>";
+    if ($self->{session}->authorized($results->{user_level}, 'engr')) {
+        print '<th>Persistent</th>';
     }
-    else { print "<th> </th>"; }
+    else { print '<th> </th>'; }
     print qq{
       </tr>
       <tr>
         <td>
-          <input type="text" name="start_year" size="6" maxlength="4"></input>
+          <input type='text' name='start_year' size='6' maxlength='4'></input>
         </td>
         <td>
-          <input type="text" name="start_month" size="6" maxlength="2"></input>
+          <input type='text' name='start_month' size='6' maxlength='2'></input>
         </td>
         <td>
-          <input type="text" name="start_date" size="6" maxlength="2"></input>
+          <input type='text' name='start_date' size='6' maxlength='2'></input>
         </td>
         <td>
-          <input type="text" name="start_hour" size="6" maxlength="2"></input>
+          <input type='text' name='start_hour' size='6' maxlength='2'></input>
         </td>
         <td>
-          <input type="text" name="start_minute" size="6" maxlength="2">
+          <input type='text' name='start_minute' size='6' maxlength='2'>
           </input>
         </td>
-        <td id="tz_option_list"></td>
+        <td id='tz_option_list'></td>
         <td>
-          <input type="text" name="duration_hour" size="10" maxlength="16">
+          <input type='text' name='duration_hour' size='10' maxlength='16'>
           </input>
         </td>
         <td>
     };
-    if ($results->{engr}) {
-        print '  <input type="checkbox" name="persistent" value="0"></input>';
+    if ($self->{session}->authorized($results->{user_level}, 'engr')) {
+        print "  <input type='checkbox' name='persistent' value='0'></input>";
     }
     print qq{
         </td>
       </tr>
-      <tr id="time_settings_example">
-        <td colspan="8"></td>
+      <tr id='time_settings_example'>
+        <td colspan='8'></td>
       </tr>
       <tr>
         <td> </td><td>(1-12)</td><td>(1-31)</td><td>(0-23)</td><td>(0-59)</td>
@@ -135,30 +135,30 @@ sub output {
     </table>
 
     <p>Please let us know the purpose of making this reservation.</p>
-    <table cols="1">
+    <table cols='1'>
       <tr>
-        <td class="required"><textarea name="reservation_description" rows="2" 
-            cols="98"> </textarea></td>
+        <td class='required'><textarea name='reservation_description' rows='2' 
+            cols='98'> </textarea></td>
       </tr>
     </table>
 
     <p>The following are optional fields.  <strong>DSCP</strong> sets the 
     differentiated services code point.</p>
 
-    <table cols="4">
-      <tr><td colspan="4"> </td></tr>
+    <table cols='4'>
+      <tr><td colspan='4'> </td></tr>
       <tr><th>Source port</th> <th>Destination port</th> <th>Protocol</th>
           <th>DSCP</th>
       </tr>
       <tr>
-        <td><input type="text" name="reservation_src_port" maxlength="5" 
-                   size="17"></input></td>
-        <td><input type="text" name="reservation_dst_port" maxlength="5"
-                   size="17"></input></td>
-        <td><input type="text" name="reservation_protocol" 
-                   size="17"></input></td>
-        <td><input type="text" name="reservation_dscp" maxlength="2" 
-                   size="17"></input></td>
+        <td><input type='text' name='reservation_src_port' maxlength='5' 
+                   size='17'></input></td>
+        <td><input type='text' name='reservation_dst_port' maxlength='5'
+                   size='17'></input></td>
+        <td><input type='text' name='reservation_protocol' 
+                   size='17'></input></td>
+        <td><input type='text' name='reservation_dscp' maxlength='2' 
+                   size='17'></input></td>
       </tr>
       <tr>
         <td>(1024-65535)</td> <td>(1024-65535)</td> <td>(0-255), or string</td>
@@ -167,18 +167,18 @@ sub output {
     </table>
     };
 
-    if ($results->{engr}) {
+    if ($self->{session}->authorized($results->{user_level}, 'engr')) {
         print qq{
     <p>
     <strong>WARNING</strong>:  Entries in the following fields may change 
     default routing behavior for the selected flow.</p>
 
-    <table cols="2">
-      <tr><td colspan="2"></td></tr>
+    <table cols='2'>
+      <tr><td colspan='2'></td></tr>
       <tr><th>Ingress loopback</th><th>Egress loopback</th></tr>
       <tr>
-        <td><input type="text" name="ingress_router"></input></td>
-        <td><input type="text" name="egress_router"></input></td>
+        <td><input type='text' name='ingress_router'></input></td>
+        <td><input type='text' name='egress_router'></input></td>
       </tr>
       <tr><td>(Host name or IP address)</td><td>(Host name or IP address)</td>
       </tr>
@@ -190,7 +190,7 @@ sub output {
     </div>
     };
     print "</xml>\n";
-} #____________________________________________________________________________ 
+} #____________________________________________________________________________
 
 
 ######
