b59a20786d600bd71b39e17a2f68c0dd4228d777
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-11-23 02:43:24 +0000

    Initial check-in.  Makes a SOAP call to cancel a reservation, given its reservation id, and outputs the results in XML format.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@1584 c75a6b91-02ef-0310-b74a-e20389c9c41d

---
+++ oscars/WBUI/mod_perl/Client/BSS/CancelReservation.pm
@@ -0,0 +1,35 @@
+###############################################################################
+package Client::BSS::CancelReservation;
+
+# Makes a SOAP call to cancel the reservation listed in the reservation
+# details form, and displays the modified reservation results.
+#
+# Last modified:  November 22, 2005
+# David Robertson (dwrobertson@lbl.gov)
+
+use strict;
+
+use Data::Dumper;
+
+use Client::BSS::Details;
+
+use Client::SOAPAdapter;
+our @ISA = qw{Client::SOAPAdapter};
+
+#______________________________________________________________________________
+
+
+###############################################################################
+# output:  prints out the details of the cancelled reservation
+# In:   results of SOAP cancel call
+# Out:  None
+#
+sub output {
+    my( $self, $results ) = @_;
+
+    Client::BSS::Details::output_details($results, $self->{session});
+} #____________________________________________________________________________
+
+
+######
+1;
