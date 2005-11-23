c087f7df3e8fbb1ae8692a19f6d7349623d6014c
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-11-23 02:46:00 +0000

    Moved material to output reservation details to Details.pm .  Changed quotes where interpolation not necessary.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@1587 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/WBUI/mod_perl/Client/BSS/ViewReservations.pm
+++ oscars/WBUI/mod_perl/Client/BSS/ViewReservations.pm
@@ -1,10 +1,9 @@
-###############################################################################
+##############################################################################
 package Client::BSS::ViewReservations;
 
-# Handles request to view a given set of reservations, or a particular
-# reservation's details.
+# Handles request to view a given set of reservations.
 #
-# Last modified:  November 21, 2005
+# Last modified:  November 22, 2005
 # David Robertson (dwrobertson@lbl.gov)
 
 use strict;
@@ -14,12 +13,12 @@ use Data::Dumper;
 use Client::SOAPAdapter;
 our @ISA = qw{Client::SOAPAdapter};
 
-#____________________________________________________________________________ 
+#_____________________________________________________________________________
 
 
 ###############################################################################
 # output:  print list of all reservations if the caller has 
-#          admin privileges, otherwise just print that user's reservations
+#          engr privileges, otherwise just print that user's reservations
 # In:   results of SOAP call
 # Out:  None
 #
@@ -33,19 +32,19 @@ sub output {
     print "<xml>\n";
     print qq{
     <msg>Successfully retrieved reservations.</msg>
-    <div id="zebratable_ui">
+    <div id='zebratable_ui'>
     <p>Click on a column header to sort by that column. Times given are in the
     time zone of the browser.  Click on the Reservation Tag link to view
     detailed information about the reservation.</p>
 
-    <p><form method="post" action="" onsubmit="return submit_form(this,
-            '/perl/adapt.pl?method=list_form', '$params_str');">
-    <input type="submit" value="Refresh"></input>
-    <input type="hidden" name="user_dn" value="$results->{user_dn}">
+    <p><form method='post' action='' onsubmit="return submit_form(this,
+            'view_reservations', '');">
+    <input type='submit' value='Refresh'></input>
+    <input type='hidden' name='user_dn' value="$results->{user_dn}">
     </input>
     </form></p>
 
-    <table cellspacing="0" width="90%" class="sortable" id="reservationlist">
+    <table cellspacing='0' width='90%' class='sortable' id='reservationlist'>
     <thead>
       <tr><td>Tag</td><td>Start Time</td><td>End Time</td><td>Status</td>
           <td>Origin</td><td>Destination</td>
@@ -63,7 +62,7 @@ sub output {
     </div>
     };
     print "</xml>\n";
-} #____________________________________________________________________________ 
+} #____________________________________________________________________________
 
 
 ###############################################################################
@@ -84,9 +83,9 @@ sub print_row {
     print qq{
     <tr>
       <td>
-      <a href="#" style="/styleSheets/layout.css"
-       onclick="return new_page(
-          '/perl/adapt.pl?method=get_details;reservation_id=$row->{reservation_id}');"
+      <a href='#' style='/styleSheets/layout.css'
+       onclick="return new_section(
+          'view_details', 'reservation_id=$row->{reservation_id}');"
           >$row->{reservation_tag}</a>
       </td>
       <td>$row->{reservation_start_time}</td>
@@ -96,110 +95,7 @@ sub print_row {
       <td>$row->{destination_host}</td>
     </tr>
     };
-} #____________________________________________________________________________ 
-
-
-###############################################################################
-# output_details:  print details of reservation returned by SOAP call
-# In:   results of SOAP call
-# Out:  None
-#
-sub output_details {
-    my( $results ) = @_;
-
-    my( $end_time );
-
-    my $params_str;
-
-    if ($results->{reservation_end_time} ne '2039-01-01 00:00:00') {
-        $end_time = $results->{reservation_end_time};
-    }
-    else { $end_time = 'PERSISTENT'; }
-    my $src_port = $results->{reservation_src_port} || 'DEFAULT';
-    my $dst_port = $results->{reservation_dst_port} || 'DEFAULT';
-    my $protocol = $results->{reservation_protocol} || 'DEFAULT';
-    my $dscp = $results->{reservation_dscp} || 'DEFAULT';
-
-    print "<xml>\n";
-    print qq{
-    <msg>Successfully got reservation details.</msg>
-    <div id="zebratable_ui">
-    <p><strong>Reservation Details</strong></p>
-
-    <table width="90%" id="reservationlist">
-      <tr><td>Tag</td><td>$results->{reservation_tag}</td></tr>
-      <tr><td>User</td><td>$results->{user_dn}</td></tr> 
-      <tr><td>Description</td><td>$results->{reservation_description}</tr>
-      <tr><td>Start time</td><td>$results->{reservation_start_time}</td></tr>
-      <tr><td>End time</td><td>$end_time</td></tr>
-      <tr><td>Created time</td><td>$results->{reservation_created_time}
-        </td></tr>
-      <tr><td>Bandwidth</td><td>$results->{reservation_bandwidth}</td></tr>
-      <tr><td>Burst limit</td><td>$results->{reservation_burst_limit}</td></tr>
-      <tr><td>Status</td><td>$results->{reservation_status}</td></tr>
-      <tr><td>Source</td><td>$results->{source_host}</td></tr>
-      <tr><td>Destination</td><td>$results->{destination_host}</td></tr>
-      <tr><td>Source port</td><td>$src_port</td></tr>
-      <tr><td>Destination port</td>$dst_port</td></tr>
-      <tr><td>Protocol</td><td>$protocol</td></tr>
-      <tr><td>DSCP</td><td>$dscp</td></tr>
-    };
-    # TODO:  AAAS must undef these if user doesn't have authorization to set
-    #        If one of these isn't set, none are set
-    if ( $results->{reservaton_class} ) {
-        print qq{
-        <tr><td>Class</td><td>$results->{reservation_class}</td></tr>
-        <tr><td>Ingress router</td><td>$results->{ingress_router}</td></tr>
-        <tr><td>Ingress loopback</td><td>$results->{ingress_ip}</td></tr>
-        <tr><td>Egress router</td><td>$results->{egress_router}</td></tr>
-        <tr><td>Egress loopback</td><td>$results->{egress_ip}</td></tr>
-        <tr><td>Routers in path</td><td>
-        };
-        my $path_str = "";
-        for $_ (@{$results->{reservation_path}}) {
-            $path_str .= $_ . " -> ";
-        }
-        # remove last '->'
-        substr($path_str, -4, 4) = '';
-        print qq{
-        $path_str</td></tr>
-        };
-    }
-
-    if (($results->{reservation_status} eq 'pending') ||
-        ($results->{reservation_status} eq 'active')) {
-        print qq{
-          <td>Action: </td>
-          <td><a href="#" style="/styleSheets/layout.css"
-            onclick="return new_page(
-            '/perl/adapt.pl?method=cancel;reservation_id=$results->{reservation_id}')";
-            >CANCEL</a></td></tr>
-        };
-    }
-
-    print qq{
-    </table>
-    <form method="post" action="" onsubmit="return submit_form(this,
-                                  '/perl/adapt.pl?method=get_details',
-                                  '$params_str');">
-    <input type="hidden" name="user_dn" value="$results->{user_dn}">
-    </input>
-    <input type="hidden" name="reservation_id"
-           value="$results->{reservation_id}">
-    </input>
-    <p><input type="submit" value="Refresh"> 
-    </input></p>
-    </form>
-
-    <p><a href="#" style="/perl/adapt.pl?method=list_form"
-          onclick="return new_page('/perl/adapt.pl?method=list_form');">
-          $results->{user_last_name}
-    <strong>Back to reservations list</strong></a></p>
-    <p>For inquiries, please contact the project administrator.</p>
-    </div>
-    };
-    print  "</xml>\n";
-} #____________________________________________________________________________ 
+} #____________________________________________________________________________
 
 
 ######
