c21f62ae812a40de85fca916bdb77ebce53fbfd5
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-11-23 19:09:39 +0000

    Changed arguments to authorized method, fixed bug in printing error.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@1614 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/servers/AAAS/SOAP/Dispatcher.pm
+++ oscars/servers/AAAS/SOAP/Dispatcher.pm
@@ -53,9 +53,9 @@ sub dispatch {
         my $err = $v->validate($params);
         if ($err) { throw Error::Simple($err); }
         my $method_name = $params->{method};
-        if (!$auth->authorized($params->{user_level}, $method_name)) {
+        if (!$auth->authorized($params, $method_name)) {
             throw Error::Simple(
-                "User $params->{user_dn} not authorized to make $m call");
+                "User $params->{user_dn} not authorized to make $method_name call");
         }
         # AAAS handles this call
         if ( $params->{server_name} ne 'BSS' ) {
