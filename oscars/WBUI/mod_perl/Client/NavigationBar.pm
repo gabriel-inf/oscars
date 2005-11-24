e43d57ea9a49411434d2acc92255146cdd49464d
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-11-24 01:28:06 +0000

    Fixed bug in where id for tab is assigned (was in <li>, now in <a>)
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@1640 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/WBUI/mod_perl/Client/NavigationBar.pm
+++ oscars/WBUI/mod_perl/Client/NavigationBar.pm
@@ -30,47 +30,53 @@ sub initialize {
 
     <navigation_bar>
     <ul id="tabnav">
-      <li id="get_info">
+      <li>
         <a style="/styleSheets/layout.css" title="Information page"
-           class="active"
+           class="active" id="get_info"
            onclick="new_section('get_info');return false;"
            href="#">Information</a>
       </li>
     };
     if ($user_permission eq 'admin') {    # TODO:  FIX auth
       print qq{
-      <li id="view_users">
+      <li>
         <a style="/styleSheets/layout.css" title="List user accounts"
+           id="view_users"
            onclick="new_section('view_users');return false;"
            href="#">List Accounts</a>
       </li>
-      <li id="add_user">
+      <li>
         <a style="/styleSheets/layout.css" title="Add a new user account"
+           id="add_user"
            onclick="new_section('add_user');return false;"
            href="#">Add User</a>
       </li>
       };
     }
     print qq{
-      <li id="get_profile">
+      <li>
         <a style="/styleSheets/layout.css" 
            title="View and/or edit your information"
+           id="get_profile"
            onclick="new_section('get_profile');return false;"
            href="#">User Profile</a>
       </li>
-      <li id="view_reservations">
+      <li>
         <a style="/styleSheets/layout.css" 
            title="View/Edit selected list of reservations"
+           id="view_reservations"
            onclick="new_section('view_reservations');return false;"
            href="#">View/Edit Reservations</a>
       </li>
-      <li id="create_reservation_form">
+      <li>
         <a style="/styleSheets/layout.css" title="Create a new reservation"
+           id="create_reservation_form"
            onclick="new_section('create_reservation_form');return false;"
            href="#">Make Reservation</a>
       </li>
-      <li id="logout">
+      <li>
         <a style="/styleSheets/layout.css" title="Log out on click"
+           id="logout"
            href="/perl/adapt.pl?method=logout">Log out</a>
       </li>
     </ul>
