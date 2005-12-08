80f2a905f480495365d7a7f9b85bf8abc5942b64
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-12-08 04:10:35 +0000

    Going back to original directory structure.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@1743 c75a6b91-02ef-0310-b74a-e20389c9c41d

---
+++ oscars/lib/OSCARS/WBUI/adapt.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl -w
+
+# adapt.pl: Script called by all CGI forms.  Client::SOAPAdapter
+#           (called by Client::Runner) does all the work.
+# Last modified:  December 7, 2005
+# David Robertson (dwrobertson@lbl.gov)
+
+use strict;
+use Client::Runner;
+
+Client::Runner::run();
+
+######
+1;
