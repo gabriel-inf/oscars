98748ed953eb976c4ec1562d86d73c70bce0b908
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-12-08 02:50:33 +0000

    Moving packages from Client to this directory.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@1723 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/lib/OSCARS/WBUI/adapt.pl
+++ oscars/lib/OSCARS/WBUI/adapt.pl
@@ -1,14 +1,14 @@
 #!/usr/bin/perl -w
 
-# adapt.pl: Script called by all CGI forms.  Client::SOAPAdapter
-#           (called by Client::Runner) does all the work.
-# Last modified:  November 15, 2005
+# adapt.pl: Script called by all CGI forms.  SOAPAdapter
+#           (called by Runner) does all the work.
+# Last modified:  December 7, 2005
 # David Robertson (dwrobertson@lbl.gov)
 
 use strict;
-use Client::Runner;
+use OSCARS::WBUI::Runner;
 
-Client::Runner::run();
+OSCARS::WBUI::Runner::run();
 
 ######
 1;
