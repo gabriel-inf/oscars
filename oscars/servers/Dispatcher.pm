0c696a46f0596d85e37c7559cda2f2406ebcca4c
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-12-08 02:16:41 +0000

    Part of one last major rearrangement to make OSCARS more like a standard Perl module, and to flatten out the hierarchy a bit.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@1695 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/servers/Dispatcher.pm
+++ oscars/servers/Dispatcher.pm
@@ -1,7 +1,8 @@
-package Dispatcher;
+##############################################################################
+package OSCARS::Dispatcher;
 
-# Dispatcher.pm:  SOAP::Lite meta-dispatcher for all servers
-# Last modified:  November 10, 2005
+# SOAP::Lite meta-dispatcher for all servers
+# Last modified:  Dedember 7, 2005
 # David Robertson (dwrobertson@lbl.gov)
 
 use FindBin::Real;
@@ -10,18 +11,19 @@ use Data::Dumper;
 
 # TODO:  these should be dynamically determined instead
 use lib qw(/usr/local/esnet/servers/prod);
-use AAAS::SOAP::Dispatcher;
+use OSCARS::AAAS::Dispatcher;
 
+
+##############################################################################
 sub dispatch {
     my ( $class_name, $params ) = @_;
 
     # TODO:  get URI and extract prod or test directory name
     #        build hash of dispatchers, dispatch to right version
     #        by manipulating %INC (somehow) with directory name
-    $results = AAAS::SOAP::Dispatcher->dispatch($params);
+    $results = OSCARS::AAAS::Dispatcher->dispatch($params);
     return $results;
-}
-######
+} #___________________________________________________________________________
 
 ######
 1;
