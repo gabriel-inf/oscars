b9f9c6fd827ccc5711fb508f21f0f74ee36e9800
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-12-14 01:02:09 +0000

    Logging moved back to AAAS.  No AAAS dependencies again in BSS.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@1822 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/lib/OSCARS/BSS/Dispatcher.pm
+++ oscars/lib/OSCARS/BSS/Dispatcher.pm
@@ -4,14 +4,13 @@ package OSCARS::BSS::Dispatcher;
 # SOAP::Lite dispatcher for BSS.  Note well:  calls to the BSS are currently
 # assumed to have already been validated and authorized through the AAAS.
 #
-# Last modified:  December 9, 2005
+# Last modified:  December 13, 2005
 # David Robertson (dwrobertson@lbl.gov)
 # Jason Lee       (jrlee@lbl.gov)
 
 use Data::Dumper;
 use Error qw(:try);
 
-use OSCARS::AAAS::Logger;
 use OSCARS::BSS::Database;
 use OSCARS::BSS::Methods;
 
@@ -39,26 +38,14 @@ sub dispatch {
     my $results = {};
     try {
         my $method_name = $params->{method};
-        $params->{logger} =
-                OSCARS::AAAS::Logger->new( 'dir' => '/home/oscars/logs',
-                                           'method' => $method_name);
-        $params->{logger}->start_log( $params->{user_dn} );
         $results = $request_handler->$method_name($params) ;
     }
-    catch Error::Simple with {
-        $ex = shift;
-    }
-    otherwise {
-        $ex = shift;
-    }
-    finally {
-        if ($ex) {
-            $params->{logger}->write_log("BSS EXCEPTION:");
-            $params->{logger}->write_log("BSS: $ex->{-text}");
-        }
-    };
-    $params->{logger}->end_log( $results );
-    # caught by SOAP to indicate fault
+    catch Error::Simple with { $ex = shift; }
+    otherwise { $ex = shift; }
+    finally { ; };
+
+    # Caught by SOAP to indicate fault.  AAAS uses faultstring to update
+    # its log for this call.
     if ($ex) {
         die SOAP::Fault->faultcode('Server')
                  ->faultstring($ex->{-text});
