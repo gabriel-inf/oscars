528f4d8026a2f670132b8f5e77989436a4eb0c92
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2005-12-25 00:15:47 +0000

    Fixed method names, uri, and proxy.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@1958 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/cron/scheduler.pl
+++ oscars/cron/scheduler.pl
@@ -4,21 +4,19 @@
 
 use SOAP::Lite;
 
-use lib qw(/usr/local/esnet/servers/prod);
-
 use strict;
 
 my %params;
 
 my $soap_server = SOAP::Lite
-    ->uri('http://localhost:2000/OSCARS/AAAS/Dispatcher')
-    ->proxy('http://localhost:2000/aaas');
+    ->uri('http://localhost:2000/OSCARS/Dispatcher')
+    ->proxy('http://localhost:2000/Server');
 
 $params{user_dn} = 'scheduler';
 $params{time_interval} = 20;
-$params{method} = 'find_pending_reservations';
+$params{method} = 'FindPendingReservations';
 $params{server_name} = 'BSS';
 $params{user_level} = 4;
 my $som = $soap_server->dispatch(\%params);
-$params{method} = 'find_expired_reservations';
+$params{method} = 'FindExpiredReservations';
 $som = $soap_server->dispatch(\%params);
