51675d9ba0b48dc36af8c9615dba338ba5c21a3f
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2006-02-14 22:32:24 +0000

    Removed unused material.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@2211 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/lib/OSCARS/User.pm
+++ oscars/lib/OSCARS/User.pm
@@ -1,4 +1,4 @@
-###############################################################################
+#==============================================================================
 package OSCARS::User;
 
 =head1 NAME
@@ -11,9 +11,10 @@ OSCARS::User - Handles user database connections, sessions, and history.
 
 =head1 DESCRIPTION
 
-This module contains information about all users currently logged in.
-It maintains persistent information about each user is in the OSCARS database.
-It inherits from OSCARS::Database.
+This module contains information about one user currently logged in.
+It caches information about that user, retrieved from the OSCARS 
+database.   All operations performed against the database go through
+this class, which maintains the user's database handle via the superclass.
 
 =head1 AUTHOR
 
@@ -21,7 +22,7 @@ David Robertson (dwrobertson@lbl.gov)
 
 =head1 LAST MODIFIED
 
-December 20, 2005
+February 10, 2006
 
 =cut
 
@@ -39,8 +40,38 @@ sub new {
     my( $self ) = { %args };
   
     bless( $self, $class );
+    $self->initialize();
     return( $self );
 }
+
+sub initialize {
+    my( $self ) = @_;
+
+    # initially not authenticated
+    $self->{is_authenticated} = 0;
+    $self->connect( $self->{database} );
+} #____________________________________________________________________________
+
+
+###############################################################################
+# authenticated:  returns whether user has been authenticated or not
+#
+sub authenticated {
+    my( $self ) = @_;
+
+    return $self->{is_authenticated};
+} #____________________________________________________________________________
+
+
+###############################################################################
+# set_authenticated:  set's user authentication status
+#
+sub set_authenticated {
+    my( $self, $auth_status ) = @_;
+
+    $self->{is_authenticated} = $auth_status;
+} #____________________________________________________________________________
+
 
 ######
 1;
