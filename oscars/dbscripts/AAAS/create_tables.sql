abbcfbdbb36bece5dc1d9ad999a8dfdadd99b77f
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2006-02-14 22:13:55 +0000

    New authorizations, resources, resourcepermissions, and permissions tables.  Redid servers table (now daemons table).
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@2195 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/dbscripts/AAAS/create_tables.sql
+++ oscars/dbscripts/AAAS/create_tables.sql
@@ -5,79 +5,84 @@ CREATE DATABASE IF NOT EXISTS AAAS;
 USE AAAS;
 
 CREATE TABLE IF NOT EXISTS users (
-    user_id INT(6) NOT NULL AUTO_INCREMENT,
-    user_last_name VARCHAR(30) NOT NULL,
-    user_first_name VARCHAR(20),
-    user_dn VARCHAR(30) NOT NULL,
-    user_password varchar(30),
-    user_email_primary VARCHAR(30) NOT NULL,
-    user_level INT(2) NOT NULL,
-    user_email_secondary VARCHAR(30),
-    user_phone_primary VARCHAR(20),
-    user_phone_secondary VARCHAR(20),
-    user_description TEXT,
-    user_status VARCHAR(20),
-    user_last_active_time DATETIME,
-    user_register_time DATETIME,
-    user_activation_key VARCHAR(40),
-    institution_id INT(6) NOT NULL, -- foreign key
+    user_id			INT NOT NULL AUTO_INCREMENT,
+    user_dn			TEXT NOT NULL,
+    user_last_name	 	TEXT NOT NULL,
+    user_first_name		TEXT NOT NULL,
+    user_email_primary		TEXT NOT NULL,
+    user_phone_primary		TEXT NOT NULL,
+    user_password		TEXT,
+    user_description		TEXT,
+    user_email_secondary	TEXT,
+    user_phone_secondary	TEXT,
+    user_status			TEXT,
+    user_activation_key		TEXT,
+    user_last_active_time	DATETIME,
+    user_register_time		DATETIME,
+    institution_id		INT NOT NULL,	-- foreign key
     PRIMARY KEY (user_id)
 
 ) type=MyISAM;
 
 CREATE TABLE IF NOT EXISTS institutions (
-    institution_id INT(5) NOT NULL AUTO_INCREMENT,
-    institution_name VARCHAR(50),
+    institution_id		INT NOT NULL AUTO_INCREMENT,
+    institution_name		TEXT NOT NULL,
     PRIMARY KEY (institution_id)
 ) type=MyISAM;
 
+CREATE TABLE IF NOT EXISTS resources (
+    resource_id			INT NOT NULL AUTO_INCREMENT,
+    resource_hidden		BOOLEAN NOT NULL,
+    resource_name		TEXT NOT NULL,
+    resource_type               TEXT NOT NULL,
+    resource_description	TEXT,
+    resource_update_time	DATETIME NOT NULL,
+    PRIMARY KEY (resource_id)
+) type=MyISAM;
 
-CREATE TABLE IF NOT EXISTS user_levels (
-    user_level_id INT(5) NOT NULL AUTO_INCREMENT,
-    user_level_bit INT(3) NOT NULL,
-    user_level_description VARCHAR(16) NOT NULL,
-    auth_type_id INT(3) NOT NULL,
-    PRIMARY KEY (user_level_id)
+CREATE TABLE IF NOT EXISTS permissions (
+    permission_id		INT NOT NULL AUTO_INCREMENT,
+    permission_name		TEXT NOT NULL,
+    permission_description	TEXT,
+    permission_update_time	DATETIME NOT NULL,
+    PRIMARY KEY (permission_id)
+) type=MyISAM;
+
+-- cross reference table
+CREATE TABLE IF NOT EXISTS resourcepermissions (
+    resource_id			INT NOT NULL,	-- foreign key
+    permission_id		INT NOT NULL,	-- foreign key
+    PRIMARY KEY (resource_id, permission_id)
+) type=MyISAM;
+
+CREATE TABLE IF NOT EXISTS authorizations (
+    authorization_id		INT NOT NULL AUTO_INCREMENT,
+    authorization_context	TEXT NOT NULL,
+    authorization_update_time	DATETIME NOT NULL,
+    user_id			INT NOT NULL,	-- foreign key
+    resource_id 		INT NOT NULL,	-- foreign key
+    permission_id		INT NOT NULL,	-- foreign key
+    PRIMARY KEY (authorization_id)
 ) type=MyISAM;
 
 
-CREATE TABLE IF NOT EXISTS auth_types (
-    auth_type_id INT(3) NOT NULL AUTO_INCREMENT,
-    auth_name VARCHAR(30),
-    PRIMARY KEY (auth_type_id)
-) type=MyISAM;
-
-
-CREATE TABLE IF NOT EXISTS allocations (
-    allocation_id INT(5) NOT NULL AUTO_INCREMENT,
-        -- in seconds
-    allocation_amount INT(9),
-    allocation_used INT(9),
-    user_id INT(6) NOT NULL,
-    PRIMARY KEY (allocation_id)
-) type = MyISAM;
-
-
--- Table containing default AAAS server info, and info about each running 
--- server.  When a server is started, it registers itself by creating
--- an entry in this table.
-CREATE TABLE IF NOT EXISTS servers (
-    server_id               INT NOT NULL AUTO_INCREMENT,
-      -- Same name as the associated OSCARS component
-    server_name             TEXT,
-      -- whether this server should be started
-    server_use              BOOLEAN NOT NULL,
-    server_port             INT NOT NULL,
-      -- used to start SOAP::Lite server
-    server_uri              TEXT,
-    server_proxy            TEXT,
-
-        -- debug level
-    server_debug            INT NOT NULL,
-    server_start_time       DATETIME,
-    server_end_time         DATETIME,
-
-    server_mail_list        VARCHAR(36) NOT NULL,
-    server_send_mail        BOOLEAN NOT NULL,
-    PRIMARY KEY (server_id)
+-- Table containing information necessary to start up a SOAP::Lite daemon
+-- and for clients to access it.
+CREATE TABLE IF NOT EXISTS daemons (
+    daemon_id			INT NOT NULL AUTO_INCREMENT,
+      --  currently only AAAS and BSS
+    daemon_component_type	TEXT NOT NULL,
+      --  defaults are AAAS and BSS, can be other ones set up for testing
+    daemon_component_name	TEXT NOT NULL,
+      -- used to start SOAP::Lite daemon
+    daemon_host_name		TEXT NOT NULL,
+    daemon_port			INT NOT NULL,
+      -- information necessary for client of this server. Built up from
+      -- host name, port, and class name.
+    daemon_dispatcher_class	TEXT NOT NULL,
+    daemon_server_class		TEXT NOT NULL,
+      -- not currently used
+    daemon_mail_list		TEXT NOT NULL,
+    daemon_send_mail		BOOLEAN NOT NULL,
+    PRIMARY KEY (daemon_id)
 ) type=MyISAM;
