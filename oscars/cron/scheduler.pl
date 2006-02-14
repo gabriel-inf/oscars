95bb6196aece041b09b225d4eed2003fc5f4740f
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2006-02-14 22:07:35 +0000

    Updated for ROAM changes.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@2187 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/cron/scheduler.pl
+++ oscars/cron/scheduler.pl
@@ -1,22 +1,64 @@
 #!/usr/bin/perl -w
 
-# Run by cron script
+use strict;
 
 use SOAP::Lite;
 
-use strict;
+use OSCARS::ResourceManager;
+use Data::Dumper;
 
-my %params;
+my $db_name = 'AAAS';
+my $aaa_component_name = 'AAAS';
+my $bss_component_name = 'BSS';
+my $rm = OSCARS::ResourceManager->new( 'database' => $db_name);
 
-my $soap_server = SOAP::Lite
-    ->uri('http://localhost:2000/OSCARS/Dispatcher')
-    ->proxy('http://localhost:2000/Server');
+my( $login, $password ) = $rm->get_test_account('engr');
 
-$params{user_dn} = 'scheduler';
-$params{time_interval} = 20;
-$params{method} = 'FindPendingReservations';
-$params{server_name} = 'BSS';
-$params{user_level} = 4;
-my $som = $soap_server->dispatch(\%params);
-$params{method} = 'FindExpiredReservations';
-$som = $soap_server->dispatch(\%params);
+my( $status, $msg ) = FindPendingReservations( $login, $password );
+( $status, $msg ) = FindExpiredReservations( $login, $password );
+
+
+#############################################################################
+#
+sub FindPendingReservations {
+    my ( $user_dn, $password ) = @_;
+
+    # password necessary for test to run, but not for this method in general
+    my %params = ('user_dn' => $user_dn, 'user_password' => $password );
+
+    $params{server} = $bss_component_name;
+    $params{method} = 'FindPendingReservations';
+    $params{time_interval} = 20;
+
+    my $som = $rm->add_client($aaa_component_name)->dispatch(\%params);
+    if ($som->faultstring) { return( 0, $som->faultstring ); }
+    my $results = $som->result;
+
+    my $msg = "\nReservations handled:\n";
+    $msg .= Dumper($results->{list});
+    $msg .= "\n";
+    return( 1, $msg );
+} #___________________________________________________________________________
+
+
+#############################################################################
+#
+sub FindExpiredReservations {
+    my ( $user_dn, $password ) = @_;
+
+    # password necessary for test to run, but not for this method in general
+    my %params = ('user_dn' => $user_dn, 'user_password' => $password );
+
+    $params{server} = $bss_component_name;
+    $params{method} = 'FindExpiredReservations';
+    $params{time_interval} = 20;
+
+    my $som = $rm->add_client($aaa_component_name)->dispatch(\%params);
+    if ($som->faultstring) { return( 0, $som->faultstring ); }
+    my $results = $som->result;
+
+    my $msg = "\nReservations handled:\n";
+    $msg .= Dumper($results->{list});
+    $msg .= "\n";
+    return( 1, $msg );
+} #___________________________________________________________________________
