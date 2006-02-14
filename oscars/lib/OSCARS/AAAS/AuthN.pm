4773dc1bf7a4fc34d2f7d207b5a7de39333b5028
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2006-02-14 22:36:40 +0000

    Moved authentication material from Method/Login.pm to here.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@2218 c75a6b91-02ef-0310-b74a-e20389c9c41d

---
+++ oscars/lib/OSCARS/AAAS/AuthN.pm
@@ -0,0 +1,76 @@
+#==============================================================================
+package OSCARS::AAAS::AuthN;
+
+=head1 NAME
+
+OSCARS::AAAS::AuthN - performs authenticatication for OSCARS
+
+=head1 SYNOPSIS
+
+  use OSCARS::AAAS::AuthN;
+
+=head1 DESCRIPTION
+
+Performs authentication required to access OSCARS.
+
+=head1 AUTHORS
+
+David Robertson (dwrobertson@lbl.gov)
+Soo-yeon Hwang (dapi@umich.edu)
+
+=head1 LAST MODIFIED
+
+February 10, 2006
+
+=cut
+
+
+use strict;
+
+use Data::Dumper;
+use Error qw(:try);
+
+
+sub new {
+    my ($class, %args) = @_;
+    my ($self) = {%args};
+  
+    bless($self, $class);
+    return($self);
+} #____________________________________________________________________________
+
+
+###############################################################################
+# authenticate:  authenticates user
+#
+# In:  user DN, hash of paraeters
+# Out: reference to hash of results containing user dn and user level.
+#
+sub authenticate {
+    my( $self, $user, $params ) = @_;
+
+    if ($user->authenticated()) { return 1; }
+    if (!$params->{user_password}) {
+        throw Error::Simple('Attempting to access a SOAP method before authenticating.');
+    }
+    # Get the password and privilege level from the database.
+    my $statement = 'SELECT user_password FROM users WHERE user_dn = ?';
+    my $results = $user->get_row($statement, $user->{dn});
+    # Make sure user exists.
+    if ( !$results ) {
+        throw Error::Simple('Please check your login name and try again.');
+    }
+    # compare passwords
+    my $encoded_password = crypt($params->{user_password}, 'oscars');
+    if ( $results->{user_password} ne $encoded_password ) {
+        # see if password already encrypted
+        if ( $results->{user_password} ne $params->{user_password} ) {
+            throw Error::Simple('Please check your password and try again.');
+	}
+    }
+    $user->set_authenticated(1);
+} #____________________________________________________________________________
+
+
+######
+1;
