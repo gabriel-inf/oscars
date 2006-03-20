fdf51d07009d45dfa6045504e31cf6d995a8e81a
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2006-03-20 22:52:08 +0000

    Had not kept this up to date.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@2318 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/cron/scheduler.pl
+++ oscars/cron/scheduler.pl
@@ -8,29 +8,28 @@ use OSCARS::ResourceManager;
 use Data::Dumper;
 
 my $db_name = 'AAAS';
-my $aaa_component_name = 'AAAS';
 my $bss_component_name = 'BSS';
 my $rm = OSCARS::ResourceManager->new( 'database' => $db_name);
 
-my( $login, $password ) = $rm->get_test_account('engr');
+my( $user_login, $user_password ) = $rm->get_test_account('engr');
 
-my( $status, $msg ) = FindPendingReservations( $login, $password );
-( $status, $msg ) = FindExpiredReservations( $login, $password );
+my( $status, $msg ) = FindPendingReservations( $user_login, $user_password );
+( $status, $msg ) = FindExpiredReservations( $user_login, $user_password );
 
 
 #############################################################################
 #
 sub FindPendingReservations {
-    my ( $user_dn, $password ) = @_;
+    my ( $user_login, $user_password ) = @_;
 
     # password necessary for test to run, but not for this method in general
-    my %params = ('user_dn' => $user_dn, 'user_password' => $password );
+    my %params = ('user_login' => $user_login, 'user_password' => $user_password );
 
     $params{server} = $bss_component_name;
     $params{method} = 'FindPendingReservations';
     $params{time_interval} = 20;
 
-    my $som = $rm->add_client($aaa_component_name)->dispatch(\%params);
+    my $som = $rm->add_client()->dispatch(\%params);
     if ($som->faultstring) { return( 0, $som->faultstring ); }
     my $results = $som->result;
 
@@ -44,16 +43,16 @@ sub FindPendingReservations {
 #############################################################################
 #
 sub FindExpiredReservations {
-    my ( $user_dn, $password ) = @_;
+    my ( $user_login, $user_password ) = @_;
 
     # password necessary for test to run, but not for this method in general
-    my %params = ('user_dn' => $user_dn, 'user_password' => $password );
+    my %params = ('user_login' => $user_login, 'user_password' => $user_password );
 
     $params{server} = $bss_component_name;
     $params{method} = 'FindExpiredReservations';
     $params{time_interval} = 20;
 
-    my $som = $rm->add_client($aaa_component_name)->dispatch(\%params);
+    my $som = $rm->add_client()->dispatch(\%params);
     if ($som->faultstring) { return( 0, $som->faultstring ); }
     my $results = $som->result;
 
