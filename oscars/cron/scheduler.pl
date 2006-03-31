24438ff5c1c0aac2c098b229a45ca1f49e6a05b9
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2006-03-31 21:16:19 +0000

    Updates for using authentication plugin.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@2370 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/cron/scheduler.pl
+++ oscars/cron/scheduler.pl
@@ -10,8 +10,9 @@ use Data::Dumper;
 my $db_name = 'AAAS';
 my $bss_component_name = 'BSS';
 my $rm = OSCARS::ResourceManager->new( 'database' => $db_name);
+my $aaa_status = $rm->use_authentication_plugin('OSCARS::AAAS::AuthN', $db_name);
 
-my( $user_login, $user_password ) = $rm->get_test_account('engr');
+my( $user_login, $user_password ) = $rm->get_test_account('testaccount');
 
 my( $status, $msg ) = FindPendingReservations( $user_login, $user_password );
 ( $status, $msg ) = FindExpiredReservations( $user_login, $user_password );
