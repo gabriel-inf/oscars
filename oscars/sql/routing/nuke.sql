26d203d881784aa661e286461af1afbae3b84bfa
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2006-04-22 21:15:59 +0000

    This database will contain only topology information.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@2529 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/sql/routing/nuke.sql
+++ oscars/sql/routing/nuke.sql
@@ -1,13 +1,3 @@
-DROP TABLE clients;
 DROP TABLE routers;
 DROP TABLE interfaces;
 DROP TABLE ipaddrs;
-DROP TABLE hosts;
-DROP TABLE reservations;
-DROP TABLE scheduler_confs;
-DROP TABLE trace_confs;
-DROP TABLE snmp_confs;
-DROP TABLE pss_confs;
-DROP TABLE archived_reservations;
-DROP TABLE test_confs;
-DROP TABLE test_addresses;
