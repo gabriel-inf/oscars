933c769f95db7a934a1ac691cde09e84f9b97586
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2006-04-26 18:18:33 +0000

    Created new view to directly return summarized user list.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@2591 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/sql/oscars/createViews.sql
+++ oscars/sql/oscars/createViews.sql
@@ -1,6 +1,13 @@
 USE oscars;
 
-CREATE VIEW userReservations AS
+CREATE OR REPLACE VIEW userReservations AS
     SELECT startTime, endTime, createdTime, bandwidth, burstLimit, status,
            class, srcPort, destPort, dscp, protocol, tag, description,
            srcHostId, destHostId, origTimeZone FROM reservations;
+
+CREATE OR REPLACE VIEW userList AS
+    SELECT u.lastName, u.firstName, u.login,
+           i.name AS institutionName, u.phonePrimary
+    FROM users u, institutions i
+    WHERE i.id = u.institutionId
+    ORDER BY u.lastName;
