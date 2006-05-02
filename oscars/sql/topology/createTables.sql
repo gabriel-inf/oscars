291e4812356ad8c7e330bac9f9cb0864e7a4e856
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2006-05-02 03:25:40 +0000

    Moved topology-related configuration tables here from oscars database, name changes in configTrace, configPSS, and addition of dscp column to configPSS.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@2605 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/sql/topology/createTables.sql
+++ oscars/sql/topology/createTables.sql
@@ -37,3 +37,76 @@ CREATE TABLE IF NOT EXISTS ipaddrs (
     interfaceId		INT NOT NULL,	-- foreign key
     PRIMARY KEY (id)
 ) type=MyISAM;
+
+
+-- table for intradomain paths of reservations
+CREATE TABLE IF NOT EXISTS paths (
+    pathId			INT NOT NULL AUTO_INCREMENT,
+       -- total number of hops, including ingress and egress
+    numHops			INT,
+    PRIMARY KEY (pathId)
+) type=MyISAM;
+
+-- cross reference table
+CREATE TABLE IF NOT EXISTS pathInterfaces (
+    pathId			INT NOT NULL,	-- foreign key
+    interfaceId			INT NOT NULL,	-- foreign key
+       -- used to order path
+    sequenceNumber              INT NOT NULL,
+    PRIMARY KEY (pathId, interfaceId)
+) type=MyISAM;
+
+
+-- Configuration tables ------------------------------------------
+
+-- Configuration for route discovery
+CREATE TABLE IF NOT EXISTS configTrace (
+    id			INT NOT NULL AUTO_INCREMENT,
+      -- traceroute information section
+        -- source of traceroutes
+    jnxSource		TEXT NOT NULL,
+        -- SSH variables to use for Juniper login
+    jnxUser		TEXT NOT NULL,
+    jnxKey 		TEXT NOT NULL,
+        -- traceroute configuration variables
+    ttl			INT NOT NULL,
+    timeout		INT NOT NULL,
+        -- Whether to run traceroute from the routers
+        -- (requires ssh keys setup)
+    runTrace		BOOLEAN NOT NULL,
+    PRIMARY KEY (id)
+) type=MyISAM;
+
+-- Configuration for SNMP queries
+CREATE TABLE IF NOT EXISTS configSNMP (
+    id			INT NOT NULL AUTO_INCREMENT,
+        -- community string (TODO:  secure enough here?
+    community		TEXT NOT NULL,
+    version		TEXT NOT NULL,
+    domainSuffix	TEXT NOT NULL,
+    port		INT NOT NULL,
+    timeout		INT NOT NULL,
+    retries		INT NOT NULL,
+    PRIMARY KEY (id)
+) type=MyISAM;
+
+-- Configuration for LSP setup and teardown
+CREATE TABLE IF NOT EXISTS configPSS (
+    id			INT NOT NULL AUTO_INCREMENT,
+    access		TEXT NOT NULL,
+    login		TEXT NOT NULL,
+    passwd		TEXT NOT NULL,
+    firewallMarker	TEXT NOT NULL,
+    setupFile		TEXT NOT NULL,
+    teardownFile	TEXT NOT NULL,
+    externalInterfaceFilter	TEXT NOT NULL,
+    CoS			INT NOT NULL,
+    dscp		TEXT NOT NULL,
+        -- in bps
+    burstLimit		INT NOT NULL,
+    setupPriority	INT NOT NULL,
+    reservationPriority	INT NOT NULL,
+        -- allow LSP configuration
+    allowLSP		BOOLEAN NOT NULL,
+    PRIMARY KEY (id)
+) type=MyISAM;
--- oscars/sql/topology/dropTables.sql
+++ oscars/sql/topology/dropTables.sql
@@ -1,3 +1,8 @@
 DROP TABLE routers;
 DROP TABLE interfaces;
 DROP TABLE ipaddrs;
+DROP TABLE paths;
+DROP TABLE pathInterfaces;
+DROP TABLE configTrace;
+DROP TABLE configSNMP;
+DROP TABLE configPSS;
