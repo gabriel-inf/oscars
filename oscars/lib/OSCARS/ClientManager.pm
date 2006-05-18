ee71d9e5876c3a99165f1468c8a9a07e4ebcd84d
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2006-05-18 06:00:32 +0000

    Caching information from clients table, rather than SOAP::Lite clients. Added on_action to client creation (necessary for doc/literal).
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@2695 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/lib/OSCARS/ClientManager.pm
+++ oscars/lib/OSCARS/ClientManager.pm
@@ -3,7 +3,7 @@ package OSCARS::ClientManager;
 
 =head1 NAME
 
-OSCARS::ClientManager - SOAP client manager.
+OSCARS::ClientManager - SOAP client creator.
 
 =head1 SYNOPSIS
 
@@ -11,16 +11,17 @@ OSCARS::ClientManager - SOAP client manager.
 
 =head1 DESCRIPTION
 
-Manages SOAP clients.  TODO:  Persistent instance, rather than one per
-request.
+Creates SOAP clients, using cached information from the clients table in the
+oscars database.
 
 =head1 AUTHORS
 
 David Robertson (dwrobertson@lbl.gov)
+Mary Thompson (mrthompson@lbl.gov)
 
 =head1 LAST MODIFIED
 
-May 3, 2006
+May 17, 2006
 
 =cut
 
@@ -50,45 +51,56 @@ sub new {
 sub initialize {
     my( $self ) = @_;
 
-    $self->{clients} = {};
-    #Points to the public key of the X509 certificate
+    # cache information from the clients table
+    $self->{clientInfo} = $self->getClientInfo();
+    # points to the public key of the X509 certificate
     $ENV{HTTPS_CERT_FILE} = $ENV{HOME}."/.globus/usercert.pem";
-    #Points to the private key of the cert - must be unencrypted
+    # points to the private key of the cert - must be unencrypted
     $ENV{HTTPS_KEY_FILE}  = $ENV{HOME}."/.globus/userkey.pem";
-    #Tells WSRF::Lite to sign the message with the above cert
+    # tells WSRF::Lite to sign the message with the above cert
     $ENV{WSS_SIGN} = 'true';
 } #____________________________________________________________________________
 
 
 ###############################################################################
-# getClient:  Adds WSRF::Lite client for given domain to clients 
-#              hash, indexed by autonomous system number.
+# getClientInfo:  Caches information from clients table in oscars database.
 #
-sub getClient {
-    my( $self, $asNum ) = @_;
+sub getClientInfo {
+    my( $self ) = @_;
 
-    my( $statement, $client );
-
-    if (!$asNum) { $asNum = 'local'; }
-    if ($self->{clients}->{$asNum}) { return $self->{clients}->{$asNum}; }
     my $dbconn = OSCARS::Database->new();
     $dbconn->connect($self->{database});
-    # currently only handles one server per domain
-    if ($asNum ne 'local') {
-        $statement = 'SELECT * FROM clients WHERE asNum = ?';
-        $client = $dbconn->getRow($statement, $asNum);
-    }
-    else {
-	# local domain not given an AS number in the clients table
-        $statement = 'SELECT * FROM clients WHERE asNum IS NULL';
-        $client = $dbconn->getRow($statement);
-    }
+    my $statement = 'SELECT * FROM clients';
+    my $rows = $dbconn->doSelect($statement);
     $dbconn->disconnect();
-    if (!$client) { return undef; }
-    $self->{clients}->{$asNum} = WSRF::Lite
-                                        -> uri($client->{uri})
-                                        -> proxy($client->{proxy});
-    return $self->{clients}->{$asNum};
+    my $clientInfo = {};
+    for my $row (@$rows) {
+	my $domain = $row->{asNum};
+	if (!$domain) { $domain = 'local'; }
+	$clientInfo->{$domain} = {};
+	$clientInfo->{$domain}->{uri} = $row->{uri};
+	$clientInfo->{$domain}->{proxy} = $row->{proxy};
+    }
+    $clientInfo->{namespace} = 'http://oscars.es.net/OSCARS/Dispatcher';
+    return $clientInfo;
+} #____________________________________________________________________________
+
+
+###############################################################################
+# getClient:  Returns new WSRF::Lite client for given domain, with a SOAP
+#             action set to the given method.
+#
+sub getClient {
+    my( $self, $methodName, $domain ) = @_;
+
+    # default is local domain
+    if ( !$domain ) { $domain = 'local'; }
+    my $soapAction = $self->{clientInfo}->{namespace} . '/' . $methodName;
+    my $client = WSRF::Lite
+        -> uri( $self->{clientInfo}->{$domain}->{uri} )
+        -> proxy( $self->{clientInfo}->{$domain}->{proxy} )
+	-> on_action ( sub { return "$soapAction" } );
+    return $client;
 } #____________________________________________________________________________
 
 
