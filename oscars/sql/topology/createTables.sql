04c7f6efe57ef98e2c5c1d9e217f1435f6bb8865
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2006-06-20 05:12:30 +0000

    Further normalization; moved information in loopback and traceAddress fields in routers to description field in ipaddrs.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@2885 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/sql/topology/createTables.sql
+++ oscars/sql/topology/createTables.sql
@@ -6,9 +6,6 @@ CREATE TABLE IF NOT EXISTS routers (
     id			INT NOT NULL AUTO_INCREMENT,
     valid		BOOLEAN NOT NULL,
     name		TEXT NOT NULL,
-        -- loopback interface IP, if present
-    loopback		TEXT,
-    traceAddress	TEXT,
     PRIMARY KEY (id)
 ) type=MyISAM;
 
@@ -34,6 +31,8 @@ CREATE TABLE IF NOT EXISTS ipaddrs (
     id			INT NOT NULL AUTO_INCREMENT,
         -- IP address
     IP			TEXT NOT NULL,
+        -- description (currently loopback, traceAddress, or NULL)
+    description         TEXT,
         -- key of corresponding interface in interfaces table
     interfaceId		INT NOT NULL,	-- foreign key
     PRIMARY KEY (id)
