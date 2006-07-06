c46561dcbae5da1475d15e6a6dfab1b5210a5520
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2006-07-06 22:56:32 +0000

    Name change for table.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@2944 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars/sql/topology/dropTables.sql
+++ oscars/sql/topology/dropTables.sql
@@ -2,7 +2,7 @@ DROP TABLE routers;
 DROP TABLE interfaces;
 DROP TABLE ipaddrs;
 DROP TABLE paths;
-DROP TABLE pathInterfaces;
+DROP TABLE pathIpaddrs;
 DROP TABLE configTrace;
 DROP TABLE configSNMP;
 DROP TABLE configPSS;
