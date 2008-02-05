5c2959b6a71af7ecf8c478d5009c4bb1d2511445
haniotak <haniotak@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2008-02-05 23:38:03 +0000

    Added dynamic class loading based on config for notify subsystem
    See examples/server/oscars.properties as to how to configure
    
    See src.net.es.oscars.notify.FileWriterObserver.java for
    a simple example of a custom class
    
    Users can add their own class which will be dynamically loaded
    at runtime as long as the .jar or .class file is visible to
    the system classloader.
    
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@5957 c75a6b91-02ef-0310-b74a-e20389c9c41d

---
+++ src/net/es/oscars/notify/FileWriterObserver.java
@@ -0,0 +1,35 @@
+package net.es.oscars.notify;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+
+
+import net.es.oscars.PropHandler;
+
+/**
+ * 
+ * @author haniotak
+ * Example notify observer class
+ *
+ */
+public class FileWriterObserver implements Observer {
+
+    public FileWriterObserver() {
+    }
+
+    public void update (Observable obj, Object arg) {
+		BufferedWriter out = null;
+		try {
+			// obviously do different stuff here
+			out = new BufferedWriter( new FileWriter( "/tmp/fileToWrite.txt"));
+			out.write(arg.toString());
+			out.flush();
+			
+		} catch (IOException e){
+			// do something
+		}
+
+    }
+}
--- src/net/es/oscars/notify/NotifyInitializer.java
+++ src/net/es/oscars/notify/NotifyInitializer.java
@@ -2,9 +2,16 @@ package net.es.oscars.notify;
 
 import java.util.*;
 
+import org.apache.log4j.Logger;
+
+import net.es.oscars.PropHandler;
+
 
 public class NotifyInitializer {
-	private NotifierSource source;
+    private Properties props;
+    private Logger log;
+    
+    private NotifierSource source;
 	
 	public NotifierSource getSource() {
 		return source;
@@ -19,13 +26,34 @@ public class NotifyInitializer {
 	
 
     public NotifyInitializer() {
+        this.log = Logger.getLogger(this.getClass());
+
+        PropHandler propHandler = new PropHandler("oscars.properties");
+        this.props = propHandler.getPropertyGroup("notify.observer", true);
+
     	this.source = new NotifierSource();
     	this.observers = new ArrayList<Observer>();
     	
-    	// TODO: dynamically add observers from config params
-
-    	EmailObserver email = new EmailObserver();
-    	this.addObserver(email);
+    	
+    	ClassLoader cl = ClassLoader.getSystemClassLoader();
+    	
+    	Integer i = 1;
+    	while (this.props.getProperty(i.toString()) != null) {
+    		String className = this.props.getProperty(i.toString());
+    		try {
+    			Class theClass = cl.loadClass(className);
+	    		Observer o = (Observer) theClass.newInstance();
+	    		this.addObserver(o);
+    		} catch (java.lang.ClassNotFoundException cnfex) {
+    			this.log.error("notifyInitializer: class not found for: "+className);
+    		} catch (java.lang.InstantiationException initex) {
+    			this.log.error("notifyInitializer: initialization error for: "+className);
+    		} catch (java.lang.IllegalAccessException accex) {
+    			this.log.error("notifyInitializer: illegal access for: "+className);
+    		}
+    		i++;
+    	}
+    	
     }
     
     
