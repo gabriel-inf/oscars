4109dddc46bffa66e5d5de15333cfb7f4358da93
haniotak <haniotak@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2008-03-28 22:38:14 +0000

    Installer shell script v0.1 - Will check environment & prerequisites - Will download, build and deploy Axis2 with Rampart - Will create & initialize databases and MySQL user
    Future work: 
    - Generate ssh keys
    - Customize oscars.properties
    
    
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@6250 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- install.sh
+++ install.sh
@@ -1,76 +1,83 @@
 #!/bin/sh
 
-
 if [ -d src/net/es/oscars ]; then
 	echo "We seem to be in the correct directory";
 else
-	echo "Run this script from its directory. ";
+	echo "Error. Run this script from its directory. ";
 	exit 1;
 fi
 
 INSTALL_HOME=`pwd`;
+IDC_HOSTNAME=`hostname`;
 
-echo "Checking prerequisites.";
+echo "Checking prerequisites.\n";
 
+# Find Ant
 if [ `which ant` ]; then
-	echo "Found ant";
+	echo "  Found ant";
 else
-	echo "Ant not found. Install and add ant to your PATH and rerun installer.";
+	echo "  Ant not found. Install and add ant to your PATH and rerun installer.";
 fi
 
-
+# Find Tomcat
 if [ -n "$CATALINA_HOME" ]; then
-	echo "Environment variable CATALINA_HOME is set to $CATALINA_HOME";
+	echo "  Environment variable CATALINA_HOME is set to $CATALINA_HOME";
 else
-	echo "Environment variable CATALINA_HOME is not set. Download and install Apache Tomcat v. 5.5 and set the CATALINA_HOME environment variable to the install root directory.";
+	echo "  Environment variable CATALINA_HOME is not set. Download and install Apache Tomcat v. 5.5 and set the CATALINA_HOME environment variable to the install root directory.";
 	exit 1;
 fi
 
+# Find environment variables
 DOMAIN_FOUND=0;
 if [ -n "$DOMAIN_HOME" ]; then
-	echo "Environment variable DOMAIN_HOME is set to $DOMAIN_HOME";
+	echo "  Environment variable DOMAIN_HOME is set to $DOMAIN_HOME";
 	DOMAIN_FOUND=1;
 else
-	echo "Environment variable DOMAIN_HOME is not set. Continuing without it.";
+	echo "  Environment variable DOMAIN_HOME is not set. Continuing without it.";
 fi
 
+
+
+# Detect JTA
 if [ -f ./lib/jta.jar ]; then
-	echo "Found jta.jar under lib";
+	echo "  Found jta.jar under lib";
 else
-	echo "jta.jar not found. Download from Sun's website and copy it under ./lib .";
+	echo "  jta.jar not found. Due to license restrictions you must manually download it from Sun's website and copy it under ./lib .";
 	exit 1;
 fi
 
 
-DEPLOYED_AXIS=0;
+# Detect Axis, Rampart
 FOUND_AXIS=0;
+DEPLOYED_AXIS=0;
 if  [ -f $CATALINA_HOME/webapps/axis2/WEB-INF/lib/mail-1.4.jar ]; then
 	FOUND_AXIS="$CATALINA_HOME/webapps/axis2/WEB-INF/lib/";
 	DEPLOYED_AXIS=1;
-	echo "Found axis2 library at $FOUND_AXIS";
+	echo "  Found axis2 library at $FOUND_AXIS";
 elif [ -f lib/axis2/mail-1.4.jar ]; then
 	FOUND_AXIS="lib/axis2";
-	echo "Found axis2 library at $FOUND_AXIS . Axis2 webapp is not deployed.";
+	echo "  Found axis2 library at $FOUND_AXIS . Axis2 webapp is not deployed.";
 else
-	echo "Axis2 library not found.";
+	echo "  Axis2 library not found.";
 fi
 
 FOUND_RAMPART=0;
 if [ -f $FOUND_AXIS/rampart-core-1.3.jar ]; then
 	FOUND_RAMPART="$FOUND_AXIS/rampart-core-1.3.jar";
-	echo "Found rampart library at $FOUND_RAMPART";
+	echo "  Found rampart library at $FOUND_RAMPART";
 else
-	echo "Rampart library not found.";
+	echo "  Rampart library not found.";
 fi
 
 
-# Axis2 build
+# DEBUG STATEMENT
+# FOUND_RAMPART=0;
 
-FOUND_RAMPART=0;
-if [ $FOUND_AXIS == 0 ] || [ $FOUND_RAMPART == 0 ]; then
+# Build Axis2
+if [ $FOUND_AXIS == 0 ] || [ $FOUND_RAMPART == 0 ] || [ $DEPLOYED_AXIS == 0 ]; then
 	READ_BUILDAXIS=0;
 	while [ $READ_BUILDAXIS == 0 ]; do
-		echo "Axis2 library not detected. Should I build it for you y/n?"
+		echo "Axis2 installation not detected. Should I build it for you y/n?"
 		read READ_BUILDAXIS;
 		if [ "$READ_BUILDAXIS" != "y" ] && [ "$READ_BUILDAXIS" != "Y" ] && [ "$READ_BUILDAXIS" != "n" ] && [ "$READ_BUILDAXIS" != "N" ]; then
 			READ_BUILDAXIS=0;
@@ -79,14 +86,40 @@ if [ $FOUND_AXIS == 0 ] || [ $FOUND_RAMPART == 0 ]; then
 
 	BUILD_AXIS=0;
 	if [ "$READ_BUILDAXIS" == "y" ] || [ "$READ_BUILDAXIS" == "Y" ]; then
-		echo "OK, will build Axis2 for you.";
+		echo "  OK, will build Axis2 for you.";
 		BUILD_AXIS=1;
 	else
-		echo "Cannot continue without Axis2 library.";
+		echo "  Cannot continue without Axis2 library. You can download and copy required files into lib/axis2, then run installer again.";
 		exit 1;
 	fi
 fi
 
+
+# DEBUG STATEMENT
+# DEPLOYED_AXIS=0;
+
+# Ask to deploy
+READ_DEPLOYAXIS=0;
+if [ $DEPLOYED_AXIS == 0 ]; then
+	while [ $READ_DEPLOYAXIS == 0 ]; do
+		echo "Axis2 is not deployed. Should I do this for you y/n?"
+		read READ_DEPLOYAXIS;
+		if [ "$READ_DEPLOYAXIS" != "y" ] && [ "$READ_DEPLOYAXIS" != "Y" ] && [ "$READ_DEPLOYAXIS" != "n" ] && [ "$READ_DEPLOYAXIS" != "N" ]; then
+			READ_DEPLOYAXIS=0;
+		fi
+	done
+fi
+
+
+DEPLOY_AXIS=0;
+if [ "$READ_DEPLOYAXIS" == "y" ] || [ "$READ_DEPLOYAXIS" == "Y" ]; then
+	echo "  OK, will deploy Axis2 for you.";
+	DEPLOY_AXIS=1;
+fi
+
+
+
+# Axis2 retrieve & build
 
 if [ $BUILD_AXIS ]; then
 	if [ ! -d dists ]; then
@@ -94,16 +127,23 @@ if [ $BUILD_AXIS ]; then
 	fi
 	if [ ! -d dists/axis2-1.3 ]; then
 		if [ ! -f dists/axis2-1.3-bin.zip ]; then
+			echo "  Downloading Axis2...";
 			`wget -P dists http://www.devlib.org/apache/ws/axis2/1_3/axis2-1.3-bin.zip`;
 		fi
+		echo "  Axis downloaded. Unzipping...";
 		`unzip -qq -d dists dists/axis2-1.3-bin.zip`;
+	else
+		echo "  Axis2 already downloaded and unzipped";
 	fi
 	if [ ! -d dists/rampart-1.3 ]; then
 		if [ ! -f dists/rampart-1.3.zip ]; then
+			echo "  Downloading Rampart...";
 			`wget -P dists http://apache.ziply.com/ws/rampart/1_3/rampart-1.3.zip`;
 		fi
+		echo "  Rampart  downloaded. Unzipping...";
 		`unzip -qq -d dists dists/rampart-1.3.zip`;
 	fi
+	echo "  Building Axis2 with Rampart...";
 	`sed -e 's/CHANGE_THIS/\.\./g' conf/axis2/build.xml > dists/axis2-1.3/webapp/build.xml`;
 	cd dists/axis2-1.3/webapp;
 	ant;
@@ -111,32 +151,40 @@ if [ $BUILD_AXIS ]; then
 fi
 
 
-
-if [ $FOUND_AXIS == 0 ] || [ $FOUND_RAMPART == 0 ]; then
-	READ_INSTALLAXIS=0;
-	if [ ! $DEPLOYED_AXIS ]; then
-		while [ $READ_INSTALLAXIS == 0 ]; do
-			echo "Axis2 is not installed. Should I do this for you y/n?"
-			read READ_AXIS;
-			if [ "$READ_INSTALLAXIS" != "y" ] && [ "$READ_INSTALLAXIS" != "Y" ] && [ "$READ_INSTALLAXIS" != "n" ] && [ "$READ_INSTALLAXIS" != "N" ]; then
-				READ_INSTALLAXIS=0;
-			fi
-		done
+# Axis2 deploy
+if [ $DEPLOY_AXIS != 0 ]; then
+	echo "  Deploying Axis2 webapp...";
+	FOUND_AXIS2_WAR=0;
+	if [ -f dists/axis2-1.3/dist/axis2.war ]; then
+	 	FOUND_AXIS2_WAR="dists/axis2-1.3/dist/axis2.war";
+		echo "  Found $FOUND_AXIS2_WAR";
+	else
+		echo "Input path to axis2.war";
+		read FOUND_AXIS2_WAR;
+		if [ -f $FOUND_AXIS2_WAR ]; then
+			echo "  Found $FOUND_AXIS2_WAR";
+		else
+			echo "  File $FOUND_AXIS2_WAR not found. Exiting.";
+			exit 1;
+		fi
 	fi
 
-
-	INSTALL_AXIS=0;
-	if [ "$READ_INSTALLAXIS" == "y" ] || [ "$READ_INSTALLAXIS" == "Y" ]; then
-		echo "OK, will install Axis2 for you.";
-		INSTALL_AXIS=1;
-	fi
+	echo "  Stopping Tomcat...";
+	$CATALINA_HOME/bin/shutdown.sh;
+	echo "  Copying axis2.war ...";
+	cp $FOUND_AXIS2_WAR $CATALINA_HOME/webapps/;
+	echo "  Restarting Tomcat...";
+	$CATALINA_HOME/bin/startup.sh;
 fi
 
 
-
-
-
 echo "Your kit looks good.";
+	echo "Verify the local hostname for this IDC. Leave blank for \"$IDC_HOSTNAME\"."
+	read READ_HOSTNAME;
+	if [ $READ_HOSTNAME ]; then
+		IDC_HOSTNAME=READ_HOSTNAME;
+	fi
+	echo "  Using $IDC_HOSTNAME . ";
 
 
 
@@ -151,27 +199,140 @@ done
 
 INSTALL_DB=0;
 if [ "$READ_INSTALLDB" == "y" ] || [ "$READ_INSTALLDB" == "Y" ]; then
-	echo "OK, will install databases.";
+	echo "  OK, will install databases.";
 	INSTALL_DB=1;
 fi
 
+if [ $INSTALL_DB == 1 ]; then
+	FOUND_MYSQL=0;
+	if [ `which mysql` ]; then
+		FOUND_MYSQL=`which mysql`;
+		echo "  Found mysql client at $FOUND_MYSQL";
+	else
+		echo "  MySQL client not found. Add mysql to your PATH and rerun installer, or install databases manually.";
+	fi
 
-FOUND_MYSQL=0;
-if [ `which mysql` ]; then
-	FOUND_MYSQL=`which mysql`;
-	echo "Found mysql client at $FOUND_MYSQL";
-else
-	echo "MySQL client not found. Add mysql to your PATH and rerun installer, or install databases manually.";
+
+	MYSQL_PRIV_ACCESS=0;
+	while [ $MYSQL_PRIV_ACCESS == 0 ]; do
+		echo "Input the MySQL server hostname; leave blank for localhost"
+		read MYSQL_SERVER;
+		if [ ! $MYSQL_SERVER ]; then
+			MYSQL_SERVER="localhost";
+		fi
+			echo "  Using $MYSQL_SERVER . ";
+
+		echo "Input a privileged MySQL username on that host. Leave blank for root."
+		read MYSQL_PRIV_USERNAME;
+		if [ ! "$MYSQL_PRIV_USERNAME" ]; then
+			MYSQL_PRIV_USERNAME="root";
+		fi
+		echo "  Using $MYSQL_PRIV_USERNAME . ";
+
+		echo "Input the password for the privileged account"
+		stty -echo;
+		read MYSQL_PRIV_PASSWORD;
+		stty echo;
+
+		`mysql --user=$MYSQL_PRIV_USERNAME --password="$MYSQL_PRIV_PASSWORD" --host=$MYSQL_SERVER --execute="STATUS" > /dev/null 2> /dev/null`;
+		if [ $? != 0 ]; then
+			echo "  Could not connect. Check password and try again. Ctrl-C to exit.";
+		else
+			echo "  Privileged account access verified.";
+			MYSQL_PRIV_ACCESS=1;
+		fi
+	done
+
+	MYSQL_GOT_IDC_CREDENTIALS=0;
+
+	while [ $MYSQL_GOT_IDC_CREDENTIALS == 0 ]; do
+
+		echo "Input a MySQL username the IDC will use to connect. Leave blank for \"oscars\""
+		read MYSQL_IDC_USERNAME;
+		if [ ! "$MYSQL_IDC_USERNAME" ]; then
+			MYSQL_IDC_USERNAME="oscars";
+		fi
+		echo "  Using $MYSQL_IDC_USERNAME . ";
+
+		echo "Input the password for the IDC account"
+		stty -echo;
+		read MYSQL_IDC_PASSWORD;
+		stty echo;
+		`mysql --user=$MYSQL_IDC_USERNAME --password="$MYSQL_IDC_PASSWORD" --host=$MYSQL_SERVER --execute="STATUS" > /dev/null 2> /dev/null`;
+		if [ $? != 0 ]; then
+			echo "  Could not connect with IDC credentials. Will grant IDC account privileges.";
+			echo "Input the password for the IDC account one more time:"
+			stty -echo;
+			read MYSQL_IDC_PASSWORD2;
+			stty echo;
+			if [ "$MYSQL_IDC_PASSWORD2" == "$MYSQL_IDC_PASSWORD" ]; then
+				MYSQL_GOT_IDC_CREDENTIALS=1;
+				echo "  Passwords match.";
+			else
+				echo "  Passwords do not match. Starting over.";
+			fi
+			MYSQL_IDC_HAS_ACCESS=0;
+		else
+			echo "  IDC account access verified.";
+			MYSQL_IDC_HAS_ACCESS=1;
+			MYSQL_GOT_IDC_CREDENTIALS=1;
+		fi
+	done
+
+	echo "Input the BSS database name. Leave blank for \"bss\"."
+	read MYSQL_BSS_DBNAME;
+	if [ ! $MYSQL_BSS_DBNAME ]; then			echo "  Using oscars";
+
+		MYSQL_BSS_DBNAME="bss";
+	fi
+	echo "  Using $MYSQL_BSS_DBNAME . ";
+
+	echo "Input the AAA database name. Leave blank for \"aaa\"."
+	read MYSQL_AAA_DBNAME;
+	if [ ! $MYSQL_AAA_DBNAME ]; then
+		MYSQL_AAA_DBNAME="aaa";
+	fi
+	echo "  Using $MYSQL_AAA_DBNAME . ";
+
+
+
+	echo "  Creating databases...";
+	`mysql --user=$MYSQL_PRIV_USERNAME --password="$MYSQL_PRIV_PASSWORD" --host=$MYSQL_SERVER --execute="CREATE DATABASE $MYSQL_BSS_DBNAME" > /dev/null 2> /dev/null`;
+	`mysql --user=$MYSQL_PRIV_USERNAME --password="$MYSQL_PRIV_PASSWORD" --host=$MYSQL_SERVER --execute="CREATE DATABASE $MYSQL_AAA_DBNAME" > /dev/null 2> /dev/null`;
+	echo "  Databases created...";
+	echo "  Initializing databases...";
+	`mysql --user=$MYSQL_PRIV_USERNAME --password="$MYSQL_PRIV_PASSWORD" --host=$MYSQL_SERVER -D$MYSQL_BSS_DBNAME < sql/bss/createTables.sql`;
+	`mysql --user=$MYSQL_PRIV_USERNAME --password="$MYSQL_PRIV_PASSWORD" --host=$MYSQL_SERVER -D$MYSQL_AAA_DBNAME < sql/aaa/createTables.sql`;
+	echo "  Databases initialized...";
+
+	echo "  Granting privileges to IDC account...";
+	if [ "$MYSQL_SERVER" == "localhost" ]; then
+		MYSQL_IDC_HOSTNAME="localhost";
+	else
+		MYSQL_IDC_HOSTNAME=IDC_HOSTNAME;
+	fi
+	`mysql --user=$MYSQL_PRIV_USERNAME --password="$MYSQL_PRIV_PASSWORD" --host=$MYSQL_SERVER --execute="GRANT ALL PRIVILEGES ON $MYSQL_BSS_DBNAME.* TO '$MYSQL_IDC_USERNAME'@'$MYSQL_IDC_HOSTNAME' IDENTIFIED BY '$MYSQL_IDC_PASSWORD'"`;
+	`mysql --user=$MYSQL_PRIV_USERNAME --password="$MYSQL_PRIV_PASSWORD" --host=$MYSQL_SERVER --execute="GRANT ALL PRIVILEGES ON $MYSQL_AAA_DBNAME.* TO '$MYSQL_IDC_USERNAME'@'$MYSQL_IDC_HOSTNAME' IDENTIFIED BY '$MYSQL_IDC_PASSWORD'"`;
+	echo "  IDC account authorized.";
+
 fi
 
-echo "Database setup."
+#TODO: customize oscars.properties with MySQL data, generate ssh keys, etc
 
 
+echo "Cleaning before compile...";
+ant clean;
+echo "Compiling...";
+ant compile;
+
+#
+# echo "  Stopping Tomcat...";
+# $CATALINA_HOME/bin/shutdown.sh;
+# echo "Deploying IDC...";
+# ant deployall;
+# echo "  Restarting Tomcat...";
+# $CATALINA_HOME/bin/startup.sh;
 
 
-
-
-
-
-echo "OK";
+echo "IDC installed.";
 exit 0;
