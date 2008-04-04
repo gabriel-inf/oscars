2cae17a49e20e239965d4f0460574905ccec2557
haniotak <haniotak@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2008-04-04 22:46:33 +0000

    Added more args to ListReservationsCLI Interim commit for listReservations (TOP SECRET SURPRISE VISUALIZATION PROJECT)
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@6275 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- examples/javaClients/ListReservationCLI.java
+++ examples/javaClients/ListReservationCLI.java
@@ -5,6 +5,8 @@ import org.ogf.schema.network.topology.ctrlplane._20070626.CtrlPlaneHopContent;
 import org.ogf.schema.network.topology.ctrlplane._20070626.CtrlPlanePathContent;
 
 import net.es.oscars.client.Client;
+import net.es.oscars.lookup.LookupException;
+import net.es.oscars.lookup.PSLookupClient;
 import net.es.oscars.oscars.AAAFaultMessage;
 import net.es.oscars.wsdlTypes.Layer2Info;
 import net.es.oscars.wsdlTypes.Layer3Info;
@@ -14,144 +16,179 @@ import net.es.oscars.wsdlTypes.MplsInfo;
 import net.es.oscars.wsdlTypes.PathInfo;
 import net.es.oscars.wsdlTypes.ResDetails;
 
-
 public class ListReservationCLI {
-	private String url;
-	private String repo;
-	private String status;
-	private int numResults;
-	
-	public ListRequest readArgs(String[] args){
-		/* Set request parameters */
-		this.status = null;
-		this.numResults = 10;
-		try{
-	        for(int i = 0; i < args.length; i++){
-	        	if(args[i].equals("-url")){
-	        		this.url = args[i+1];
-	        	}else if(args[i].equals("-repo")){
-	        		this.repo = args[i+1];
-	        	}else if(args[i].equals("-status")){
-	        		this.status = args[i+1];
-	        	}else if(args[i].equals("-numresults")){
-	        	    int n = Integer.parseInt(args[i+1]);
-	        		this.numResults = n;
-	        	}else if(args[i].equals("-help")){
-	        		this.printHelp();
-	        		System.exit(0);
-	        	}
-	        }
-		} catch(Exception e){
-			System.out.println("Error: " + e.getMessage());
-			this.printHelp();
-		}
-		
-		if(this.url==null || this.repo==null){
-			this.printHelp();
-			System.exit(0);
-		}
-		ListRequest listReq = new ListRequest();
-		if (this.status != null) {
-			listReq.addResStatus(this.status);
-		}
-		
-		listReq.setResRequested(this.numResults);
-		
-		return listReq;
-	}
-	
-	public String getUrl(){
-		return url;
-	}
-	
-	public String getRepo(){
-		return repo;
-	}
-	
-	public void printResDetails(ResDetails response){
-		 PathInfo pathInfo = response.getPathInfo();
+    private String url;
+    private String repo;
+    private String status = null;
+    private String description = null;
+    private String endpoint = null;
+    private int numResults = 10;;
+
+    public ListRequest readArgs(String[] args){
+        /* Set request parameters */
+        try{
+            for(int i = 0; i < args.length; i++){
+                if(args[i].equals("-url")){
+                    this.url = args[i+1].trim();
+                }else if(args[i].equals("-repo")){
+                    this.repo = args[i+1].trim();
+                }else if(args[i].equals("-status")){
+                    this.status = args[i+1].trim();
+                }else if(args[i].equals("-desc")){
+                    this.description = args[i+1].trim();
+                }else if(args[i].equals("-endpoint")){
+                    this.endpoint = args[i+1].trim();
+                }else if(args[i].equals("-numresults")){
+                    int n = Integer.parseInt(args[i+1].trim());
+                    this.numResults = n;
+                }else if(args[i].equals("-help")){
+                    this.printHelp();
+                    System.exit(0);
+                }
+            }
+        } catch(Exception e){
+            System.out.println("Error: " + e.getMessage());
+            this.printHelp();
+        }
+
+        if(this.url==null || this.repo==null){
+            this.printHelp();
+            System.exit(0);
+        }
+
+        ListRequest listReq = new ListRequest();
+
+        String[] statuses;
+        if (this.status != null) {
+            statuses = this.status.trim().split(",");
+            for (String status: statuses) {
+                status = status.trim();
+                if (!status.equals("")) {
+                    listReq.addResStatus(status);
+                }
+            }
+        }
+
+        if (this.description != null && !this.description.equals("")) {
+            listReq.setDescription(this.description);
+        }
+
+        String[] links;
+        if (this.endpoint != null) {
+            links = this.endpoint.trim().split(",");
+            for (String link: links) {
+                link = link.trim();
+                if (!link.equals("")) {
+                    listReq.addLinkId(link);
+                }
+            }
+        }
+
+
+
+
+        listReq.setResRequested(this.numResults);
+
+        return listReq;
+    }
+
+    public String getUrl(){
+        return url;
+    }
+
+    public String getRepo(){
+        return repo;
+    }
+
+    public void printResDetails(ResDetails response){
+         PathInfo pathInfo = response.getPathInfo();
          CtrlPlanePathContent path = pathInfo.getPath();
          Layer2Info layer2Info = pathInfo.getLayer2Info();
          Layer3Info layer3Info = pathInfo.getLayer3Info();
          MplsInfo mplsInfo = pathInfo.getMplsInfo();
-                  
-         /* Print response information */ 
-         System.out.println("GRI: " + response.getGlobalReservationId()); 
-         System.out.println("Login: " + response.getLogin()); 
-         System.out.println("Status: " + response.getStatus()); 
-         System.out.println("Start Time: " + response.getStartTime()); 
-         System.out.println("End Time: " + response.getEndTime()); 
+         /* Print response information */
+         System.out.println("GRI: " + response.getGlobalReservationId());
+         System.out.println("Login: " + response.getLogin());
+         System.out.println("Status: " + response.getStatus());
+         System.out.println("Start Time: " + response.getStartTime());
+         System.out.println("End Time: " + response.getEndTime());
          System.out.println("Time of request: " + response.getCreateTime());
          System.out.println("Bandwidth: " + response.getBandwidth());
          System.out.println("Description: " + response.getDescription());
          System.out.println("Path Setup Mode: " + pathInfo.getPathSetupMode());
          if(layer2Info != null){
-         	System.out.println("Source Endpoint: " + layer2Info.getSrcEndpoint());
-         	System.out.println("Destination Endpoint: " + layer2Info.getDestEndpoint());
-         	System.out.println("Source VLAN: " + layer2Info.getSrcVtag());
-         	System.out.println("Destination VLAN: " + layer2Info.getDestVtag());
+             System.out.println("Source Endpoint: " + layer2Info.getSrcEndpoint());
+             System.out.println("Destination Endpoint: " + layer2Info.getDestEndpoint());
+             System.out.println("Source VLAN: " + layer2Info.getSrcVtag());
+             System.out.println("Destination VLAN: " + layer2Info.getDestVtag());
          }
          if(layer3Info != null){
-         	System.out.println("Source Host: " + layer3Info.getSrcHost());
-         	System.out.println("Destination Host: " + layer3Info.getDestHost());
-         	System.out.println("Source L4 Port: " + layer3Info.getSrcIpPort());
-         	System.out.println("Destination L4 Port: " + layer3Info.getDestIpPort());
-         	System.out.println("Protocol: " + layer3Info.getProtocol());
-         	System.out.println("DSCP: " + layer3Info.getDscp());
+             System.out.println("Source Host: " + layer3Info.getSrcHost());
+             System.out.println("Destination Host: " + layer3Info.getDestHost());
+             System.out.println("Source L4 Port: " + layer3Info.getSrcIpPort());
+             System.out.println("Destination L4 Port: " + layer3Info.getDestIpPort());
+             System.out.println("Protocol: " + layer3Info.getProtocol());
+             System.out.println("DSCP: " + layer3Info.getDscp());
          }
          if(mplsInfo != null){
-         	System.out.println("Burst Limit: " + mplsInfo.getBurstLimit());
-         	System.out.println("LSP Class: " + mplsInfo.getLspClass());
+             System.out.println("Burst Limit: " + mplsInfo.getBurstLimit());
+             System.out.println("LSP Class: " + mplsInfo.getLspClass());
          }
          System.out.println("Path: ");
          for (CtrlPlaneHopContent hop : path.getHop()){
-         	System.out.println("\t" + hop.getLinkIdRef());
+             System.out.println("\t" + hop.getLinkIdRef());
          }
-	}
-	public void printHelp(){
-		 System.out.println("General Parameters:");
-		 System.out.println("\t-help\t displays this message.");
-		 System.out.println("\t-url\t required. the url of the IDC.");
-		 System.out.println("\t-repo\t required. the location of the repo directory");
-		 System.out.println("\t-status STATUS\t optional. indicates that only reservations of a certain STATUS should be retrieved");
-	}
-	
-	public static void main(String[] args){ 
-        /* Initialize Values */ 
-	 	ListReservationCLI cli = new ListReservationCLI();
-        Client oscarsClient = new Client(); 
+    }
+    public void printHelp(){
+         System.out.println("General Parameters:");
+         System.out.println("\t-help\t displays this message.");
+         System.out.println("\t-url\t required. the url of the IDC.");
+         System.out.println("\t-repo\t required. the location of the repo directory");
+         System.out.println("\t[-status STATUS]\t retrieve reservations with status matching the argument. Use commas to separate values. \n\t\tExample: -status \"ACTIVE,PENDING\"");
+         System.out.println("\t[-desc DESCRIPTION]\t . retrieve reservations with description matching the argument. \n\t\tExample: -desc \"PRODUCTION\"");
+         System.out.println("\t[-endpoint ENDPOINT]\t . retrieve reservations affecting this endpoint. Use commas to separate values. \n\t\tExample: -endpoint \"lambdastation.unl.edu,lambdastation.caltech.edu\"");
+    }
+
+    public static void main(String[] args){
+        /* Initialize Values */
+         ListReservationCLI cli = new ListReservationCLI();
+        Client oscarsClient = new Client();
         ListRequest listReq = cli.readArgs(args);
-        String url = cli.getUrl(); 
+        String url = cli.getUrl();
         String repo = cli.getRepo();
 
-        /* Initialize client instance */ 
+        // to be used if / when we have reverse lookup
+        String lookupURL = "http://packrat.internet2.edu:8009/perfSONAR_PS/services/LS";
+        PSLookupClient lookupClient = new PSLookupClient();
+        lookupClient.setFname("repo/perfSONAR-LSQuery.xml");
+        lookupClient.setUrl(lookupURL);
+
+        /* Initialize client instance */
         try {
-			oscarsClient.setUp(true, url, repo);
-			
-			/* Send Request */ 
+            oscarsClient.setUp(true, url, repo);
+
+            /* Send Request */
             ListReply response = oscarsClient.listReservations(listReq);
             ResDetails[] details = response.getResDetails();
             int numResults = response.getTotalResults();
-            
+
             for(int i = 0; details != null && i < details.length; i++){
-            	cli.printResDetails(details[i]);
+                cli.printResDetails(details[i]);
             }
-            
+
             System.out.println(numResults + " reservations match request.");
-		} catch (AxisFault e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (RemoteException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (AAAFaultMessage e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
-             
-            
+        } catch (AxisFault e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (RemoteException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (AAAFaultMessage e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
    }
 }
--- examples/javaClients/ListReservationsClient.java
+++ examples/javaClients/ListReservationsClient.java
@@ -4,12 +4,19 @@ import java.io.InputStreamReader;
 import java.rmi.RemoteException;
 import java.util.*;
 
+import org.ogf.schema.network.topology.ctrlplane._20070626.CtrlPlaneHopContent;
+import org.ogf.schema.network.topology.ctrlplane._20070626.CtrlPlanePathContent;
+
 import net.es.oscars.oscars.AAAFaultMessage;
 import net.es.oscars.oscars.BSSFaultMessage;
 import net.es.oscars.wsdlTypes.*;
 import net.es.oscars.client.Client;
+import net.es.oscars.bss.topology.URNParser;
 
 public class ListReservationsClient extends ExampleClient {
+
+    private boolean createDot = false;
+
     /**
      * @param args
      *            [0] directory name of the client repository contains
@@ -46,20 +53,22 @@ public class ListReservationsClient extends ExampleClient {
         // make the call to the server
         ListReply response = this.getClient().listReservations(listReq);
         this.outputResponse(response);
+//            this.createDOT(response);
         return response;
     }
-    
+
     public ListRequest readParams(boolean isInteractive) {
         ListRequest listReq = new ListRequest();
 
         // Prompt for input parameters specific to query
         try {
-        	String linkId = "";
-        	String[] statuses;
-        	String strResults = "";
-        	String strOffset = "";
-        	String description = "";
-        	
+            String linkId = "";
+            String[] statuses;
+            String strResults = "";
+            String strOffset = "";
+            String description = "";
+            String strCreateDot = "";
+
             if (isInteractive) {
                 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
                 System.out.print("Statuses (comma separated, blank for all): ");
@@ -73,36 +82,43 @@ public class ListReservationsClient extends ExampleClient {
                 strResults = br.readLine().trim();
                 System.out.print("Offset (default 0): ");
                 strOffset = br.readLine().trim();
+                /*
+                System.out.print("Create DOT file y/n (default n): ");
+                strCreateDot = br.readLine().trim();
+                if (strCreateDot.equals("y") || strCreateDot.equals("Y")) {
+                    this.createDot = true;
+                }
+                */
             } else {
-            	String statusesInput = this.getProperties().getProperty("statuses").trim();
+                String statusesInput = this.getProperties().getProperty("statuses").trim();
                 statuses = statusesInput.split(",");
                 linkId = this.getProperties().getProperty("linkId");
             }
 
             Integer resRequested = 10;
             Integer resOffset = 0;
-            
+
             try {
-            	resRequested = Integer.parseInt(strResults);
+                resRequested = Integer.parseInt(strResults);
             } catch (Exception ex) { }
-            
+
             try {
-            	resOffset = Integer.parseInt(strOffset);
+                resOffset = Integer.parseInt(strOffset);
             } catch (Exception ex) { }
-            
+
             listReq.setResRequested(resRequested);
             listReq.setResOffset(resOffset);
-            
+
             for (String status: statuses) {
-            	listReq.addResStatus(status.trim());
-            }                
+                listReq.addResStatus(status.trim());
+            }
             if (!linkId.equals("")) {
-            	listReq.addLinkId(linkId.trim());
+                listReq.addLinkId(linkId.trim());
             }
             if (!description.equals("")) {
-            	listReq.setDescription(description);
+                listReq.setDescription(description);
             }
-            
+
         } catch (IOException ioe) {
             System.out.println("IO error reading query input");
             System.exit(1);
@@ -137,4 +153,202 @@ public class ListReservationsClient extends ExampleClient {
             System.out.println("no reservations were found");
         }
     }
+
+
+
+    public void createDOT(ListReply response) {
+        String[] colors = new String[10];
+        colors[0] = "red";
+        colors[1] = "blue";
+        colors[2] = "green";
+        colors[3] = "yellow";
+        colors[4] = "black";
+        colors[5] = "purple";
+        colors[6] = "pink";
+        colors[7] = "brown";
+        colors[8] = "orange";
+        colors[9] = "violet";
+        String color;
+
+        ResDetails[] resList;
+        if ((response != null) && (resList = response.getResDetails()) != null) {
+
+            Hashtable<String, Hashtable<String, ArrayList<String>>> nodePorts = new Hashtable<String, Hashtable<String, ArrayList<String>>>();
+            Hashtable<String, ArrayList<String>> portLinks;
+            ArrayList<String> links;
+            ArrayList<ArrayList<String>> coloredHops = new ArrayList<ArrayList<String>>();
+            String[] gris = new String[100];
+
+            String output = "\n\n\ndigraph reservations {\n";
+            String sources = "";
+            String dests = "";
+
+            for (int i = 0; i < resList.length; i++) {
+                ResDetails resv = resList[i];
+                color = colors[i];
+
+                String gri = resv.getGlobalReservationId();
+                gris[i] = gri;
+
+                String resvDescription = resv.getDescription().trim();
+                resvDescription = resvDescription.replaceAll("\\[PRODUCTION\\]", "");
+                resvDescription = resvDescription.replaceAll("\\[PRODUCTION CIRCUIT\\]", "");
+
+                sources += "\""+gri+"-start\" [label=\""+gri+"\\n"+resvDescription+"\"];\n";
+                dests += "\""+gri+"-end\" [label=\""+gri+"\\n"+resvDescription+"\"];\n";
+
+                int bandwidth = resv.getBandwidth();
+                PathInfo pathInfo = resList[i].getPathInfo();
+                Layer2Info layer2Info = pathInfo.getLayer2Info();
+                if (layer2Info != null) {
+                    CtrlPlanePathContent path = pathInfo.getPath();
+                    CtrlPlaneHopContent[] hops = path.getHop();
+                    ArrayList<String> theseHops = new ArrayList<String>();
+
+                    for (int j = 0; j < hops.length; j++) {
+
+                        CtrlPlaneHopContent hop = hops[j];
+                        String topoId = "urn:ogf:network:"+hop.getId();
+
+                        Hashtable<String, String> parsed = URNParser.parseTopoIdent(topoId);
+                        String domainId = parsed.get("domainId");
+                        String nodeId   = parsed.get("nodeId");
+                        String portId   = parsed.get("portId");
+                        String linkId   = parsed.get("linkId");
+
+
+                        if (linkId.equals("*")) {
+                            portId = portId+":"+linkId;
+                        } else {
+                            portId = linkId;
+                        }
+
+                        if (nodePorts.get(nodeId) != null) {
+                            portLinks = (Hashtable<String, ArrayList<String>>) nodePorts.get(nodeId);
+                        } else {
+                            portLinks = new Hashtable<String, ArrayList<String>>();
+                            nodePorts.put(nodeId, portLinks);
+                        }
+
+                        if (portLinks.containsKey(portId)) {
+                            links = portLinks.get(portId);
+                        } else {
+                            links = new ArrayList<String>();
+                            portLinks.put(portId, links);
+                        }
+//                        output += "\""+nodeId+":"+portId+"\";\n";
+                        theseHops.add("\""+nodeId+":"+portId+"\"");
+
+                    }
+                    coloredHops.add(theseHops);
+                }
+            } // end for loop
+
+            output += "{\n";
+            output += "rank=source;\n";
+            output += sources;
+            output += "}\n";
+
+            output += "{\n";
+            output += "rank=sink;\n";
+            output += dests;
+            output += "}\n";
+
+            // place ports of the same node in their own cluster
+            Iterator nodeIt = nodePorts.keySet().iterator();
+            while (nodeIt.hasNext()) {
+                String nodeId = (String) nodeIt.next();
+                portLinks = nodePorts.get(nodeId);
+//                output += "\""+nodeId+"\" [shape=record,label=\"" + nodeId + " | { ";
+
+                Iterator portIt = portLinks.keySet().iterator();
+                output += "subgraph \"cluster_"+nodeId+"\" {\n";
+                output += " label=\""+nodeId+"\";\n";
+                while (portIt.hasNext()) {
+
+                    String portId = (String) portIt.next();
+                    output += "\""+nodeId+":"+portId+"\" [label=\""+portId+"\"];\n";
+//                    output += "<"+portId+"> "+portId;
+                    if (portIt.hasNext()) {
+//                        output += " | ";
+                    }
+                }
+                output += "}\n";
+//s                output += "} \"];\n";
+            }
+
+            // make labels for inter-node hops
+            int i = 0;
+            Hashtable<String, Integer> edgeBWs = new Hashtable<String, Integer>();
+            for (ArrayList<String> theHops : coloredHops) {
+                ResDetails resv = resList[i];
+                String gri = resv.getGlobalReservationId();
+                int bandwidth = resv.getBandwidth();
+                i++;
+                for (int j = 0; j < theHops.size() - 1; j++) {
+                    String hop = theHops.get(j);
+                    String nextHop = theHops.get(j+1);
+                    String edge = hop+":"+nextHop;
+                    if (j == 0 || j % 2 > 0 ) {
+                        int edgeBW = 0;
+                        if (edgeBWs.containsKey(edge)) {
+                            edgeBW = edgeBWs.get(edge);
+                        }
+                        edgeBW += bandwidth;
+                        edgeBWs.put(edge, edgeBW);
+
+//                        output += hop+" -> "+nextHop+" [color="+color+",dir=both, style=bold];\n";
+//                    } else {
+//                        output += hop+" -> "+nextHop+" [color="+color+",dir=both,style=bold];\n";
+                    }
+                }
+            }
+
+            // output edges
+            i = 0;
+            for (ArrayList<String> theHops : coloredHops) {
+                ResDetails resv = resList[i];
+                String gri = resv.getGlobalReservationId();
+                int bandwidth = resv.getBandwidth();
+                color = colors[i];
+                i++;
+                for (int j = 0; j < theHops.size() - 1; j++) {
+                    String hop = theHops.get(j);
+                    String nextHop = theHops.get(j+1);
+                    String edge = hop+":"+nextHop;
+                    String reverseEdge = nextHop+":"+hop;
+                    int edgeBW = 0;
+                    String label="";
+                    if (edgeBWs.containsKey(edge)) {
+                        edgeBW = edgeBWs.get(edge);
+                        label = "total:\\n"+edgeBW+" Mbps";
+                        edgeBWs.remove(edge);
+                        edgeBWs.remove(reverseEdge);
+                    } else if (edgeBWs.containsKey(reverseEdge)) {
+                        edgeBW = edgeBWs.get(reverseEdge);
+                        label = "total:\\n"+edgeBW+" Mbps";
+                        edgeBWs.remove(edge);
+                        edgeBWs.remove(reverseEdge);
+                    }
+
+                    if (j == 0) {
+                        label = bandwidth+" Mbps";
+                        output += "\""+gri+"-start\" -> "+hop+" [color="+color+",dir=both, style=bold, weight=5, group="+color+", label=\""+label+"\", minlen=2];\n";
+                    }
+                    if (j % 2 > 0 ) {
+                        output += hop+" -> "+nextHop+" [color="+color+",dir=both, style=bold, weight=5, group="+color+", label=\""+label+"\", minlen=3];\n";
+                    } else {
+                        output += hop+" -> "+nextHop+" [color="+color+",dir=both, group="+color+"];\n";
+                    }
+                    if (j == theHops.size() -2) {
+                        output += nextHop +" -> \""+gri+"-end\" [color="+color+",dir=both, style=bold, weight=5, group="+color+", minlen=2s];\n";
+                    }
+                }
+            }
+            output += "}\n";
+
+            System.out.println(output);
+        }
+    }
+
 }
