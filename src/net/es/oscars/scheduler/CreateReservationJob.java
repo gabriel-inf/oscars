1b803e76050a9a1507c19a4f4f8a4f622f18ccdb
haniotak <haniotak@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2008-07-10 22:22:44 +0000

    Added asynchronous reservation creation job
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@6690 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- src/net/es/oscars/scheduler/CreateReservationJob.java
+++ src/net/es/oscars/scheduler/CreateReservationJob.java
@@ -1,8 +1,12 @@
 package net.es.oscars.scheduler;
 import org.apache.log4j.Logger;
+import org.hibernate.Session;
 import org.quartz.*;
 import net.es.oscars.bss.*;
-import net.es.oscars.oscars.OSCARSCore;
+import net.es.oscars.wsdlTypes.*;
+import net.es.oscars.interdomain.*;
+import net.es.oscars.notify.*;
+import net.es.oscars.oscars.*;
 import net.es.oscars.pss.PSSException;
 
 public class CreateReservationJob extends ChainingJob implements org.quartz.Job {
@@ -14,22 +18,103 @@ public class CreateReservationJob extends ChainingJob implements org.quartz.Job 
         String jobName = context.getJobDetail().getFullName();
         this.log = Logger.getLogger(this.getClass());
         this.log.debug("CreateReservationJob.start name:"+jobName);
-
         this.core = OSCARSCore.getInstance();
+        
+        Forwarder forwarder = core.getForwarder();
+        ReservationManager rm = core.getReservationManager();
+        EventProducer eventProducer = new EventProducer();
+        
         JobDataMap dataMap = context.getJobDetail().getJobDataMap();
         Reservation resv = (Reservation) dataMap.get("reservation");
+        PathInfo pathInfo = (PathInfo) dataMap.get("pathInfo");
+        String login = (String) dataMap.get("login");
 
         String gri;
         if (resv != null) {
             gri = (String) resv.getGlobalReservationId();
         } else {
             this.log.error("No reservation associated with job name:"+jobName);
+            super.execute(context);
             return;
         }
         this.log.debug("GRI is: "+gri+"for job name: "+jobName);
+        
+        try {
+            StateEngine.canUpdateStatus(resv, StateEngine.RESERVED);
+        } catch (BSSException ex) {
+            this.log.error(ex);
+            super.execute(context);
+            return;
+        }
+        
+        Session bss = core.getBssSession();
+        bss.beginTransaction();
+        boolean wasReserved = true;
+        
+        String errMessage = null;
+        
+        try {
+
+            rm.create(resv, login, pathInfo);
+            
+            TypeConverter tc = core.getTypeConverter();
+            tc.ensureLocalIds(pathInfo);
+            
+            // FIXME: why does this sometimes get unset?
+            pathInfo.getPath().setId("unimplemented");
+            
+            // checks whether next domain should be contacted, forwards to
+            // the next domain if necessary, and handles the response
+            
+            
+            // **** IMPORTANT ****
+            // FIXME: this bit right here makes CreateReservationJobs kinda slow
+            CreateReply forwardReply = forwarder.create(resv, pathInfo);
+            rm.finalizeResv(forwardReply, resv, pathInfo);
+            rm.store(resv);
+            eventProducer.addEvent(OSCARSEvent.RESV_CREATE_COMPLETED, login, "JOB", resv);
+        } catch (BSSException ex) {
+            this.log.error(ex);
+            errMessage = ex.getMessage();
+            wasReserved = false;
+        } catch (InterdomainException ex) {
+            this.log.error(ex);
+            errMessage = ex.getMessage();
+            wasReserved = false;
+        } catch (Exception ex) {
+            this.log.error(ex);
+            errMessage = ex.getMessage();
+            wasReserved = false;
+        } finally {
+            forwarder.cleanUp();
+            if (errMessage != null) {
+                eventProducer.addEvent(OSCARSEvent.RESV_CREATE_FAILED, login, "JOB", resv, "", errMessage);
+                this.log.debug("createReservation failed: " + errMessage);
+            }
+        }
+        
+        
+        
+        
+        String status;
+        StateEngine stateEngine = new StateEngine();
+        try {
+            status = StateEngine.getStatus(resv);
+            this.log.debug("Reservation status was: "+status);
+            if (wasReserved) {
+                status = stateEngine.updateStatus(resv, StateEngine.RESERVED);
+            } else {
+                status = stateEngine.updateStatus(resv, StateEngine.FAILED);
+            }
+            this.log.debug("Reservation status now is: "+status);
+        } catch (BSSException ex) {
+            this.log.error("State engine error", ex);
+        }
+
+
+        bss.getTransaction().commit();
+        
         super.execute(context);
-
-
 
         this.log.debug("CreateReservationJob.end name:"+jobName);
     }
