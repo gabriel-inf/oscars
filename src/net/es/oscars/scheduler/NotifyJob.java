2b2562c55bfa170a7ccef730185c7f7403e476df
haniotak <haniotak@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2008-07-14 20:23:32 +0000

    Added BSS session init to NotifyJob thread it's necessary if you're going to do Reservation.toString()
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@6709 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- src/net/es/oscars/scheduler/NotifyJob.java
+++ src/net/es/oscars/scheduler/NotifyJob.java
@@ -1,6 +1,7 @@
 package net.es.oscars.scheduler;
 
 import org.apache.log4j.Logger;
+import org.hibernate.Session;
 import org.quartz.*;
 
 import net.es.oscars.oscars.OSCARSCore;
@@ -9,18 +10,25 @@ import net.es.oscars.notify.*;
 public class NotifyJob implements Job{
     private Logger log;
     private OSCARSCore core;
-    
+
     public void execute(JobExecutionContext context) throws JobExecutionException {
         this.log = Logger.getLogger(this.getClass());
         String jobName = context.getJobDetail().getFullName();
         this.log.info("NotifyJob.start name:"+jobName);
-        
+
         this.core = OSCARSCore.getInstance();
+
+        // need this to get info for Reservation objects
+        Session bss = core.getBssSession();
+        bss.beginTransaction();
         JobDataMap dataMap = context.getJobDetail().getJobDataMap();
         NotifierSource observable = this.core.getNotifier().getSource();
         Object event = (Object) dataMap.get("event");
 
         observable.eventOccured(event);
+        bss.getTransaction().commit();
+
+
         this.log.info("NotifyJob.end name:"+jobName);
     }
 }
