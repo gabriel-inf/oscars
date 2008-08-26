7e26cccf60dfad26a1d70a7f4850c03cc4ae235a
andy <andy@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2008-08-26 00:42:38 +0000

    Added code to timeout path setup if no notifications are received. Also added code to internally retry confimations every few seconds to compensate for clocks that may be out of sync
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@7035 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- src/net/es/oscars/pss/PathSetupManager.java
+++ src/net/es/oscars/pss/PathSetupManager.java
@@ -1,6 +1,7 @@
 package net.es.oscars.pss;
 
 import java.util.*;
+import org.quartz.*;
 import org.apache.log4j.*;
 import net.es.oscars.PropHandler;
 import net.es.oscars.bss.*;
@@ -8,6 +9,8 @@ import net.es.oscars.bss.topology.*;
 import net.es.oscars.interdomain.*;
 import net.es.oscars.wsdlTypes.*;
 import net.es.oscars.notify.*;
+import net.es.oscars.scheduler.PathTimeoutJob;
+
 import net.es.oscars.oscars.OSCARSCore;
 
 /**
@@ -25,6 +28,9 @@ public class PathSetupManager{
     private ReservationLogger rsvLogger;
     private OSCARSCore core;
     
+    private long SETUP_CONFIRM_TIMEOUT = 600;//10min
+    private long TEARDOWN_CONFIRM_TIMEOUT = 600;//10min
+    
     /** Constructor. */
     public PathSetupManager(String dbname) {
         PropHandler propHandler = new PropHandler("oscars.properties");
@@ -34,8 +40,44 @@ public class PathSetupManager{
         this.pss = pssFactory.createPSS(this.props.getProperty("method"), dbname);
         this.log = Logger.getLogger(this.getClass());
         this.rsvLogger = new ReservationLogger(this.log);
-//        this.log.info("PSS type is:["+this.props.getProperty("method")+"]");
-        this.dbname = dbname;
+        this.dbname = dbname;      
+        //Init timeout globals
+        Properties timeProps = propHandler.getPropertyGroup("timeout", true);
+        String defaultTimeoutStr = timeProps.getProperty("default");
+        String createTimeoutStr = timeProps.getProperty("createPath.confirm");
+        String teardownTimeoutStr = timeProps.getProperty("teardownPath.confirm");
+        
+        long defaultTimeout = 0;
+        if(defaultTimeoutStr != null){
+            try{
+                defaultTimeout = Long.parseLong(defaultTimeoutStr);
+            }catch(Exception e){
+                this.log.error("Default timeout.default property invalid. " +
+                               "Defaulting to another value for timeout.");
+            }
+        }
+        
+        if(createTimeoutStr != null){
+            try{
+                SETUP_CONFIRM_TIMEOUT = Long.parseLong(createTimeoutStr);
+            }catch(Exception e){
+                this.log.error("timeout.createPath.confirm property invalid." +
+                               "Defaulting to another value for timeout.");
+            }
+        }else if(defaultTimeout > 0){
+            SETUP_CONFIRM_TIMEOUT = defaultTimeout;
+        }
+        
+        if(teardownTimeoutStr != null){
+            try{
+                TEARDOWN_CONFIRM_TIMEOUT = Long.parseLong(teardownTimeoutStr);
+            }catch(Exception e){
+                this.log.error("timeout.teardownPath.confirm property invalid." +
+                               "Defaulting to another value for timeout.");
+            }
+        }else if(defaultTimeout > 0){
+            TEARDOWN_CONFIRM_TIMEOUT = defaultTimeout;
+        }
     }
 
     /**
@@ -61,8 +103,6 @@ public class PathSetupManager{
             throw new PSSException("Path setup not currently supported");
         }
         
-        
-        
         /* Create path */
         try{
             se.updateStatus(resv, StateEngine.INSETUP);
@@ -71,7 +111,7 @@ public class PathSetupManager{
             if(nextDomain == null){
                 this.updateCreateStatus(2, resv);
             }else{
-                //schedule time out
+                this.scheduleStatusCheck(SETUP_CONFIRM_TIMEOUT, resv, "setup", false);
             }
             
             /* Get previous domain */
@@ -83,7 +123,7 @@ public class PathSetupManager{
             if(firstDomain == null || firstDomain.isLocal()){
                 this.updateCreateStatus(4, resv);
             }else{
-                //schedule timeout
+                this.scheduleStatusCheck(SETUP_CONFIRM_TIMEOUT, resv, "setup", true);
             }
             status = this.pss.createPath(resv);
         } catch(Exception e) {
@@ -207,6 +247,16 @@ public class PathSetupManager{
         return status;
     }
     
+    /**
+     * Handles the notifications of an upstream or downstream PATH_SETUP_CONFIRMED
+     * event.
+     *
+     * @param gri the gri of the affected reservation
+     * @param producerID the producer of the notification
+     * @param upstream true if supposed to be from upstream neighbor
+     * @throws BSSException
+     *
+     */
     public void handleSetupEvent(String gri, String producerID, boolean upstream) 
                                 throws BSSException{
         ReservationDAO dao = new ReservationDAO(this.dbname);
@@ -255,14 +305,8 @@ public class PathSetupManager{
             return;
         }
         
-        String status = se.getStatus(resv);
-        if(StateEngine.INSETUP.equals(status)){
-            this.updateCreateStatus(newLocalStatus, resv);
-            return;
-        }
-        
-        //schedule a new job
-        
+        this.scheduleUpdateAttempt(0,gri,login,StateEngine.INSETUP,
+                                   newLocalStatus,"setup",upstream,-1);
     }
     
     /**
@@ -300,6 +344,87 @@ public class PathSetupManager{
             se.updateStatus(resv, StateEngine.ACTIVE);
             se.updateLocalStatus(resv, 0);
             eventProducer.addEvent(OSCARSEvent.PATH_SETUP_COMPLETED, login, "JOB", resv);
+        }
+     }
+     
+     /**
+     * Schedules a job to check if path can be updated 
+     *
+     * @param wait the time to wait before attempting
+     * @param gri the reservation too update
+     * @param login the login of the reservation owner
+     * @param targStatus the state the reservation must be in for a notification to be handled
+     * @param newLocalStatus the new local state to apply to the reservation
+     * @param op setup or teardown
+     * @param upstream true if from upstream
+     * @param retries the number of times to retry
+     */
+     public void scheduleUpdateAttempt(long wait, String gri, 
+                               String login,  String targStatus, 
+                               int newLocalStatus, String op, 
+                               boolean upstream, int retries){
+        Scheduler sched = this.core.getScheduleManager().getScheduler();
+        String triggerName = "pathRetryTrig-" + gri.hashCode();
+        String jobName = "pathRetryJob-" + gri.hashCode();
+        long time = System.currentTimeMillis() + wait*1000;
+        Date date = new Date(time);
+        SimpleTrigger trigger = new SimpleTrigger(triggerName, null, 
+                                                  date, null, 0, 0L);
+        JobDetail jobDetail = new JobDetail(jobName, "PATH_RETRY", 
+                                            PathTimeoutJob.class);
+        JobDataMap dataMap = new JobDataMap();
+        dataMap.put("retry", true);
+        dataMap.put("gri", gri);
+        dataMap.put("login", login);
+        dataMap.put("targStatus", targStatus);
+        dataMap.put("newLocalStatus", newLocalStatus);
+        dataMap.put("op", op);
+        dataMap.put("upstream", upstream);
+        dataMap.put("retries", retries);
+        jobDetail.setJobDataMap(dataMap);
+        
+        try{
+            this.log.debug("Adding job " + jobName);
+            sched.scheduleJob(jobDetail, trigger);
+            this.log.debug("Job added.");
+        }catch(SchedulerException ex){
+            this.log.error("Scheduler exception: " + ex);    
+        }
+     }
+     
+     /**
+     * Schedules a job to check if a request timed out
+     *
+     * @param timeout the time to wait
+     * @param resv the reservation to be check
+     * @param op setup or teardown
+     * @param upstream true if applies to upstream event
+     */
+     public void scheduleStatusCheck(long timeout, Reservation resv, String op, boolean upstream){
+        Scheduler sched = this.core.getScheduleManager().getScheduler();
+        StateEngine se = this.core.getStateEngine();
+        String prefix = op + (upstream?"-up-":"-down-");
+        String triggerName = prefix + "pathTimeoutTrig-" + resv.hashCode();
+        String jobName = prefix + "pathTimeoutJob-" + resv.hashCode();
+        long time = System.currentTimeMillis() + timeout*1000;
+        Date date = new Date(time);
+        SimpleTrigger trigger = new SimpleTrigger(triggerName, null, 
+                                                  date, null, 0, 0L);
+        JobDetail jobDetail = new JobDetail(jobName, "PATH_TIMEOUT", 
+                                            PathTimeoutJob.class);
+        JobDataMap dataMap = new JobDataMap();
+        dataMap.put("statusCheck", true);
+        dataMap.put("gri", resv.getGlobalReservationId());
+        dataMap.put("status", se.getStatus(resv));
+        dataMap.put("op", op);
+        dataMap.put("upstream", upstream);
+        jobDetail.setJobDataMap(dataMap);
+        try{
+            this.log.debug("Adding job " + jobName);
+            sched.scheduleJob(jobDetail, trigger);
+            this.log.debug("Job added.");
+        }catch(SchedulerException ex){
+            this.log.error("Scheduler exception: " + ex);    
         }
      }
 }
---
+++ src/net/es/oscars/scheduler/PathTimeoutJob.java
@@ -0,0 +1,187 @@
+package net.es.oscars.scheduler;
+
+import java.util.Properties;
+import java.util.Date;
+import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.quartz.*;
+import net.es.oscars.bss.*;
+import net.es.oscars.bss.topology.*;
+import net.es.oscars.wsdlTypes.*;
+import net.es.oscars.interdomain.*;
+import net.es.oscars.notify.*;
+import net.es.oscars.oscars.*;
+import net.es.oscars.pss.*;
+import net.es.oscars.PropHandler;
+
+public class PathTimeoutJob implements org.quartz.Job {
+    private Logger log;
+    private OSCARSCore core;
+    private StateEngine se;
+    private int SETUP_RETRIES = 10;
+    private int TEARDOWN_RETRIES = 10;
+    private long SETUP_RETRY_WAIT = 30;
+    private long TEARDOWN_RETRY_WAIT = 30;
+    
+    
+    /**
+     * Assigns the job to the start, confirm or complete method
+     *
+     * @param context the job context
+     */
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        String jobName = context.getJobDetail().getFullName();
+        this.log = Logger.getLogger(this.getClass());
+        this.log.debug("PathTimeoutJob.start name:"+jobName);
+        this.core = OSCARSCore.getInstance();
+        this.se = this.core.getStateEngine();
+        PathSetupManager pm = this.core.getPathSetupManager();
+        String idcURL = this.core.getServiceManager().getIdcURL();
+        EventProducer eventProducer = new EventProducer();
+        
+        JobDataMap dataMap = context.getJobDetail().getJobDataMap();
+        String gri =  dataMap.getString("gri");
+        String login = dataMap.getString("login");
+        String targStatus = dataMap.getString("targStatus");
+        int newLocalStatus = dataMap.getInt("newLocalStatus");
+        int retries = dataMap.getInt("retries");
+        String op = dataMap.getString("op");
+        boolean upstream = dataMap.getBoolean("upstream");
+        
+        Reservation resv = null;
+        this.log.debug("GRI is: "+dataMap.get("gri")+"for job name: "+jobName);
+        this.init();
+        
+        String failedEvent = null;
+        String eventType = null;
+        long retryWait = SETUP_RETRY_WAIT;
+        if(op.equals("setup")){
+            failedEvent = OSCARSEvent.PATH_SETUP_FAILED;
+            eventType = (upstream ? OSCARSEvent.UP_PATH_SETUP_CONFIRMED : 
+                                   OSCARSEvent.DOWN_PATH_SETUP_CONFIRMED);
+            retries = (retries == -1 ? SETUP_RETRIES : retries);
+        }else if(op.equals("teardown")){
+            failedEvent = OSCARSEvent.PATH_TEARDOWN_FAILED;
+            eventType = (upstream ? OSCARSEvent.UP_PATH_TEARDOWN_CONFIRMED : 
+                                   OSCARSEvent.DOWN_PATH_TEARDOWN_CONFIRMED);
+            retries = (retries == -1 ? TEARDOWN_RETRIES : retries);
+            retryWait = TEARDOWN_RETRY_WAIT;
+        }else{
+            return;
+        }
+        
+        Session bss = core.getBssSession();
+        bss.beginTransaction();
+        
+        /* Get reservation */
+        try{
+            ReservationDAO dao = new ReservationDAO(this.core.getBssDbName());
+            resv = dao.query(gri);
+        }catch(BSSException ex){
+            bss.getTransaction().rollback();
+            this.log.error(ex.getMessage());
+            eventProducer.addEvent(failedEvent, login, 
+                                   "JOB", "", ex.getMessage());
+            return;
+        }
+        
+        try{
+            if(dataMap.containsKey("retry")){
+                /* try updating the status if a confirmed event was sent from
+                   another domain whose clock is slightly earlier than the 
+                   local clock */
+                String status = se.getStatus(resv);
+                if(targStatus.equals(status)){
+                    pm.updateCreateStatus(newLocalStatus, resv);
+                }else if(retries >= 1){
+                    retries--;
+                    pm.scheduleUpdateAttempt(retryWait, gri, login, targStatus,
+                                        newLocalStatus, op, upstream, retries);
+                }else{
+                    throw new BSSException("Unable to update path after numerous attempts.");
+                }
+            }else if(dataMap.containsKey("statusCheck")){
+                /* timeout a reservation if its not updated by 
+                   the time this job runs */
+                String status = this.se.getStatus(resv);
+                int localStatus = this.se.getLocalStatus(resv);
+                int bit = (upstream ? 4 : 2);
+                if(status.equals(dataMap.getString("status")) && 
+                    (localStatus & bit) != 1){
+                    throw new BSSException("Timeout while waiting for event "+
+                                           eventType);
+                }
+            }else{
+                this.log.error("Unknown path timeout job cannot be executed");
+            }
+            bss.getTransaction().commit();
+        }catch(Exception ex){
+            ex.printStackTrace();
+            //Rollback any changes...
+            bss.getTransaction().rollback();
+            //...then start new transaction to update status
+            try{
+                bss = core.getBssSession();
+                bss.beginTransaction();
+                se.updateStatus(resv, StateEngine.FAILED);
+                se.updateLocalStatus(resv, 0);
+                eventProducer.addEvent(failedEvent, login, idcURL, 
+                                       resv, "", ex.getMessage());
+                bss.getTransaction().commit();
+            }catch(Exception ex2){
+                bss.getTransaction().rollback();
+                this.log.error(ex2);
+            }
+        }
+        this.log.debug("PathTimeoutJob.end name:"+jobName);
+    }
+    
+    /**
+     * Reads in timeout properties
+     */
+     public void init(){
+        PropHandler propHandler = new PropHandler("oscars.properties");
+        Properties props = propHandler.getPropertyGroup("timeout", true);
+        String createRetries = props.getProperty("createPath.retryAttempts");
+        String createRetryWait = props.getProperty("createPath.retryWait");
+        String teardownRetries = props.getProperty("teardownPath.retryAttempts");
+        String teardownRetryWait = props.getProperty("teardownPath.retryWait");
+        
+        if(createRetries != null){
+            try{
+                SETUP_RETRIES = Integer.parseInt(createRetries);
+            }catch(Exception e){
+                this.log.error("timeout.createPath.retryAttempts property invalid." +
+                               "Defaulting to another value for timeout.");
+            }
+        }
+        
+        if(createRetryWait != null){
+            try{
+                SETUP_RETRY_WAIT = Long.parseLong(createRetryWait);
+            }catch(Exception e){
+                this.log.error("timeout.createPath.retryWait property invalid." +
+                               "Defaulting to another value for timeout.");
+            }
+        }
+        
+        if(teardownRetries != null){
+            try{
+                TEARDOWN_RETRIES = Integer.parseInt(teardownRetries);
+            }catch(Exception e){
+                this.log.error("timeout.teardownPath.retryAttempts property invalid." +
+                               "Defaulting to another value for timeout.");
+            }
+        }
+        
+        if(teardownRetryWait != null){
+            try{
+                TEARDOWN_RETRY_WAIT = Long.parseLong(teardownRetryWait);
+            }catch(Exception e){
+                this.log.error("timeout.teardownPath.retryWait property invalid." +
+                               "Defaulting to another value for timeout.");
+            }
+        }
+     }
+
+}
