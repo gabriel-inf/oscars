caa0619c383b017782f969934b82f6b42a76dc7c
andy <andy@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2008-09-06 13:58:29 +0000

    Fixed NULL error in bss script caused by ESnet specific INSERTS. Moved inserts to own script
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@7110 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- sql/bss/createTables.sql
+++ sql/bss/createTables.sql
@@ -341,18 +341,6 @@ CREATE TABLE IF NOT EXISTS sites (
     PRIMARY KEY (id)
 ) type=MyISAM;
 
-
-INSERT IGNORE INTO sites VALUES ( 1,"Energy Sciences Network", 
-     (select id from domains where topologyIdent="es.net"));
-INSERT IGNORE INTO sites VALUES (2, "Internet2",
-      (select id from domains where topologyIdent="dcn.internet2.edu"));
-INSERT IGNORE INTO sites VALUES (3, "Brookhaven National Laboratory",
-      (select id from domains where topologyIdent="bnl.gov"));
-INSERT IGNORE INTO sites VALUES (4, "Fermilab",
-      (select id from domains where topologyIdent="fnal.gov")); 
-INSERT IGNORE INTO sites VALUES (5, "DANTE",
-      (select id from domains where topologyIdent="geant2.net")); 
-
 --
 -- Table to lookup external servcies associated with a domain
 -- Used as a backup to the perfSONAR LS
