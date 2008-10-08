e160dfd28b7cbda4fb9b91c65985c2107448e7c2
aaron <aaron@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2008-10-08 15:33:13 +0000

    Allow only registering an opaque domain with the Topology Service. An opaque domain contains only those elements that are externally facing. So, if enabled, only links to remote locations, ports with links to remote locations and nodes with ports with links to remote locations will be included in the registration. The pathfinding can use these opaque domains instead of the real thing, though weighting probably needs to be thought about. Currently, opaque domains will have higher priority since they're weighted as though it's cost-free to move through the domain.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@7217 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- src/net/es/oscars/scheduler/TopologyRegisterJob.java
+++ src/net/es/oscars/scheduler/TopologyRegisterJob.java
@@ -12,6 +12,9 @@ import org.apache.axiom.om.OMAbstractFactory;
 import org.apache.axis2.databinding.utils.writer.MTOMAwareXMLSerializer;
 import org.ogf.schema.network.topology.ctrlplane.CtrlPlaneTopologyContent;
 import org.ogf.schema.network.topology.ctrlplane.CtrlPlaneDomainContent;
+import org.ogf.schema.network.topology.ctrlplane.CtrlPlaneNodeContent;
+import org.ogf.schema.network.topology.ctrlplane.CtrlPlanePortContent;
+import org.ogf.schema.network.topology.ctrlplane.CtrlPlaneLinkContent;
 import edu.internet2.perfsonar.*;
 
 import net.es.oscars.oscars.OSCARSCore;
@@ -28,6 +31,7 @@ public class TopologyRegisterJob implements Job{
     private OSCARSCore core;
     private long RENEW_TIME = 1800;//30 minutes
     private String URL = "http://127.0.0.1:8089/perfSONAR_PS/services/topology";
+    private boolean makeDomainsOpaque = false;
     
     /**
      * Registers topology with the topology service
@@ -43,7 +47,7 @@ public class TopologyRegisterJob implements Job{
         ServiceManager serviceMgr = this.core.getServiceManager();
         JobDataMap dataMap = context.getJobDetail().getJobDataMap();
         this.init();
-        this.log.debug("url=" + URL);
+        this.log.debug("Topology URL=" + URL);
         PSTopologyClient psClient = new PSTopologyClient(URL);
         
         Session bss = this.core.getBssSession();
@@ -59,6 +63,10 @@ public class TopologyRegisterJob implements Job{
                 String domainId = parseResults.get("domainId");
                 Domain dbDomain = domainDAO.fromTopologyIdent(domainId);
                 if(dbDomain != null && dbDomain.isLocal()){
+                    if (this.makeDomainsOpaque) {
+                        this.makeDomainOpaque(domain);
+                    }
+
                     StringWriter sw = new StringWriter();
                     XMLStreamWriter writer = XMLOutputFactory.newInstance().createXMLStreamWriter(sw);
                     MTOMAwareXMLSerializer mtom = new MTOMAwareXMLSerializer(writer);
@@ -81,7 +89,67 @@ public class TopologyRegisterJob implements Job{
         serviceMgr.scheduleServiceJob(TopologyRegisterJob.class, dataMap, new Date(nextJobTime));
         this.log.info("TopologyRegisterJob.end name:"+jobName);
     }
-    
+
+    /**
+    * We iterate through the domain, blowing away the existing element children
+    * and re-adding them if they are an external facing element.
+    */
+     private void makeDomainOpaque(CtrlPlaneDomainContent domain) {
+        String domainId = domain.getId();
+
+        if (domainId == null) {
+            return;
+        }
+
+        List<CtrlPlaneNodeContent> newNodes = new ArrayList<CtrlPlaneNodeContent>();
+ 
+        Iterator<CtrlPlaneNodeContent> nodeIter = Arrays.asList(domain.getNode()).iterator();
+        while(nodeIter.hasNext()) {
+            CtrlPlaneNodeContent node = nodeIter.next();
+
+            List<CtrlPlanePortContent> newPorts = new ArrayList<CtrlPlanePortContent>();
+            Iterator<CtrlPlanePortContent> portIter = Arrays.asList(node.getPort()).iterator();
+            while(portIter.hasNext()) {
+                CtrlPlanePortContent port = portIter.next();
+
+                List<CtrlPlaneLinkContent> newLinks = new ArrayList<CtrlPlaneLinkContent>();
+
+                Iterator<CtrlPlaneLinkContent> linkIter = Arrays.asList(port.getLink()).iterator();
+                while(linkIter.hasNext()) {
+                    CtrlPlaneLinkContent link = linkIter.next();
+
+                    String localId = link.getId();
+                    String remoteLinkId = link.getRemoteLinkId();
+
+                    if (remoteLinkId == null || localId == null) {
+                        continue;
+                    }
+
+                    Hashtable<String, String> remoteLinkURN = URNParser.parseTopoIdent(remoteLinkId);
+                    if (remoteLinkURN.get("error") != null) {
+                        continue;
+                    }
+
+                    if (remoteLinkURN.get("domainFQID").equals(domainId) == false) {
+                        newLinks.add(link);
+                    }
+                }
+
+                if (newLinks.isEmpty() == false) {
+                    port.setLink(newLinks.toArray(new CtrlPlaneLinkContent[newLinks.size()]));
+                    newPorts.add(port);
+                }
+            }
+
+            if (newPorts.isEmpty() == false) {
+                node.setPort(newPorts.toArray(new CtrlPlanePortContent[newPorts.size()]));
+                newNodes.add(node);
+            }
+        }
+
+        domain.setNode(newNodes.toArray(new CtrlPlaneNodeContent[newNodes.size()]));
+    }
+
     /**
      * Initializes the job and loads values from oscars.properties into global variables
      */
@@ -100,6 +168,17 @@ public class TopologyRegisterJob implements Job{
             try{
                 URL = psProps.getProperty("topology_url");
             }catch(Exception e){}
+        }
+
+        if(psProps.getProperty("domainOpacity") != null){
+            String opacity = psProps.getProperty("domainOpacity");
+            if (opacity.equals("complete")) {
+                this.makeDomainsOpaque = true;
+            } else if (opacity.equals("none")) {
+                this.makeDomainsOpaque = false;
+            } else {
+                this.log.error("Unknown domain opacity for topology registration, "+opacity+", must be 'complete' or 'none'");
+            }
         }
     }
     
