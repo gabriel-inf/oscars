cde15632ea7e307ffbfb1cb91f1ae019bb250c12
andy <andy@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2008-10-17 14:51:41 +0000

    Added code to do_install.sh to edit services.xml with CATALINA_HOME prior to deployment
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@7243 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- do_install.sh
+++ do_install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 echo "  ";
 echo "--- Checking prerequisites...";
 if [ -d src/net/es/oscars ]; then
@@ -36,6 +36,26 @@ if [ "$ans" == "y" ] || [ "$ans" == "Y" ]; then
 	ant setupServer;
 	STATUS2=", server configured"
 fi
+
+#Update services.xml
+CATALINA_HOME_LEN=`expr length "$CATALINA_HOME"`;
+if [ `echo $CATALINA_HOME | egrep "/$"` ]; then
+    CATALINA_HOME_LEN=`expr $CATALINA_HOME_LEN - 1`;
+fi
+CATALINA_HOME_ESC=`expr substr "$CATALINA_HOME" 1 $CATALINA_HOME_LEN`;
+CATALINA_HOME_ESC=`echo $CATALINA_HOME_ESC | sed -e 's/\//\\\\\//g'`;
+sed -i -e "s/<\!ENTITY rampConfig SYSTEM \".*\">/<\!ENTITY rampConfig SYSTEM \"$CATALINA_HOME_ESC\/shared\/classes\/repo\/rampConfig.xml\">/" conf/server/services.xml;
+if [ $? != 0 ]; then
+    echo "Error modifying conf/server/services.xml"
+    exit 1;
+fi
+sed -i -e "s/<\!ENTITY rampConfig SYSTEM \".*\">/<\!ENTITY rampConfig SYSTEM \"$CATALINA_HOME_ESC\/shared\/classes\/repo\/rampConfig.xml\">/" conf/notify-server/services.xml;
+if [ $? != 0 ]; then
+    echo "Error modifying conf/notify-server/services.xml"
+    exit 1;
+fi
+
+#Deploy IDC
 echo "--- Deploying IDC...";
 ant deployall;
 echo "  ";
