92767d4a98e74e75b9d61125794de2d4569cda71
haniotak <haniotak@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2008-12-03 22:54:36 +0000

    Set constructor to private on pathType
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@7361 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- src/net/es/oscars/bss/topology/PathType.java
+++ src/net/es/oscars/bss/topology/PathType.java
@@ -5,6 +5,10 @@ public class PathType {
     public static final String INTERDOMAIN = "interdomain";
     public static final String LOCAL = "local";
     public static final String REQUESTED = "requested";
+    
+    // never construct this object
+    private PathType() {
+    }
 
     public static boolean isValid(String pathType) {
         if (pathType.equals(INTERDOMAIN)) {
