4dc6a5c5f652dee3518f29510b2808a9f9e3e779
haniotak <haniotak@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2008-12-13 00:32:16 +0000

    Various improvements for init scripts
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@7423 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars_core.sh
+++ oscars_core.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 
-
+# Try to cd to the application base directory if the variable is set
 cd ${OSCARS_BASE-.};
 
-# update classpath
+
+# Create the classpath:
 OSCARS_CLASSPATH=""
 for f in ./lib/*.jar
 do
@@ -17,21 +18,24 @@ do
     AXIS2_CLASSPATH="$AXIS2_CLASSPATH":$f
 done
 
-
-export CATALINA_HOME=/usr/local/tomcat/;
-
 CLASSPATH=$AXIS2_CLASSPATH:$OSCARS_CLASSPATH
-CLASSPATH=$CLASSPATH:build/OSCARS.jar:$CATALINA_HOME/shared/classes/
+CLASSPATH=$CLASSPATH:build/OSCARS.jar
 
 export CLASSPATH=$CLASSPATH
+
 # echo "CLASSPATH is: $CLASSPATH"
+
+
+# Decide if we are being called as a daemon or from the command-line
 CALLING_STYLE=${DAEMON_STYLE-0}
 
 if [ $CALLING_STYLE -ne 1 ]; then
+    touch /tmp/oscars_core.lock
     java -Djava.net.preferIPv4Stack=true net.es.oscars.oscars.OSCARSRunner $*
+    rm -f /tmp/oscars_core.lock
 else
     nohup java -Djava.net.preferIPv4Stack=true net.es.oscars.oscars.OSCARSRunner $* > /dev/null 2&>1 &
-    echo $! > /tmp/oscars_core.pid
+    echo $! > $PID_FILE
 fi
 
 
