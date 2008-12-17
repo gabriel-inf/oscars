ed4e504c2415c296e4b0c566e21e7a23017d28da
haniotak <haniotak@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2008-12-17 00:56:33 +0000

    Added shell script & ant task for new AAA jar
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@7442 c75a6b91-02ef-0310-b74a-e20389c9c41d

---
+++ oscars_aaa.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+
+# Try to cd to the application base directory if the variable is set
+cd ${OSCARS_BASE-.};
+
+
+# Create the classpath:
+OSCARS_CLASSPATH=""
+for f in ./lib/*.jar
+do
+    OSCARS_CLASSPATH="$OSCARS_CLASSPATH":$f
+done
+AXIS2_CLASSPATH=""
+for f in ./lib/axis2/*.jar
+do
+    AXIS2_CLASSPATH="$AXIS2_CLASSPATH":$f
+done
+
+
+CLASSPATH=$AXIS2_CLASSPATH:$OSCARS_CLASSPATH
+CLASSPATH=$CLASSPATH:build/AAA.jar
+
+export CLASSPATH=$CLASSPATH
+
+# echo "CLASSPATH is: $CLASSPATH"
+
+
+# Decide if we are being called as a daemon or from the command-line
+CALLING_STYLE=${DAEMON_STYLE-0}
+
+if [ $CALLING_STYLE -ne 1 ]; then
+    touch /tmp/oscars_aaa.lock
+    java -Djava.net.preferIPv4Stack=true net.es.oscars.aaa.AAARunner $*
+    rm -f /tmp/oscars_aaa.lock
+else
+    nohup java -Djava.net.preferIPv4Stack=true net.es.oscars.aaa.AAARunner $* > /dev/null 2&>1 &
+    echo $! > $PID_FILE
+fi
+
+
+exit 0
