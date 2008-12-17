68ec78e326fe1f2c7ec6b0f5d84a438d5d900921
haniotak <haniotak@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2008-12-17 00:27:07 +0000

    Changes to make PropertyLoader behave more like PropHandler
    (depending on how we handle config we might not use these changes)
    
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@7439 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- src/net/es/oscars/PropertyLoader.java
+++ src/net/es/oscars/PropertyLoader.java
@@ -19,84 +19,101 @@ public abstract class PropertyLoader
      * /some/pkg/Resource
      * /some/pkg/Resource.properties
      * </pre>
-     * 
+     *
      * @param name classpath resource name [may not be null]
      * @param loader classloader through which to load the resource [null
      * is equivalent to the application loader]
-     * 
+     *
      * @return resource converted to java.util.Properties [may be null if the
      * resource was not found and THROW_ON_LOAD_FAILURE is false]
      * @throws IllegalArgumentException if the resource was not found and
      * THROW_ON_LOAD_FAILURE is true
      */
-    public static Properties loadProperties (String name, ClassLoader loader)
+    public static Properties loadProperties (String name, ClassLoader loader, String groupName, boolean stripPrefix)
     {
         if (name == null)
             throw new IllegalArgumentException ("null input: name");
-        
+
         if (name.startsWith ("/"))
             name = name.substring (1);
-            
+
         if (name.endsWith (SUFFIX))
             name = name.substring (0, name.length () - SUFFIX.length ());
-        
-        Properties result = null;
-        
+
+        Properties tempProps = null;
+
         InputStream in = null;
         try {
             if (loader == null) loader = ClassLoader.getSystemClassLoader ();
-            
-            if (LOAD_AS_RESOURCE_BUNDLE) {    
+
+            if (LOAD_AS_RESOURCE_BUNDLE) {
                 name = name.replace ('/', '.');
                 // Throws MissingResourceException on lookup failures:
                 final ResourceBundle rb = ResourceBundle.getBundle (name,
                     Locale.getDefault (), loader);
-                
-                result = new Properties ();
+
+                tempProps = new Properties ();
                 for (Enumeration keys = rb.getKeys (); keys.hasMoreElements ();) {
                     final String key = (String) keys.nextElement ();
                     final String value = rb.getString (key);
-                    
-                    result.put (key, value);
-                } 
+
+                    tempProps.put (key, value);
+                }
             } else {
                 name = name.replace ('.', '/');
-                
+
                 if (! name.endsWith (SUFFIX))
                     name = name.concat (SUFFIX);
-                                
+
                 // Returns null on lookup failures:
                 in = loader.getResourceAsStream (name);
                 if (in != null) {
-                    result = new Properties ();
-                    result.load (in); // Can throw IOException
+                    tempProps = new Properties ();
+                    tempProps.load (in); // Can throw IOException
                 }
             }
         } catch (Exception e) {
-            result = null;
+            tempProps = null;
         } finally {
             if (in != null) try { in.close (); } catch (Throwable ignore) {}
         }
-        
-        if (THROW_ON_LOAD_FAILURE && (result == null)) {
+
+        if (THROW_ON_LOAD_FAILURE && (tempProps == null)) {
             throw new IllegalArgumentException ("could not load [" + name + "]"+
                 " as " + (LOAD_AS_RESOURCE_BUNDLE
                 ? "a resource bundle"
                 : "a classloader resource"));
         }
-        
+
+        Properties result = new Properties();
+        Enumeration e = tempProps.propertyNames();
+        while (e.hasMoreElements()) {
+            String propertyName = null;
+            String elem = ( String )e.nextElement();
+            if (elem.startsWith(groupName)) {
+                if (stripPrefix) {
+                    // get rid of period as well
+                    propertyName = elem.substring(groupName.length()+1);
+                } else {
+                    propertyName = elem;
+                }
+                String propertyValue = tempProps.getProperty(elem);
+                result.setProperty(propertyName, propertyValue);
+            }
+        }
+
         return result;
     }
-    
+
     /**
      * A convenience overload of {@link #loadProperties(String, ClassLoader)}
      * that uses the current thread's context classloader.
      */
-    public static Properties loadProperties (final String name) {
-        return loadProperties (name, Thread.currentThread().getContextClassLoader());
+    public static Properties loadProperties (final String name, String groupName, boolean stripPrefix) {
+        return loadProperties (name, Thread.currentThread().getContextClassLoader(), groupName, stripPrefix);
     }
-        
+
     private static final boolean THROW_ON_LOAD_FAILURE = true;
     private static final boolean LOAD_AS_RESOURCE_BUNDLE = false;
     private static final String SUFFIX = ".properties";
-} 
+}
