dae2d1cefbaa9ff035e8eba5259cd6b986183f84
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2009-01-07 20:08:52 +0000

    Fixed attribute create with browser, and stylistic changes.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@7478 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- src/net/es/oscars/rmi/aaa/AttributeModelRmiHandler.java
+++ src/net/es/oscars/rmi/aaa/AttributeModelRmiHandler.java
@@ -18,13 +18,13 @@ public class AttributeModelRmiHandler extends ModelRmiHandlerImpl {
     private AAACore core = AAACore.getInstance();
     private Logger log;
 
-
     public AttributeModelRmiHandler() {
         this.log = Logger.getLogger(this.getClass());
     }
 
+    public HashMap<String, Object> list(HashMap<String, Object> parameters)
+            throws RemoteException {
 
-    public HashMap<String, Object> list(HashMap<String, Object> parameters) throws RemoteException {
         this.log.debug("listAttributes.start");
         Session aaa = core.getAaaSession();
         aaa.beginTransaction();
@@ -46,44 +46,43 @@ public class AttributeModelRmiHandler extends ModelRmiHandlerImpl {
             throw new RemoteException("unknown listBy");
         }
         HashMap<String, Object> result = new HashMap<String, Object>();
-
         for (Attribute attr: attributes) {
             Hibernate.initialize(attr);
         }
-
         aaa.getTransaction().commit();
         result.put("attributes", attributes);
         this.log.debug("listAttributes.end");
         return result;
     }
 
+    public HashMap<String, Object> add(HashMap<String, Object> parameters)
+            throws RemoteException {
 
-    public HashMap<String, Object> add(HashMap<String, Object> parameters) throws RemoteException {
         this.log.info("addAttribute.start");
         Session aaa = core.getAaaSession();
         aaa.beginTransaction();
         HashMap<String, Object> result = new HashMap<String, Object>();
-
         AttributeDAO dao = new AttributeDAO(core.getAaaDbName());
         Attribute attribute = (Attribute) parameters.get("attribute");
         if (attribute == null) {
             aaa.getTransaction().rollback();
             throw new RemoteException("No attribute set");
         }
-
         Attribute oldAttribute = dao.queryByParam("name", attribute.getName());
         if (oldAttribute != null) {
-            this.log.info("already found attribute with name:"+attribute.getName());
-            dao.create(attribute);
+            aaa.getTransaction().rollback();
+            throw new RemoteException("already found attribute with name:" +
+                                      attribute.getName());
         }
-
+        dao.create(attribute);
         aaa.getTransaction().commit();
         this.log.info("addAttribute.end");
         return result;
     }
 
+    public HashMap<String, Object> modify(HashMap<String, Object> parameters)
+           throws RemoteException {
 
-    public HashMap<String, Object> modify(HashMap<String, Object> parameters) throws RemoteException {
         this.log.debug("modifyAttribute.start");
         Session aaa = core.getAaaSession();
         aaa.beginTransaction();
@@ -98,7 +97,6 @@ public class AttributeModelRmiHandler extends ModelRmiHandlerImpl {
             aaa.getTransaction().rollback();
             throw new RemoteException("oldName not set");
         }
-
         AttributeDAO dao = new AttributeDAO(core.getAaaDbName());
         Attribute oldAttribute = dao.queryByParam("name", oldName);
         if (oldAttribute == null) {
@@ -109,15 +107,14 @@ public class AttributeModelRmiHandler extends ModelRmiHandlerImpl {
         oldAttribute.setDescription(attribute.getDescription());
         oldAttribute.setAttrType(attribute.getAttrType());
         dao.update(oldAttribute);
-
-
         aaa.getTransaction().commit();
         this.log.debug("modifyAttribute.end");
         return result;
     }
 
+    public HashMap<String, Object> delete(HashMap<String, Object> parameters)
+            throws RemoteException {
 
-    public HashMap<String, Object> delete(HashMap<String, Object> parameters) throws RemoteException {
         this.log.debug("deleteAttribute.start");
         Session aaa = core.getAaaSession();
         aaa.beginTransaction();
@@ -127,23 +124,20 @@ public class AttributeModelRmiHandler extends ModelRmiHandlerImpl {
             aaa.getTransaction().rollback();
             throw new RemoteException("Attribute name not set");
         }
-
-
         AttributeDAO dao = new AttributeDAO(core.getAaaDbName());
-        UserAttributeDAO userAttributeDAO = new UserAttributeDAO(core.getAaaDbName());
+        UserAttributeDAO userAttributeDAO =
+            new UserAttributeDAO(core.getAaaDbName());
         AuthorizationDAO authDAO = new AuthorizationDAO(core.getAaaDbName());
         Attribute attribute = dao.queryByParam("name", attributeName);
-
         boolean existingUsers = false;
         boolean existingAuthorizations = false;
-
         if (attribute == null) {
             aaa.getTransaction().rollback();
             throw new RemoteException("Attribute " + attributeName + " does not exist to be deleted");
         }
-
         try {
-            List<User> users = userAttributeDAO.getUsersByAttribute(attributeName);
+            List<User> users =
+                userAttributeDAO.getUsersByAttribute(attributeName);
             StringBuilder sb = new StringBuilder();
             if (users.size() != 0) {
                 sb.append(attributeName + " has existing users: ");
@@ -152,15 +146,14 @@ public class AttributeModelRmiHandler extends ModelRmiHandlerImpl {
                 }
                 existingUsers = true;
             }
-
             List<Authorization> auths = authDAO.listAuthByAttr(attributeName);
             if (auths.size() != 0) {
                 if (existingUsers) {
-                    sb.append(".  There are " + auths.size() + " existing authorizations " +
-                        "with this attribute.");
+                    sb.append(".  There are " + auths.size() +
+                            " existing authorizations with this attribute.");
                 } else {
                     sb.append(attributeName + " has " + auths.size() +
-                              " associated authorizations.  Cannot delete.");
+                            " associated authorizations.  Cannot delete.");
                 }
                 existingAuthorizations = true;
             }
@@ -171,15 +164,15 @@ public class AttributeModelRmiHandler extends ModelRmiHandlerImpl {
             aaa.getTransaction().rollback();
             throw new RemoteException(ex.getMessage());
         }
-
-
         dao.remove(attribute);
-
         aaa.getTransaction().commit();
         this.log.debug("deleteAttribute.end");
         return result;
     }
-    public HashMap<String, Object> find(HashMap<String, Object> parameters) throws RemoteException {
+
+    public HashMap<String, Object> find(HashMap<String, Object> parameters)
+            throws RemoteException {
+
         this.log.debug("findAttribute.start");
         Integer id = (Integer) parameters.get("id");
         Session aaa = core.getAaaSession();
@@ -189,11 +182,9 @@ public class AttributeModelRmiHandler extends ModelRmiHandlerImpl {
         AttributeDAO AttributeDAO = new AttributeDAO(core.getAaaDbName());
         Attribute attribute = AttributeDAO.findById(id, false);
         Hibernate.initialize(attribute);
-
         result.put("attribute", attribute);
         aaa.getTransaction().commit();
         this.log.debug("findAttribute.end");
         return result;
     }
-
 }
