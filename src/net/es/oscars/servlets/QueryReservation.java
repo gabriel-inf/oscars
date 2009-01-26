8ac3c1d3d87f2cde61eea6c493a74e0a2cc2523b
davidr <davidr@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2009-01-26 22:45:39 +0000

    Added back contents of reservation details page.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@7524 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- src/net/es/oscars/servlets/QueryReservation.java
+++ src/net/es/oscars/servlets/QueryReservation.java
@@ -93,8 +93,9 @@ public class QueryReservation extends HttpServlet {
         }
         try {
             this.contentSection(rmiReply, outputMap);
-        } catch (BSSException ex) {
-            ;
+        } catch (BSSException e) {
+            ServletUtils.handleFailure(out, e, methodName);
+            return;
         }
         Reservation resv = rmiReply.getReservation();
         outputMap.put("status", "Reservation details for " +
@@ -118,6 +119,136 @@ public class QueryReservation extends HttpServlet {
         contentSection(RmiQueryResReply rmiReply, Map<String,Object> outputMap)
             throws BSSException {
 
-            // TODO:  add back
+        InetAddress inetAddress = null;
+        String hostName = null;
+        Long longParam = null;
+        Integer intParam = null;
+        String strParam = null;
+
+        Reservation resv = rmiReply.getReservation();
+        String gri = resv.getGlobalReservationId();
+        Path path = resv.getPath(PathType.LOCAL);
+        Layer2Data layer2Data = path.getLayer2Data();
+        Layer3Data layer3Data = path.getLayer3Data();
+        MPLSData mplsData = path.getMplsData();
+        String status = resv.getStatus();
+        // always blank NEW GRI field, current GRI is in griReplace's
+        // innerHTML
+        outputMap.put("newGri", "");
+        outputMap.put("griReplace", gri);
+        outputMap.put("statusReplace", status);
+        outputMap.put("userReplace", resv.getLogin());
+        String sanitized = resv.getDescription().replace("<", "");
+        String sanitized2 = sanitized.replace(">", "");
+        outputMap.put("descriptionReplace", sanitized2);
+
+        outputMap.put("modifyStartSeconds", resv.getStartTime());
+        outputMap.put("modifyEndSeconds", resv.getEndTime());
+        outputMap.put("createdTimeConvert", resv.getCreatedTime());
+        // convert to Mbps, commas added by Dojo
+        outputMap.put("bandwidthReplace", resv.getBandwidth()/1000000);
+        if (layer2Data != null) {
+            outputMap.put("sourceReplace", layer2Data.getSrcEndpoint());
+            outputMap.put("destinationReplace", layer2Data.getDestEndpoint());
+            String vlanTag = BssUtils.getVlanTag(path);
+            if (vlanTag != null) {
+                int storedVlan = Integer.parseInt(vlanTag);
+                int vlanNum = Math.abs(storedVlan);
+                outputMap.put("vlanReplace", vlanNum + "");
+                if (storedVlan >= 0) {
+                    outputMap.put("taggedReplace", "true");
+                } else {
+                    outputMap.put("taggedReplace", "false");
+                }
+            } else {
+                if (status.equals("SUBMITTED") || status.equals("ACCEPTED")) {
+                    outputMap.put("vlanReplace", "VLAN setup in progress");
+                } else {
+                    outputMap.put("vlanReplace",
+                                  "No VLAN tag was ever set up");
+                }
+            }
+        } else if (layer3Data != null) {
+            strParam = layer3Data.getSrcHost();
+            try {
+                inetAddress = InetAddress.getByName(strParam);
+                hostName = inetAddress.getHostName();
+            } catch (UnknownHostException e) {
+                hostName = strParam;
+            }
+            outputMap.put("sourceReplace", hostName);
+            strParam = layer3Data.getDestHost();
+            try {
+                inetAddress = InetAddress.getByName(strParam);
+                hostName = inetAddress.getHostName();
+            } catch (UnknownHostException e) {
+                hostName = strParam;
+            }
+            outputMap.put("destinationReplace", hostName);
+            intParam = layer3Data.getSrcIpPort();
+            if ((intParam != null) && (intParam != 0)) {
+                outputMap.put("sourcePortReplace", intParam);
+            }
+            intParam = layer3Data.getDestIpPort();
+            if ((intParam != null) && (intParam != 0)) {
+                outputMap.put("destinationPortReplace", intParam);
+            }
+            strParam = layer3Data.getProtocol();
+            if (strParam != null) {
+                outputMap.put("protocolReplace", strParam);
+            }
+            strParam = layer3Data.getDscp();
+            if (strParam !=  null) {
+                outputMap.put("dscpReplace", strParam);
+            }
+        }
+        if (mplsData != null) {
+            longParam = mplsData.getBurstLimit();
+            if (longParam != null) {
+                outputMap.put("burstLimitReplace", longParam);
+            }
+            if (mplsData.getLspClass() != null) {
+                outputMap.put("lspClassReplace", mplsData.getLspClass());
+            }
+        }
+        String pathStr = BssUtils.pathToString(path, false);
+        // in this case, path has not been set up yet, or an error has occurred
+        // and the path will never be set up
+        if ((pathStr != null) && pathStr.equals("")) {
+            return;
+        }
+        // don't allow non-authorized user to see internal hops
+        if ((pathStr != null) && !rmiReply.isInternalPathAuthorized()) {
+            String[] hops = pathStr.trim().split("\n");
+            pathStr = hops[0] + "\n";
+            pathStr += hops[hops.length-1];
+        }
+        if (pathStr != null) {
+            StringBuilder sb = new StringBuilder();
+            // Utils.pathToString has new line separated hops
+            String[] hops = pathStr.trim().split("\n");
+            sb.append("<tbody>");
+            // enforce one hop per line in outer table cell
+            for (int i=0; i < hops.length; i++) {
+                sb.append("<tr><td class='innerHops'>" + hops[i] + "</td></tr>");
+            }
+            sb.append("</tbody>");
+            outputMap.put("pathReplace", sb.toString());
+        }
+        path = resv.getPath(PathType.INTERDOMAIN);
+        String interPathStr = BssUtils.pathToString(path, true);
+        if ((interPathStr != null) &&
+                !interPathStr.trim().equals("")) {
+            StringBuilder sb = new StringBuilder();
+            String[] hops = interPathStr.trim().split("\n");
+            // enforce one hop per line in outer table cell
+            sb.append("<tbody>");
+            for (int i=0; i < hops.length; i++) {
+                sb.append("<tr><td class='innerHops'>" + hops[i] +
+                          "</td></tr>");
+            }
+            sb.append("</tbody>");
+            outputMap.put("interPathReplace", sb.toString());
+        }
     }
 }
