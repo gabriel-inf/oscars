2b4d30993d6462fde73733d5f6147eaaf644fa6e
andy <andy@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2009-02-22 22:11:35 +0000

    Adding TERCE back to PathfinderFactory options
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@7641 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- src/net/es/oscars/pathfinder/PCEMethod.java
+++ src/net/es/oscars/pathfinder/PCEMethod.java
@@ -5,7 +5,7 @@ public class PCEMethod {
     public static final String STATIC = "static";
     public static final String DATABASE = "database";
     public static final String PERFSONAR = "perfsonar";
-
+    public static final String TERCE = "terce";
 
     private PCEMethod() {
     }
--- src/net/es/oscars/pathfinder/PathfinderFactory.java
+++ src/net/es/oscars/pathfinder/PathfinderFactory.java
@@ -3,6 +3,7 @@ package net.es.oscars.pathfinder;
 import net.es.oscars.pathfinder.db.DBPathfinder;
 import net.es.oscars.pathfinder.perfsonar.PSPathfinder;
 import net.es.oscars.pathfinder.staticroute.*;
+import net.es.oscars.pathfinder.terce.TERCEPathfinder;
 
 
 /**
@@ -40,6 +41,8 @@ public class PathfinderFactory {
             return new PSPathfinder(dbname);
         } else if (method.equals(PCEMethod.STATIC)) {
             return new XMLFileLocalPathfinder(dbname);
+        }else if (method.equals(PCEMethod.TERCE)) {
+            return new TERCEPathfinder(dbname);
         }
         return null;
     }
