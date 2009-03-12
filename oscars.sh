96371079df4fa28a1405f215d07539ff21ae5b9b
andy <andy@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2009-03-12 20:24:39 +0000

    Getting Tomcat's PID so can force kill if needed. I think this will prevent the 'java.lang.ClassNotFoundException: net.es.oscars.rmi.bss.BssRmiInterface (no security manager: RMI class loader disabled)' error I am occasionally seeing from OSCARSCoreRunner on startup sporadically. It's hard to recreate and a kill -9 seems to fix the problem when it surfaces.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@7735 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- oscars.sh
+++ oscars.sh
@@ -9,11 +9,18 @@ printHelp ()
     echo "Description: Starts/stops Tomcat, AAA, NotificationBroker and OSCARS core processes."
     echo "Options:"
     echo "-d           daemonize all processes"
-    echo "-f           force existing aaa, oscars, and notifybroker core process to stop with a KILL signal"
+    echo "-f           force existing tomcat, aaa, oscars and notifybroker core process to stop with a KILL signal"
     echo "-h           prints this help message"
     echo "-o <file>    output file for daemon STDOUT and STDERR. defaults to /dev/null"
 }
 ###############################################################################
+DEFAULT_PID_DIR="${OSCARS_HOME-.}/run"
+FORCE_KILL=0
+#CATALINA_PID is a special variable defined by Tomcat
+if [ -z "$CATALINA_PID" ]; then
+    CATALINA_PID="$DEFAULT_PID_DIR/tomcat.pid"
+    export CATALINA_PID;
+fi
 
 cd ${OSCARS_HOME-.};
 
@@ -23,8 +30,10 @@ for opt in $*
 do
     if [ "$opt" == "stop" ]; then
         ACTION="stop"
+    elif [ "$opt" == "-f" ]; then
+        FORCE_KILL=1
     elif [ "$opt" == "-p" ]; then
-        echo "-p not allowed. All core proceses cannot use the same PID file"
+        echo "-p not allowed. All proceses cannot use the same PID file"
         exit 1
     elif [ "$opt" == "-h" ]; then
         printHelp
@@ -32,10 +41,24 @@ do
     fi
 done
 
-#Stop tomcat but suppress any complaints about it not already running
-$CATALINA_HOME/bin/shutdown.sh > /dev/null 2>&1
-echo "Sleeping for 10 seconds while Tomcat stops..."
-sleep 10
+#Restart Tomcat
+if [ $FORCE_KILL == 0 ]; then
+    #supress any error about tomcat not running
+    $CATALINA_HOME/bin/shutdown.sh > /dev/null 2>&1
+    echo "Sleeping for 10 seconds while Tomcat stops..."
+    sleep 10
+fi
+#Get the Tomcat PID
+PID=""
+if [ -f "$CATALINA_PID" ]; then
+    PID=`cat $CATALINA_PID`
+fi
+#Make sure Tomcat is really dead
+if [ -n "$PID" ]; then
+    kill -9 $PID 2>/dev/null
+    rm "$CATALINA_PID"
+fi
+#Start Tomcat or print it stopeed
 if [ -z "$ACTION" ]; then
     $CATALINA_HOME/bin/startup.sh
 else
@@ -46,7 +69,5 @@ fi
 ./aaa-core.sh -d $*
 ./notifybroker-core.sh -d $*
 ./oscars-core.sh -d $*
-
-
 
 exit 0
