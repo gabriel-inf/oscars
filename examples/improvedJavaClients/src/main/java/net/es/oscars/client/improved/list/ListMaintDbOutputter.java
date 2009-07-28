d560756db2a50419259d2289d5a32fd1402a0a15
haniotak <haniotak@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2009-07-28 20:15:21 +0000

    added BW, VLAN info
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@8108 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- examples/improvedJavaClients/src/main/java/net/es/oscars/client/improved/list/ListMaintDbOutputter.java
+++ examples/improvedJavaClients/src/main/java/net/es/oscars/client/improved/list/ListMaintDbOutputter.java
@@ -84,6 +84,7 @@ public class ListMaintDbOutputter implements ListOutputterInterface {
                 hopOutput += prefix+"path-+-0-+-"+i+"-+-hop-+-"+outHop+"\n";
                 i++;
             }
+            Integer bw = resv.getBandwidth()*1000000;
 
             output += prefix +"carrier-+-"+carrier+"\n";
             output += prefix +"id-+-"+gri+"\n";
@@ -96,6 +97,15 @@ public class ListMaintDbOutputter implements ListOutputterInterface {
             output += prefix +"endTime-+-"+endTime+"\n";
             output += prefix +"source-+-"+src+"\n";
             output += prefix +"destination-+-"+dst+"\n";
+            output += prefix +"bandwidth-+-"+bw+"\n";
+            if (resv.getPathInfo().getLayer2Info() != null) {
+                output += prefix +"type-+-L2\n";
+                String vlan = resv.getPathInfo().getLayer2Info().getSrcVtag().getString();
+                output += prefix +"vlan-+-"+vlan+"\n";
+            } else {
+                output += prefix +"type-+-L3\n";
+                output += prefix +"vlan-+-N0\n";
+            }
             output += hopOutput;
         }
 
