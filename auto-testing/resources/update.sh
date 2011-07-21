5cbd94cc3495873c4ffc22c0bad3048c2bf374be
njacobson <njacobson@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2011-07-21 13:48:34 +0000

    syncing trunk and branch
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@9626 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- auto-testing/resources/update.sh
+++ auto-testing/resources/update.sh
@@ -6,6 +6,7 @@ OSCARS_DIST=/usr/local/oscars-0.6
 OSCARS_HOME=/usr/local/oscars
 MVN_HOME=/usr/local/apache-maven-2.2.1
 
+
 cd $OSCARS_DIST
 $OSCARS_DIST/bin/stopServers.sh ALL
 
@@ -20,19 +21,35 @@ cp -f $OSCARS_HOME/VlanPCE/conf/config.SSL.yaml $HOME/VlanPCE
 mkdir -p $HOME/Utils
 cp -f $OSCARS_HOME/Utils/conf/config.yaml $HOME/Utils
 
+mkdir -p $HOME/WBUIService
+cp -f $OSCARS_HOME/WBUIService/conf/jetty.HTTP.xml $HOME/WBUIService
+cp -f $OSCARS_HOME/WBUIService/conf/jetty.SSL.xml $HOME/WBUIService
+
+
 svn update
 $MVN_HOME/bin/mvn clean
 find . -name \*wsdl -exec touch {} \;
 
 $MVN_HOME/bin/mvn install -DskipTests
 
-$OSCARS_DIST/auto-testing/resources/do_export.sh
+
+dirs=( AuthNPolicyService AuthNService AuthZPolicyService AuthZService BandwidthPCE ConnectivityPCE CoordService DijkstraPCE LookupService NotificationBridgeService OSCARSInternalService OSCARSService PCEService PSSService ResourceManagerService TopoBridgeService Utils VlanPCE WBUIService WSNBrokerService )
+
+for dir in ${dirs[@]}
+do
+	if [ -d $OSCARS_HOME/$dir/conf ]; then
+		cd $OSCARS_HOME/$dir/conf
+		rm -rf *
+	fi
+done
+
+$OSCARS_DIST/bin/exportconfig
 
 cp -f $HOME/TopoBridgeService/* $OSCARS_HOME/TopoBridgeService/conf
 cp -f $HOME/VlanPCE/* $OSCARS_HOME/VlanPCE/conf
 cp -f $HOME/Utils/* $OSCARS_HOME/Utils/conf
+cp -f $HOME/WBUIService/* $OSCARS_HOME/WBUIService/conf
 
-cp -f $OSCARS_DIST/auto-testing/test_main.pl $OSCARS_DIST/api/
 cp -f $OSCARS_DIST/auto-testing/resources/testdomain* $OSCARS_HOME/TopoBridgeService/conf/
 cp -rf $OSCARS_DIST/auto-testing/Lib $OSCARS_DIST/api/
 
