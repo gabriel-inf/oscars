e1ca40a43a1b6f13b3349d382cd299ca13a4ffd9
njacobson <njacobson@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2011-07-22 14:09:21 +0000

    synchronizeing script that updates the testbed between branch version and trunk
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@9638 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- auto-testing/resources/update.sh
+++ auto-testing/resources/update.sh
@@ -10,27 +10,36 @@ MVN_HOME=/usr/local/apache-maven-2.2.1
 cd $OSCARS_DIST
 $OSCARS_DIST/bin/stopServers.sh ALL
 
+mkdir -p $HOME/OSCARSService
+cp -f $OSCARS_HOME/OSCARSService/conf/config.HTTP.yaml $HOME/OSCARSService
+cp -f $OSCARS_HOME/OSCARSService/conf/config.SSL.yaml $HOME/OSCARSService
+
 mkdir -p $HOME/TopoBridgeService
 cp -f $OSCARS_HOME/TopoBridgeService/conf/config.HTTP.yaml $HOME/TopoBridgeService
 cp -f $OSCARS_HOME/TopoBridgeService/conf/config.SSL.yaml $HOME/TopoBridgeService
+
+mkdir -p $HOME/Utils
+cp -f $OSCARS_HOME/Utils/conf/config.yaml $HOME/Utils
 
 mkdir -p $HOME/VlanPCE
 cp -f $OSCARS_HOME/VlanPCE/conf/config.HTTP.yaml $HOME/VlanPCE
 cp -f $OSCARS_HOME/VlanPCE/conf/config.SSL.yaml $HOME/VlanPCE
 
-mkdir -p $HOME/Utils
-cp -f $OSCARS_HOME/Utils/conf/config.yaml $HOME/Utils
-
 mkdir -p $HOME/WBUIService
 cp -f $OSCARS_HOME/WBUIService/conf/jetty.HTTP.xml $HOME/WBUIService
 cp -f $OSCARS_HOME/WBUIService/conf/jetty.SSL.xml $HOME/WBUIService
 
+mkdir -p $HOME/WSNBrokerService
+cp -f $OSCARS_HOME/WSNBrokerService/conf/config.HTTP.yaml $HOME/WSNBrokerService
+cp -f $OSCARS_HOME/WSNBrokerService/conf/config.SSL.yaml $HOME/WSNBrokerService
 
-svn update
+cp -f $OSCARS_DIST/api/Lib/SimpleTest.pm $OSCARS_DIST/api/Lib/SimpleTest.pm.bak
+
+
 $MVN_HOME/bin/mvn clean
 find . -name \*wsdl -exec touch {} \;
-
-$MVN_HOME/bin/mvn install -DskipTests
+svn update
+$MVN_HOME/bin/mvn install 
 
 
 dirs=( AuthNPolicyService AuthNService AuthZPolicyService AuthZService BandwidthPCE ConnectivityPCE CoordService DijkstraPCE LookupService NotificationBridgeService OSCARSInternalService OSCARSService PCEService PSSService ResourceManagerService TopoBridgeService Utils VlanPCE WBUIService WSNBrokerService )
@@ -45,13 +54,17 @@ done
 
 $OSCARS_DIST/bin/exportconfig
 
+cp -f $HOME/OSCARSService/* $OSCARS_HOME/OSCARSService/conf
 cp -f $HOME/TopoBridgeService/* $OSCARS_HOME/TopoBridgeService/conf
-cp -f $HOME/VlanPCE/* $OSCARS_HOME/VlanPCE/conf
 cp -f $HOME/Utils/* $OSCARS_HOME/Utils/conf
+cp -f $HOME/VlanPCE/* $OSCARS_HOME/VlanPCE/conf
 cp -f $HOME/WBUIService/* $OSCARS_HOME/WBUIService/conf
+cp -f $HOME/WSNBrokerService/* $OSCARS_HOME/WSNBrokerService/conf
 
 cp -f $OSCARS_DIST/auto-testing/resources/testdomain* $OSCARS_HOME/TopoBridgeService/conf/
 cp -rf $OSCARS_DIST/auto-testing/Lib $OSCARS_DIST/api/
+cp -f $OSCARS_DIST/api/Lib/SimpleTest.pm.bak $OSCARS_DIST/api/Lib/SimpleTest.pm
 
 $OSCARS_DIST/bin/startServers.sh DEVELOPMENT ALL
+$OSCARS_DIST/bin/testServers.sh DEVELOPMENT
 
