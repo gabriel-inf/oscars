1118d247efac300c6170d4e892bf2a5662d81a57
njacobson <njacobson@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2011-10-03 15:16:47 +0000

    copying testbed update script from odev testbed
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@9843 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- auto-testing/resources/update.sh
+++ auto-testing/resources/update.sh
@@ -5,36 +5,28 @@ MODE=PRODUCTION
 
 export MAVEN_OPTS='-Xmx512M -XX:MaxPermSize=512M'
 
-OSCARS_DIST=/usr/local/oscars-0.6
+OSCARS_DIST=/usr/local/oscars_dist
 OSCARS_HOME=/usr/local/oscars
-MVN_HOME=/usr/local/apache-maven-2.2.1
+MVN_HOME=/usr/local/maven
 
 
 cd $OSCARS_DIST
 $OSCARS_DIST/bin/stopServers.sh ALL
 
-#mkdir -p $HOME/OSCARSService
-#cp -f $OSCARS_HOME/OSCARSService/conf/config.HTTP.yaml $HOME/OSCARSService
-#cp -f $OSCARS_HOME/OSCARSService/conf/config.SSL.yaml $HOME/OSCARSService
-
 mkdir -p $HOME/TopoBridgeService
 cp -f $OSCARS_HOME/TopoBridgeService/conf/config.HTTP.yaml $HOME/TopoBridgeService
 cp -f $OSCARS_HOME/TopoBridgeService/conf/config.SSL.yaml $HOME/TopoBridgeService
 
-#mkdir -p $HOME/Utils
-#cp -f $OSCARS_HOME/Utils/conf/config.yaml $HOME/Utils
+mkdir -p $HOME/Utils
+cp -f $OSCARS_HOME/Utils/conf/config.yaml $HOME/Utils
 
-#mkdir -p $HOME/VlanPCE
-#cp -f $OSCARS_HOME/VlanPCE/conf/config.HTTP.yaml $HOME/VlanPCE
-#cp -f $OSCARS_HOME/VlanPCE/conf/config.SSL.yaml $HOME/VlanPCE
+mkdir -p $HOME/WBUIService
+cp -f $OSCARS_HOME/WBUIService/conf/jetty.HTTP.xml $HOME/WBUIService
+cp -f $OSCARS_HOME/WBUIService/conf/jetty.SSL.xml $HOME/WBUIService
 
-#mkdir -p $HOME/WBUIService
-#cp -f $OSCARS_HOME/WBUIService/conf/jetty.HTTP.xml $HOME/WBUIService
-#cp -f $OSCARS_HOME/WBUIService/conf/jetty.SSL.xml $HOME/WBUIService
-
-#mkdir -p $HOME/WSNBrokerService
-#cp -f $OSCARS_HOME/WSNBrokerService/conf/config.HTTP.yaml $HOME/WSNBrokerService
-#cp -f $OSCARS_HOME/WSNBrokerService/conf/config.SSL.yaml $HOME/WSNBrokerService
+mkdir -p $HOME/WSNBrokerService
+cp -f $OSCARS_HOME/WSNBrokerService/conf/config.HTTP.yaml $HOME/WSNBrokerService
+cp -f $OSCARS_HOME/WSNBrokerService/conf/config.SSL.yaml $HOME/WSNBrokerService
 
 cp -f $OSCARS_DIST/api/Lib/SimpleTest.pm $OSCARS_DIST/api/Lib/SimpleTest.pm.bak
 
@@ -57,15 +49,13 @@ done
 
 $OSCARS_DIST/bin/exportconfig
 
-#cp -f $HOME/OSCARSService/* $OSCARS_HOME/OSCARSService/conf
 cp -f $HOME/TopoBridgeService/* $OSCARS_HOME/TopoBridgeService/conf
-#cp -f $HOME/Utils/* $OSCARS_HOME/Utils/conf
-#cp -f $HOME/VlanPCE/* $OSCARS_HOME/VlanPCE/conf
-#cp -f $HOME/WBUIService/* $OSCARS_HOME/WBUIService/conf
-#cp -f $HOME/WSNBrokerService/* $OSCARS_HOME/WSNBrokerService/conf
+cp -f $HOME/Utils/* $OSCARS_HOME/Utils/conf
+cp -f $HOME/WBUIService/* $OSCARS_HOME/WBUIService/conf
+cp -f $HOME/WSNBrokerService/* $OSCARS_HOME/WSNBrokerService/conf
 
-#cp -f $OSCARS_DIST/auto-testing/resources/testdomain* $OSCARS_HOME/TopoBridgeService/conf/
-#cp -rf $OSCARS_DIST/auto-testing/Lib $OSCARS_DIST/api/
+cp -f $OSCARS_DIST/auto-testing/resources/testdomain* $OSCARS_HOME/TopoBridgeService/conf/
+cp -rf $OSCARS_DIST/auto-testing/Lib $OSCARS_DIST/api/
 cp -f $OSCARS_DIST/api/Lib/SimpleTest.pm.bak $OSCARS_DIST/api/Lib/SimpleTest.pm
 
 #switch back to OSCARS_DIST so that the .out files will be put there
@@ -73,6 +63,4 @@ cd $OSCARS_DIST
 
 $OSCARS_DIST/bin/startServers.sh $MODE ALL
 $OSCARS_DIST/bin/testServers.sh $MODE 
-
-echo "Please run '$OSCARS_DIST/tools/bin/idc-wbuiaccess'"
 
