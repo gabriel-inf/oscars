0120d86848d6e8d44fa8d2c6816ca093e657eba9
njacobson <njacobson@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2011-11-07 20:42:41 +0000

    copying utility script from branch to trunk
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@9971 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- auto-testing/resources/update.sh
+++ auto-testing/resources/update.sh
@@ -4,30 +4,39 @@
 MODE=PRODUCTION
 
 export MAVEN_OPTS='-Xmx512M -XX:MaxPermSize=512M'
-OSCARS_DIST=/usr/local/oscars-0.6
-OSCARS_HOME=/usr/local/oscars
-MVN_HOME=/usr/local/apache-maven-2.2.1
 
-# Shutdown all services
+OSCARS_DIST=/usr/local/oscars_dist
+OSCARS_HOME=/usr/local/oscars
+MVN_HOME=/usr/local/maven
+
+
 cd $OSCARS_DIST
 $OSCARS_DIST/bin/stopServers.sh ALL
 
-# Backup config files and any scripts that will be overwritten by update
 mkdir -p $HOME/TopoBridgeService
 cp -f $OSCARS_HOME/TopoBridgeService/conf/config.HTTP.yaml $HOME/TopoBridgeService
 cp -f $OSCARS_HOME/TopoBridgeService/conf/config.SSL.yaml $HOME/TopoBridgeService
-cp -f $OSCARS_HOME/TopoBridgeService/conf/testdomain-?.net.xml $HOME/TopoBridgeService
+
 mkdir -p $HOME/Utils
 cp -f $OSCARS_HOME/Utils/conf/config.yaml $HOME/Utils
+
 mkdir -p $HOME/WBUIService
 cp -f $OSCARS_HOME/WBUIService/conf/jetty.HTTP.xml $HOME/WBUIService
 cp -f $OSCARS_HOME/WBUIService/conf/jetty.SSL.xml $HOME/WBUIService
+
 mkdir -p $HOME/WSNBrokerService
 cp -f $OSCARS_HOME/WSNBrokerService/conf/config.HTTP.yaml $HOME/WSNBrokerService
 cp -f $OSCARS_HOME/WSNBrokerService/conf/config.SSL.yaml $HOME/WSNBrokerService
+
 cp -f $OSCARS_DIST/api/Lib/SimpleTest.pm $OSCARS_DIST/api/Lib/SimpleTest.pm.bak
 
-# Clear service config directories for exportconfig
+
+$MVN_HOME/bin/mvn clean
+find . -name \*wsdl -exec touch {} \;
+svn update
+$MVN_HOME/bin/mvn install 
+
+
 dirs=( AuthNPolicyService AuthNService AuthZPolicyService AuthZService BandwidthPCE ConnectivityPCE CoordService DijkstraPCE LookupService NotificationBridgeService OSCARSInternalService OSCARSService PCEService PSSService ResourceManagerService TopoBridgeService Utils VlanPCE WBUIService WSNBrokerService )
 
 for dir in ${dirs[@]}
@@ -37,26 +46,21 @@ do
 		rm -rf *
 	fi
 done
-cd $OSCARS_DIST
 
-# Do the update
-$MVN_HOME/bin/mvn clean
-find . -name \*wsdl -exec touch {} \;
-svn update
-$MVN_HOME/bin/mvn install 
+$OSCARS_DIST/bin/exportconfig
 
-# Restore config files
 cp -f $HOME/TopoBridgeService/* $OSCARS_HOME/TopoBridgeService/conf
 cp -f $HOME/Utils/* $OSCARS_HOME/Utils/conf
 cp -f $HOME/WBUIService/* $OSCARS_HOME/WBUIService/conf
 cp -f $HOME/WSNBrokerService/* $OSCARS_HOME/WSNBrokerService/conf
-# Bring in changes to test suite
+
 cp -f $OSCARS_DIST/auto-testing/resources/testdomain* $OSCARS_HOME/TopoBridgeService/conf/
 cp -rf $OSCARS_DIST/auto-testing/Lib $OSCARS_DIST/api/
-# Restore site specific files
 cp -f $OSCARS_DIST/api/Lib/SimpleTest.pm.bak $OSCARS_DIST/api/Lib/SimpleTest.pm
 
-# Restart and test servers
+#switch back to OSCARS_DIST so that the .out files will be put there
+cd $OSCARS_DIST
+
 $OSCARS_DIST/bin/startServers.sh $MODE ALL
 $OSCARS_DIST/bin/testServers.sh $MODE 
 
