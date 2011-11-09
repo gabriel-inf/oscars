a23b763c24526d011b4bfafa4e1dc7d6d32c14b6
haniotak <haniotak@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2011-11-09 23:13:56 +0000

    initial config
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@9990 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- openflowPSS/bin/run.sh
+++ openflowPSS/bin/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Djava.net.preferIPv4Stack=true -jar target/openflowPSS-0.0.1-SNAPSHOT.one-jar.jar  $*
+java -Djava.net.preferIPv4Stack=true -jar target/bridgePSS-0.0.1-SNAPSHOT.one-jar.jar  $*
