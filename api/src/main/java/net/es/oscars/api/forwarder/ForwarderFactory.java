cf53b34f68021fc02cc84212c3c88232422ad9b1
andy <andy@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2012-01-18 18:15:01 +0000

    Adding some more logging to ForwarderFactory per issue 302
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@10124 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- api/src/main/java/net/es/oscars/api/forwarder/ForwarderFactory.java
+++ api/src/main/java/net/es/oscars/api/forwarder/ForwarderFactory.java
@@ -1,6 +1,7 @@
 package net.es.oscars.api.forwarder;
 
 import java.net.URL;
+import java.util.HashMap;
 import java.util.List;
 
 import org.apache.log4j.Logger;
@@ -8,6 +9,8 @@ import org.apache.log4j.Logger;
 
 import net.es.oscars.api.common.OSCARSIDC;
 import net.es.oscars.utils.soap.OSCARSServiceException;
+import net.es.oscars.logging.ErrSev;
+import net.es.oscars.logging.OSCARSNetLogger;
 import net.es.oscars.lookup.soap.gen.Protocol;
 
 public class ForwarderFactory {
@@ -15,10 +18,15 @@ public class ForwarderFactory {
     private static final Logger LOG = Logger.getLogger(ForwarderFactory.class.getName());
     
     public static Forwarder getForwarder (String domainId) throws OSCARSServiceException {
-
+        OSCARSNetLogger netLogger = OSCARSNetLogger.getTlogger();
+        HashMap<String,String> netLogProps = new HashMap<String,String>();
+        netLogProps.put("domainId", domainId);
+        LOG.info(netLogger.start("ForwarderFactory.getForwarder"));
         List<Protocol> protos = OSCARSIDC.getInstance().getLookupClient().lookup("urn:ogf:network:domain=" + domainId);
         if ( (protos == null) || (protos.size() == 0)) {
-            throw new OSCARSServiceException ("register request failed","system");
+            LOG.error(netLogger.error("ForwarderFactory.getForwarder", ErrSev.MAJOR, 
+                    "lookup request failed", null, netLogProps));
+            throw new OSCARSServiceException ("lookup request failed","system");
         }
 
         URL url = null;
@@ -29,9 +37,13 @@ public class ForwarderFactory {
                 try {
                     url =  new URL (p.getLocation());
                     Forwarder forwarder = new Forwarder06 (domainId, url);
+                    LOG.info(netLogger.end("ForwarderFactory.getForwarder", null, 
+                            p.getLocation(), netLogProps));
                     return forwarder;
                 } catch (Exception e) {
                     // Invalid URL. Ignore protocol
+                    LOG.error(netLogger.error("ForwarderFactory.getForwarder", 
+                            ErrSev.MAJOR, e.getMessage(), p.getLocation(), netLogProps));
                     continue;
                 }
             } else if ("http://oscars.es.net/OSCARS".equals(p.getType())) {
@@ -39,15 +51,19 @@ public class ForwarderFactory {
                 try {
                     url =  new URL (p.getLocation());
                     Forwarder forwarder = new Forwarder05 (domainId, url);
+                    LOG.info(netLogger.end("ForwarderFactory.getForwarder", null, 
+                            p.getLocation(), netLogProps));
                     return forwarder;
                 } catch (Exception e) {
                     // Invalid URL. Ignore protocol
+                    LOG.error(netLogger.error("ForwarderFactory.getForwarder",
+                            ErrSev.MAJOR, e.getMessage(), p.getLocation(), netLogProps));
                     continue;
                 }
             }
         }
 
-        LOG.error("no location for IDC controlling domain " + domainId);
+        LOG.error(netLogger.error("ForwarderFactory.getForwarder", ErrSev.MAJOR, "no location for IDC controlling domain " + domainId));
         throw new OSCARSServiceException ("no location for IDC controlling domain " + domainId);
     }
     
