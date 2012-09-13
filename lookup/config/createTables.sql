d9e5ed8d28d69db123a93b580552126c4dea2b20
haniotak <haniotak@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2012-09-13 15:45:59 +0000

    using AUTO_INCREMENT to support mysql 5.1
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@10343 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- lookup/config/createTables.sql
+++ lookup/config/createTables.sql
@@ -1,12 +1,12 @@
 CREATE TABLE services (
-    id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     type VARCHAR(100) NOT NULL,
     lastUpdated BIGINT NOT NULL
 );
 CREATE INDEX servTypeIndex ON services (type);
 
 CREATE TABLE protocols (
-    id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     serviceId INT NOT NULL,
     protocol VARCHAR(300) NOT NULL,
     url VARCHAR(300) NOT NULL
@@ -14,7 +14,7 @@ CREATE TABLE protocols (
 CREATE UNIQUE INDEX serviceProtoIndex ON protocols (serviceId, protocol);
 
 CREATE TABLE relationships (
-    id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     serviceId INT NOT NULL,
     type VARCHAR(100) NOT NULL,
     relatedTo VARCHAR(300) NOT NULL
@@ -22,7 +22,7 @@ CREATE TABLE relationships (
 CREATE INDEX relServiceTypeIndex ON relationships (serviceId, type);
 
 CREATE TABLE registrations (
-    id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     regElement VARCHAR(300) NOT NULL,
     serviceUrl VARCHAR(300) NOT NULL,
     registrationKey VARCHAR(300)
