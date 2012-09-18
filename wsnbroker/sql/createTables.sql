148db4c575e79c484362d37a9a20fdd3c5a20833
andy <andy@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2012-09-18 16:44:02 +0000

    Reverting changes made to wsnbroker and lookup sql scripts. They do not use MySQL but an embedded dery db so the changes made break things.
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@10350 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- wsnbroker/sql/createTables.sql
+++ wsnbroker/sql/createTables.sql
@@ -2,7 +2,7 @@
 -- Table of subscriptions to notifications
 --
 CREATE TABLE subscriptions (
-  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   referenceId VARCHAR(512) NOT NULL,
   userLogin VARCHAR(256) NOT NULL,
   url VARCHAR(512) NOT NULL,
@@ -15,7 +15,7 @@ CREATE UNIQUE INDEX subRefIdIndex ON subscriptions (referenceId);
 -- Table to hold filters for each subscription
 --
 CREATE TABLE subscriptionFilters (
-  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   subscriptionId INT NOT NULL,
   type VARCHAR(256) NOT NULL,
   value VARCHAR(1024) NOT NULL
