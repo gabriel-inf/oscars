57d80fe4d19474f6747afb2b92a95f7bd918d2a3
haniotak <haniotak@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2013-08-29 19:31:57 +0000

    small change in getters/setters
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@10540 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- utils/src/main/java/net/es/oscars/utils/task/Timeline.java
+++ utils/src/main/java/net/es/oscars/utils/task/Timeline.java
@@ -6,4 +6,44 @@ public class Timeline {
     protected long scheduledFor;
     protected long started;
     protected long finished;
+
+    public long getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(long createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public long getScheduledAt() {
+        return scheduledAt;
+    }
+
+    public void setScheduledAt(long scheduledAt) {
+        this.scheduledAt = scheduledAt;
+    }
+
+    public long getScheduledFor() {
+        return scheduledFor;
+    }
+
+    public void setScheduledFor(long scheduledFor) {
+        this.scheduledFor = scheduledFor;
+    }
+
+    public long getStarted() {
+        return started;
+    }
+
+    public void setStarted(long started) {
+        this.started = started;
+    }
+
+    public long getFinished() {
+        return finished;
+    }
+
+    public void setFinished(long finished) {
+        this.finished = finished;
+    }
 }
