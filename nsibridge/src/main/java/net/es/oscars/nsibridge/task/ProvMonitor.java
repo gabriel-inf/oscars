0f56a892b7974f5b3df4481a0613e63dbf59b300
haniotak <haniotak@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2013-08-30 17:22:17 +0000

    quick NPE fix, typo
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@10545 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- nsibridge/src/main/java/net/es/oscars/nsibridge/task/ProvMonitor.java
+++ nsibridge/src/main/java/net/es/oscars/nsibridge/task/ProvMonitor.java
@@ -34,15 +34,16 @@ public class ProvMonitor implements Job {
         List<ConnectionRecord> recordList = em.createQuery(query, ConnectionRecord.class).getResultList();
         em.getTransaction().commit();
         for (ConnectionRecord cr: recordList) {
-            // log.info("connId: "+cr.getConnectionId());
-            // log.info("  RSM: "+ cr.getReserveState());
-            // log.info("  PSM: "+ cr.getProvisionState());
-            // log.info("  LSM: "+ cr.getLifecycleState());
+            // log.debug("connId: "+cr.getConnectionId());
+            // log.debug("  RSM: "+ cr.getReserveState());
+            // log.debug("  PSM: "+ cr.getProvisionState());
+            // log.debug("  LSM: "+ cr.getLifecycleState());
 
             if (cr.getLifecycleState() == null) {
                 continue;
             }
-            // do not set up oscars if we are TERMINATING / TERMINATED
+
+            // do not set up oscars if we are TERMINATING / TERMINATED / FAILED
             if (cr.getLifecycleState().equals(LifecycleStateEnumType.CREATED)) {
                 if (cr.getProvisionState() == null) {
                     continue;
@@ -50,10 +51,13 @@ public class ProvMonitor implements Job {
 
                 if (cr.getProvisionState().equals(ProvisionStateEnumType.PROVISIONED)) {
                     String connId = cr.getConnectionId();
-                    // log.info("---- will SETUP: "+connId);
+                    // log.debug("---- will SETUP: "+connId);
                     OscarsProvQueue.getInstance().getInspect().put(connId, "SETUP");
 
                     ResvRecord rr = ConnectionRecord.getLatestResvRecord(cr);
+                    if (rr == null) {
+                        continue;
+                    }
 
 
                     if (rr.getStartTime().after(now)) {
