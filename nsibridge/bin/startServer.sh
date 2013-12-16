33713178ff1536d64761f7406744bb42705421c2
haniotak <haniotak@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2013-12-16 20:51:13 +0000

    fix typo
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@10756 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- nsibridge/bin/startServer.sh
+++ nsibridge/bin/startServer.sh
@@ -29,7 +29,7 @@ javaFlags="-Xmx512m "
 javaFlags="$javaFlags -Djava.net.preferIPv4Stack=true "
 javaFlags="$javaFlags -Dlog4j.configuration=file:$confDir/log4j.properties "
 javaFlags="$javaFlags -Dnsibridge.manifest=file:$confDir/manifest.yaml "
-javaFlags="$javaFlags -Dnsibridge.beans=file:$confDir/beans.xml
+javaFlags="$javaFlags -Dnsibridge.beans=file:$confDir/beans.xml "
 javaFlags="$javaFlags -Dcom.sun.xml.bind.v2.runtime.JAXBContextImpl.fastBoot=true "
 javaFlags="$javaFlags -Dorg.apache.cxf.JDKBugHacks.defaultUsesCaches=true "
 
