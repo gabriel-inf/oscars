597de3c0766313e4c33c8a3fb1cd481ae09aa17d
haniotak <haniotak@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2014-05-13 15:56:28 +0000

    create database now (fix for issue)
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@10802 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- eomplsPSS/sql/grantPrivs.sql
+++ eomplsPSS/sql/grantPrivs.sql
@@ -1,2 +1,4 @@
+CREATE DATABASE IF NOT EXISTS eomplspss;
+CREATE DATABASE IF NOT EXISTS testeomplspss;
 GRANT select, insert, update, delete ON eomplspss.* TO 'oscars'@'localhost' IDENTIFIED BY 'mypass';
 GRANT select, insert, update, delete, create, drop, alter on `testeomplspss`.* TO 'oscars'@'localhost' IDENTIFIED BY 'mypass';
