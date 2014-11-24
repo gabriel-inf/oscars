3149e3b657ba4f0563753ed89232a8dd9b597ab8
haniotak <haniotak@c75a6b91-02ef-0310-b74a-e20389c9c41d>
2014-11-24 21:10:24 +0000

    new API revision with fixes vs POODLE attack
    git-svn-id: https://oscars.es.net/repos/oscars/trunk@10853 c75a6b91-02ef-0310-b74a-e20389c9c41d

--- api/bin/expandOneJar.sh
+++ api/bin/expandOneJar.sh
@@ -2,5 +2,5 @@
 vers=`cat $OSCARS_DIST/VERSION`
 rm -rf $OSCARS_DIST/api/target/tmp
 mkdir $OSCARS_DIST/api/target/tmp
-cp $OSCARS_DIST/api/config/log4j*properties $OSCARS_DIST/api/target/classes
+cp $OSCARS_DIST/api/networkSpec/log4j*properties $OSCARS_DIST/api/target/classes
 (cd $OSCARS_DIST/api/target/tmp; jar -xf ../api-$vers.one-jar.jar )
